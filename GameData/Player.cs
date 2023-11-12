8d3ebe4b944fb9a612603ef87b06292259f7fdbc
Renatuscape <rrjonsberg@gmail.com>
2023-11-12 11:46:01 +0100

    Added support for skills and upgrades in Add().

--- GameData/Player.cs
+++ GameData/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Searcher;
@@ -9,40 +10,67 @@ public static class Player
     public static SerializableDictionary<string, int> skills = new();
     public static SerializableDictionary<string, int> upgrades = new();
 
-    /*public static void Add(string searchID, int amount)
+    public static void Add(dynamic addObject, int amount, int maxStack)
     {
-
-        if (items.ContainsKey(searchID))
+        if (addObject is Item)
         {
-            items[searchID] += amount;
+            var item = (Item)addObject;
+            if (items.ContainsKey(item.objectID))
+            {
+                if (items[item.objectID] + amount <= maxStack && items[item.objectID] + amount >= 0)
+                {
+                    items[item.objectID] += amount;
+                }
+            }
+            else
+            {
+                if (amount >= 0 && amount <= item.maxStack)
+                {
+                    items.Add(item.objectID, amount);
+                }
+            }
+        }
+        else if (addObject is Skill)
+        {
+            var skill = (Skill)addObject;
+            if (skills.ContainsKey(skill.objectID))
+            {
+                if (skills[skill.objectID] + amount <= maxStack && skills[skill.objectID] + amount >= 0)
+                {
+                    skills[skill.objectID] += amount;
+                }
+            }
+            else
+            {
+                if (amount >= 0 && amount <= skill.maxLevel)
+                {
+                    skills.Add(skill.objectID, amount);
+                }
+            }
+        }
+        else if (addObject is Upgrade)
+        {
+            var upgrade = (Upgrade)addObject;
+            if (upgrades.ContainsKey(upgrade.objectID))
+            {
+                if (upgrades[upgrade.objectID] + amount <= maxStack && upgrades[upgrade.objectID] + amount >= 0)
+                {
+                    upgrades[upgrade.objectID] += amount;
+                }
+            }
+            else
+            {
+                if (amount >= 0 && amount <= upgrade.maxLevel)
+                {
+                    upgrades.Add(upgrade.objectID, amount);
+                }
+            }
         }
         else
         {
-            var item = Items.FindByID(searchID);
-            if (item != null)
-            {
-                items.Add(item.objectID, amount);
-            }
+            Debug.LogWarning($"{addObject.name} is not a valid type for Player.Add.");
         }
-    }*/
 
-    public static void Add(Item item, int amount, int maxStack)
-    {
-        if (items.ContainsKey(item.objectID))
-        {
-            if (items[item.objectID] + amount <= maxStack && items[item.objectID] + amount >= 0)
-            {
-                items[item.objectID] += amount;
-            }
-        }
-        else
-        {
-            if (amount >= 0 && amount <= item.maxStack)
-            {
-            items.Add(item.objectID, amount);
-            }
-
-        }
     }
 
     public static int GetCount(string searchID)
