4b0967fa4ea8e3bde6f664879c1b549c84c38fea
Renatuscape <rrjonsberg@gmail.com>
2023-11-18 23:45:46 +0100

    Created simple animator class for looping animations.

---
+++ SimpleAnimator.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class SimpleAnimator : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public List<Sprite> spriteList = new();
+    public float secondsPerFrame = 0.15f;
+    int frame = 0;
+    float timeLeft;
+    void Start()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft < 0)
+        {
+            Animator(spriteList);
+            timeLeft = secondsPerFrame;
+        }
+    }
+
+    void Animator(List<Sprite> frames, bool isReversed = false)
+    {
+        if (isReversed)
+        {
+            frame--;
+
+            if (frame < 0)
+            {
+                frame = frames.Count - 1;
+            }
+        }
+        else
+        {
+            frame++;
+
+            if (frame >= frames.Count - 1)
+            {
+                frame = 0;
+            }
+        }
+
+        spriteRenderer.sprite = frames[frame];
+    }
+}
