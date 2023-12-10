98f3450940adc9c45524d91e7e85160f1bbbd6d1
Renatuscape <rrjonsberg@gmail.com>
2023-12-10 15:49:12 +0100

    Class for checking quest requirements and restrictions added.

---
+++ JsonManagers/QuestManager/AdvancementCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class AdvancementCheck
+{
+    public int minimumCharacterLevel;
+    public int minimumDaysPassed;
+
+    //The player must have at least this much
+    public List<IdIntPair> requirements;
+
+    //The player must have less than this amount
+    public List<IdIntPair> restrictions;
+}
