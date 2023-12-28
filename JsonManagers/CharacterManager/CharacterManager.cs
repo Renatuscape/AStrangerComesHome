e5c4200b1d5db44ca5d7094b5169275e56c97f57
Renatuscape <rrjonsberg@gmail.com>
2023-12-28 21:49:58 +0100

    Removed filter mode to point from CharacterManager for smooth character sprites.

--- JsonManagers/CharacterManager/CharacterManager.cs
+++ JsonManagers/CharacterManager/CharacterManager.cs
@@ -122,7 +122,7 @@ public class CharacterManager : MonoBehaviour
 
         byte[] imageBytes = File.ReadAllBytes(filePath);
         imageTexture = new Texture2D(2, 2); // Create an empty texture
-        imageTexture.filterMode = FilterMode.Point; // Set filter mode to Point for pixel-perfect clarity. PREVENTS BLURRINESS
+        //imageTexture.filterMode = FilterMode.Point; // Set filter mode to Point for pixel-perfect clarity. PREVENTS BLURRINESS FOR PIXEL ART
         imageTexture.LoadImage(imageBytes); // Load the image bytes
 
         return imageTexture;
