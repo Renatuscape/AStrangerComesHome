85b2734ce2e0eac70eddc94637c298bfd2ea00de
Renatuscape <rrjonsberg@gmail.com>
2023-12-28 18:57:37 +0100

    DialogueStage component created from old DialogueComponent. Likely obsolete.

---
+++ JsonManagers/DialogueManager/DialogueStage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class DialogueStage
+{
+    public List<string> rawContent;
+    public List<DialogueStep> dialogueSteps; //a step is one line of dialogue from one speaker
+    public List<Choice> choices = new List<Choice>(); //LEAVE CHOICES BLANK TO LOOP DIALOGUE WITHOUT PROGRESSION
+    public bool progressOnContinue; //automatically go to next stage when step is completed
+    public bool noLeaveButton;
+}
