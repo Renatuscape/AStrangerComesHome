246f8d36df8417196e2ca8ad223986e567a3b928
Renatuscape <rrjonsberg@gmail.com>
2023-12-28 18:55:59 +0100

    Created DialogueStep component for holding speaker and text. Might need expansion later to allow for expressions.

---
+++ JsonManagers/DialogueManager/DialogueStep.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueStep
+{
+    public string name;
+    public Character speaker;
+    public string text;
+}
