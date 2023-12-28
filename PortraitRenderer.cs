70bd027ed8e51a4176c69bf4666577305d172101
Renatuscape <rrjonsberg@gmail.com>
2023-12-28 18:56:20 +0100

    Started on Portrait Rendering logic

--- PortraitRenderer.cs
+++ PortraitRenderer.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PortraitRenderer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject playerSprite;
+    float playerDefaultX = -300f;
+    public GameObject rightPortraitContainer;
+    public Image rightCharacterImage;
+    float rightDefaultX = 300f;
+    public GameObject leftPortraitContainer;
+    public Image leftCharacterImage;
+    float leftDefaultX = -300f;
+
+    private void Start()
     {
-        
+        ResetValues();
+    }
+    public void EnableForShop(Character shopkeeper)
+    {
+        //SET SPRITE
+        SetSprite(rightCharacterImage, shopkeeper);
+
+        //ARRANGE
+        playerSprite.SetActive(true);
+        playerSprite.transform.position = new Vector3(-370f, 0, 0);
+        MoveSprite(playerSprite, -370f);
+        rightPortraitContainer.SetActive(true);
+        MoveSprite(rightPortraitContainer, 370f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        ResetValues();
+    }
+
+    void ResetValues()
+    {
+        playerSprite.SetActive(false);
+        MoveSprite(playerSprite, playerDefaultX);
+        rightPortraitContainer.SetActive(false);
+        MoveSprite(rightPortraitContainer, rightDefaultX);
+        leftPortraitContainer.SetActive(false);
+        MoveSprite(leftPortraitContainer, leftDefaultX);
+    }
+
+    void SetSprite(Image image, Character character)
+    {
+        image.sprite = character.sprite;
+    }
+
+    void MoveSprite(GameObject spriteObject, float positionX)
+    {
+        spriteObject.transform.localPosition = new Vector3(positionX, spriteObject.transform.localPosition.y, spriteObject.transform.localPosition.z);
     }
 }
