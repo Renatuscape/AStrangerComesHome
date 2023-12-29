9100b2fbb1cb48804481867ce9ea80f153e1d27e
Renatuscape <rrjonsberg@gmail.com>
2023-12-29 16:45:01 +0100

    Added support for various quest stage types such as memory and popup. Class should probably be renamed questStage for clarity, but might cause issues.

--- JsonManagers/DialogueManager/Dialogue.cs
+++ JsonManagers/DialogueManager/Dialogue.cs
@@ -2,19 +2,34 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+public enum StageType
+{
+    Dialogue,
+    Memory,
+    PopUp
+}
+
 [System.Serializable]
 public class Dialogue
 {
+    public StageType stageType;
     public string objectID;
     public string questID;
     public int questStage;
     public string topicName;
+    public int startTime = 0;
+    public int endTime = 0;
 
     public List<string> content;
     public List<DialogueStep> dialogueSteps = new(); //a step is one line of dialogue from one speaker
+    public List<IdIntPair> requirements = new();
     public List<Choice> choices = new (); //LEAVE CHOICES BLANK TO LOOP DIALOGUE WITHOUT PROGRESSION
     public bool progressOnContinue = false; //automatically go to next stage when step is completed
     public bool noLeaveButton = true; //if this is enabled, it is impossible to leave dialogue until you reach a choice
+
+    //MEMORY SPECIFIC
+    public string location;
+    public string hint;
 }
 
 public static class Dialogues
