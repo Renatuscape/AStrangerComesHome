c3471048c945ccf183f265408deaba03d6a82bfc
Renatuscape <rrjonsberg@gmail.com>
2023-12-29 16:46:07 +0100

    Improved dialogue manager and json dialogue structure to utilise objectID and parsing.

--- JsonManagers/DialogueManager/DialogueManager.cs
+++ JsonManagers/DialogueManager/DialogueManager.cs
@@ -5,6 +5,7 @@ using System.IO;
 using UnityEngine.UI;
 using System.Linq;
 using UnityEngineInternal;
+using System;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -51,7 +52,6 @@ public class DialogueManager : MonoBehaviour
                 {
                     foreach (Dialogue dialogue in dataWrapper.dialogues)
                     {
-                        dialogue.objectID = dialogue.questID + "-s" + dialogue.questStage;
                         InitialiseDialogue(dialogue);
                         filesLoaded++;
                     }
@@ -81,24 +81,62 @@ public class DialogueManager : MonoBehaviour
 
     public static void InitialiseDialogue(Dialogue dialogue)
     {
-        ParseDialogueSteps(dialogue); //set up dialogue steps with proper speaker objects
-        Dialogues.all.Add(dialogue);
-    }
+        if (string.IsNullOrEmpty(dialogue.objectID))
+        {
+            Debug.Log($"Missing objectID for {dialogue}");
+        }
+        else
+        {
+            ParseDialogueID(dialogue);
 
-    public static void ParseDialogueSteps(Dialogue dialogue)
+            if (dialogue.stageType == StageType.Dialogue)
+            {
+                ParseDialogueSteps(dialogue); //set up dialogue steps with proper speaker objects
+            }
+
+            Dialogues.all.Add(dialogue);
+        }
+    }
+    static void ParseDialogueID(Dialogue dialogue)
     {
-        Debug.Log($"parsing ${dialogue.objectID}" +
-            $"\n Content count: {dialogue.content.Count}");
+        //SET STAGE TYPE
+        if (dialogue.objectID.Substring(14, 1) == "M")
+        {
+            dialogue.stageType = StageType.Memory;
+        }
+        else if (dialogue.objectID.Substring(14, 1) == "P")
+        {
+            dialogue.stageType = StageType.PopUp;
+        }
+
+        //SET QUEST STAGE
+        dialogue.questStage = int.Parse(dialogue.objectID.Substring(11, 2));
+
+        //SET QUEST ID
+        dialogue.questID = dialogue.objectID.Substring(0, 10);
+    }
+    static void ParseDialogueSteps(Dialogue dialogue)
+    {
+        //Debug.Log($"parsing ${dialogue.objectID}" +
+        //    $"\n Content count: {dialogue.content.Count}");
 
         for (int i = 0; i < dialogue.content.Count; i = i + 2)
         {
             string speakerTag = dialogue.content[i];
             Character foundSpeaker = Characters.all.Find((s) => s.dialogueTag.ToLower() == speakerTag.ToLower());
-            DialogueStep step = new() { name = $"{foundSpeaker.name} - step{Mathf.FloorToInt(i/2+1)}" };
-            step.speaker = foundSpeaker;
-            step.text = dialogue.content[i + 1];
-            Debug.Log($"New step {i} - {i + 1}| {step.speaker}: {step.text}");
-            dialogue.dialogueSteps.Add(step);
+            if (foundSpeaker is not null)
+            {
+                DialogueStep step = new() { name = $"{foundSpeaker.name} - step{Mathf.FloorToInt(i / 2 + 1)}" };
+                step.speaker = foundSpeaker;
+                step.text = dialogue.content[i + 1];
+                //Debug.Log($"New step {i} - {i + 1}| {step.speaker}: {step.text}");
+                dialogue.dialogueSteps.Add(step);
+            }
+            else
+            {
+                Debug.LogWarning($"Could not parse dialogue content for {dialogue.questID} stage {dialogue.questStage} because speaker tag \"{speakerTag}\" return null.");
+            }
+
         }
     }
 }
