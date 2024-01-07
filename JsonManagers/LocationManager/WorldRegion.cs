0e644b445bcfcef73776ba6ddfadb36adc31ece0
Renatuscape <rrjonsberg@gmail.com>
2024-01-07 22:36:47 +0100

    Added new world region script to hold region data. May need JSON and expand into parallax management.

---
+++ JsonManagers/LocationManager/WorldRegion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class WorldRegion
+{
+    public string objectID; //REGION#
+    public string name;
+    public int columns;
+    public int rows;
+    public List<WorldLocation> locations = new();
+    public Vector3 defaultStartingPosition = new Vector3(0,0,0);
+}
+
+public static class Regions
+{
+    public static List<WorldRegion> all = new();
+
+    public static WorldRegion FindByID(string searchTerm)
+    {
+        return all.FirstOrDefault(r => r.objectID == searchTerm);
+    }
+}
