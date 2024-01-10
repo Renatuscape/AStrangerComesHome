eaf5e1ad051ecc0c4296fbe84add439d079915ca
Renatuscape <rrjonsberg@gmail.com>
2024-01-10 22:55:34 +0100

    Created separate scroll class to declutter AutoMap class.

---
+++ GameComponents/AutoMap/AutoMapScroller.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class AutoMapScroller
+{
+    float scrollSpeed = 0.05f;
+    public int xCutOff = 6;
+    public int yCutOff = 4;
+
+    Transform map;
+
+    public AutoMapScroller(AutoMap autoMap)
+    {
+        map = autoMap.mapContainer.transform;
+    }
+
+    public void ScrollMapUp()
+    {
+        if (map.position.y < yCutOff)
+        {
+            map.position = new Vector3(
+                map.position.x,
+                map.position.y + scrollSpeed,
+                map.position.z);
+        }
+    }
+    public void ScrollMapDown()
+    {
+        if (map.position.y > yCutOff * -1)
+        {
+            map.position = new Vector3(
+                map.position.x,
+                map.position.y - scrollSpeed,
+                map.position.z);
+        }
+    }
+    public void ScrollMapLeft()
+    {
+        if (map.position.x > xCutOff * -1)
+        {
+            map.position = new Vector3(
+            map.position.x - scrollSpeed,
+            map.position.y,
+            map.position.z);
+        }
+    }
+    public void ScrollMapRight()
+    {
+        if (map.position.x < xCutOff)
+        {
+            map.position = new Vector3(
+            map.position.x + scrollSpeed,
+            map.position.y,
+            map.position.z);
+        }
+    }
+}
