66a187fe7ada7a384dfdc94aa02e3a4eb2a8b9fd
Renatuscape <rrjonsberg@gmail.com>
2024-01-10 22:50:50 +0100

    Refactored code and implemented travelling. New map is now functional!

--- GameComponents/AutoMap/AutoMap.cs
+++ GameComponents/AutoMap/AutoMap.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using static UnityEngine.Rendering.DebugUI.Table;
 
@@ -21,27 +22,42 @@ public class AutoMap : MonoBehaviour
     public List<GameObject> mapMarkers = new();
 
     public TransientDataScript transientData;
-    public AutoMapEdges edgeCreator;
     public AutoMapBuilder mapBuilder;
+    public AutoMapScroller mapScroller;
+    public AutoMapTravelManager travelManager;
 
-    Vector3 enabledPosition;
-    Vector3 disabledPosition;
-    float scrollSpeed;
-    float xCutOff;
-    float yCutOff;
-    Transform map;
+    Vector3 enabledPosition = new Vector3(0, 0, 0);
+    Vector3 disabledPosition = new Vector3(200, 0, 0);
 
-    void Start()
+    public int mapStartX;
+    public int mapEndX;
+    public int mapStartY;
+    public int mapEndY;
+
+    private void Start()
     {
-        edgeCreator = new();
+        mapScroller = new(this);
         mapBuilder = new(this, mapContainer);
-        enabledPosition = new Vector3(0,0,0);
-        disabledPosition = new Vector3(200, 0, 0);
-        scrollSpeed = 0.05f;
+        travelManager = new(this);
 
-        map = mapContainer.transform;
         playerToken.autoMap = this;
+        ToggleEnable(false);
         StartCoroutine(TestRegion());
+    }
+
+    public void ToggleEnable(bool isTrue)
+    {
+        if (isTrue)
+        {
+            transform.position = enabledPosition;
+            GoToPlayerToken();
+        }
+
+        else
+        {
+            transform.position = disabledPosition;
+            mapContainer.transform.localPosition = new Vector3(0, 0, 0);
+        }
     }
 
     IEnumerator TestRegion()
@@ -49,8 +65,19 @@ public class AutoMap : MonoBehaviour
         yield return new WaitForSeconds(1);
         ChangeMap(Regions.FindByID("REGION1"));
     }
+
     private void Update()
     {
+        //TRAVEL LOGIC
+        if (TransientDataScript.GameState == GameState.MapMenu || TransientDataScript.GameState == GameState.Overworld || TransientDataScript.GameState == GameState.JournalMenu)
+        {
+            if (playerToken.transform.localPosition != mapMarker.transform.localPosition && travelManager is not null)
+            {
+                travelManager.Travel();
+            }
+        }
+
+        //SCROLL LOGIC
         if (TransientDataScript.GameState == GameState.MapMenu)
         {
             if (transform.position != enabledPosition)
@@ -61,74 +88,45 @@ public class AutoMap : MonoBehaviour
 
             if (worldPosition.x < LeftTarget.position.x)
             {
-                ScrollMapRight();
+                mapScroller.ScrollMapRight();
             }
             else if (worldPosition.x > RightTarget.position.x)
             {
-                ScrollMapLeft();
+                mapScroller.ScrollMapLeft();
             }
             else if (worldPosition.y > TopTarget.position.y)
             {
-                ScrollMapDown();
+                mapScroller.ScrollMapDown();
             }
 
             else if (worldPosition.y < BottomTarget.position.y)
             {
-                ScrollMapUp();
+                mapScroller.ScrollMapUp();
             }
         }
-        else if (TransientDataScript.GameState != GameState.MapMenu && transform.position != disabledPosition)
+        else //Force this bitch to hide if she hasn't
         {
-            transform.position = disabledPosition;
+            if (transform.position != disabledPosition)
+            {
+                transform.position = disabledPosition;
+            }
         }
     }
 
-    void ScrollMapUp()
+    public void ChangeMap(Region region)
     {
-        if (map.position.y < yCutOff)
-        {
-            map.position = new Vector3(
-                map.position.x,
-                map.position.y + scrollSpeed,
-                map.position.z);
-        }
-    }
-    void ScrollMapDown()
-    {
-        if (map.position.y > yCutOff * -1)
-        {
-            map.position = new Vector3(
-                map.position.x,
-                map.position.y - scrollSpeed,
-                map.position.z);
-        }
-    }
-    void ScrollMapLeft()
-    {
-        if (map.position.x > xCutOff * -1)
-        {
-            map.position = new Vector3(
-                map.position.x - scrollSpeed,
-                map.position.y,
-                map.position.z);
-        }
-    }
-    void ScrollMapRight()
-    {
-        if (map.position.x < xCutOff)
-        {
-            map.position = new Vector3(
-                map.position.x + scrollSpeed,
-               map.position.y,
-                map.position.z);
-        }
-    }
+        transientData.currentRegion = region;
 
-    public void ChangeMap(WorldRegion region, Vector3? startingPosition = null)
-    {
-        xCutOff = region.rows /2 - 2;
-        yCutOff = region.columns / 2 - 6;
-        mapBuilder.ChangeMap(region, startingPosition);
+        mapStartY = region.rows / 2 * -1;
+        mapEndY = region.rows / 2;
+        mapStartX = region.columns / 2 * -1;
+        mapEndX = region.columns / 2;
+
+        mapScroller.xCutOff = mapEndX - 2;
+        mapScroller.yCutOff = mapEndY / 2 - 6;
+
+        mapBuilder.ChangeMap(region);
+        mapContainer.transform.localPosition = new Vector3(0, 0, 0);
     }
 
     public void GoToCoordinates(Vector3 coordinates)
@@ -164,77 +162,5 @@ public class AutoMap : MonoBehaviour
 
         // Ensure the final position is set to the target coordinates
         mapContainer.transform.position = targetPosition;
-    }
-}
-
-public class AutoMapBuilder
-{
-    public AutoMap autoMap;
-
-    float spacing = 1;
-    GameObject mapContainer;
-    public AutoMapBuilder(AutoMap autoMap, GameObject mapContainer)
-    {
-        this.autoMap = autoMap;
-        this.mapContainer = mapContainer;
-    }
-
-    void GenerateMap(int rows, int columns)
-    {
-
-        int rowStartValue = rows / 2 * -1;
-        int columnStartValue = columns / 2 * -1;
-
-        for (int i = rowStartValue; i <= rows / 2; i++)
-        {
-            for (int j = columnStartValue; j <= columns / 2; j++)
-            {
-                Vector3 localPosition = new Vector3(j * spacing, i * spacing, 0);
-                var newTile = UnityEngine.Object.Instantiate(autoMap.tilePrefab);
-                newTile.transform.parent = mapContainer.transform;
-                newTile.transform.localPosition = localPosition;  // Set local position relative to mapContainer
-                newTile.GetComponent<MapTilePrefab>().autoMap = autoMap;
-                newTile.GetComponent<MapTilePrefab>().sprites = autoMap.tileSprites;
-                autoMap.mapTiles.Add(new Vector2Int(i, j), newTile);
-            }
-        }
-
-        autoMap.edgeCreator.CreateEdge(autoMap.mapTiles, rows, columns);
-    }
-
-
-    void GenerateLocationMarkers(WorldRegion region)
-    {
-        foreach (WorldLocation location in region.locations)
-        {
-            Vector3 localPosition = new Vector3(location.mapX, location.mapY, 0);
-            var newMarker = UnityEngine.Object.Instantiate(autoMap.locationMarker, localPosition, Quaternion.identity);
-            newMarker.transform.SetParent(mapContainer.transform);
-            newMarker.transform.localPosition = localPosition;  // Set local position relative to mapContainer
-            newMarker.GetComponent<MapLocationPrefab>().location = location;
-            newMarker.GetComponent<MapLocationPrefab>().transientData = autoMap.transientData;
-            newMarker.GetComponent<MapLocationPrefab>().autoMap = autoMap;
-            autoMap.mapMarkers.Add(newMarker);
-        }
-    }
-    public void ChangeMap(WorldRegion region, Vector3? startingPosition = null)
-    {
-        foreach (var t in autoMap.mapTiles)
-        {
-            UnityEngine.Object.Destroy(t.Value);
-        }
-
-        foreach (var t in autoMap.mapMarkers)
-        {
-            UnityEngine.Object.Destroy(t);
-        }
-
-        autoMap.mapTiles = new();
-        autoMap.mapMarkers = new();
-
-        GenerateMap(region.rows, region.columns);
-        GenerateLocationMarkers(region);
-        autoMap.playerToken.transform.localPosition = startingPosition ?? region.defaultStartingPosition;
-        autoMap.GoToPlayerToken();
     }
 }
