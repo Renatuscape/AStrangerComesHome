b9f9fc67873e07b24b6fbc945586312f5f86da75
Renatuscape <rrjonsberg@gmail.com>
2024-01-14 14:18:13 +0100

    Added class for changing tiles to correct sprite.

---
+++ GameComponents/AutoMap/AutoMapPainter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AutoMapPainter : MonoBehaviour
+{
+    public List<TileSprite> tilePalette;
+
+    public bool FlipTile(Region region, int xCoordinate, int yCoordinate, out TileSprite tile)
+    {
+        string tileID = region.mapLayout[xCoordinate].row[yCoordinate];
+
+        tile = tilePalette.FirstOrDefault((t)=> t.TileID == tileID);
+
+        return tile is not null;
+    }
+
+    [Serializable]
+    public class TileSprite
+    {
+        public string TileID;
+        public bool isUnobstructive;
+        public Sprite sprite;
+    }
+}
