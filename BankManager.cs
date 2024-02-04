8a1ee308c6c56026c9d65e92962e5564f6969077
Renatuscape <rrjonsberg@gmail.com>
2024-02-04 21:13:34 +0100

    Class for managing the bank interface.

---
+++ BankManager.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BankManager : MonoBehaviour
+{
+    public PortraitRenderer portraits;
+    public GameObject bankMenu;
+
+    public GameObject leaveButton;
+    public GameObject exchangeButton;
+    public GameObject loansButton;
+    public GameObject chatButton;
+
+    public GameObject exchangeMenu;
+    public GameObject loansMenu;
+
+    public bool isBankActive;
+
+    public TextMeshProUGUI guilders;
+    public TextMeshProUGUI crowns;
+    public TextMeshProUGUI shillings;
+    public TextMeshProUGUI hellers;
+    public TextMeshProUGUI oldWorldObols;
+
+    public TextMeshProUGUI amountText;
+
+    private void Update()
+    {
+        if (TransientDataScript.GameState != GameState.BankMenu)
+        {
+            isBankActive = false;
+            leaveButton.SetActive(false);
+            exchangeButton.SetActive(false);
+            loansButton.SetActive(false);
+            chatButton.SetActive(false);
+
+            if (TransientDataScript.GameState != GameState.Dialogue)
+            {
+                isBankActive = false;
+                bankMenu.SetActive(false);
+            }
+
+        }
+        else if (TransientDataScript.GameState == GameState.BankMenu && !isBankActive)
+        {
+            isBankActive = true;
+            OpenBankMenu();
+        }
+    }
+
+    public void OpenBankMenu()
+    {
+        TransientDataScript.SetGameState(GameState.BankMenu, name, gameObject);
+
+        bankMenu.SetActive(true);
+        exchangeMenu.SetActive(false);
+        loansMenu.SetActive(false);
+
+        if (TransientDataScript.GetWeekDay() == DayOfWeek.Lunden)
+        {
+            leaveButton.SetActive(true);
+            exchangeButton.SetActive(false);
+            loansButton.SetActive(false);
+            chatButton.SetActive(false);
+        }
+        else if (Player.GetCount("ARC004", "BankManager") > 0)
+        {
+            portraits.gameObject.SetActive(true);
+            portraits.EnableForShop("ARC004");
+            leaveButton.SetActive(true);
+            exchangeButton.SetActive(true);
+            loansButton.SetActive(true);
+            chatButton.SetActive(true);
+        }
+        else
+        {
+            portraits.gameObject.SetActive(true);
+            portraits.EnableForShop("ARC004");
+            leaveButton.SetActive(true);
+            exchangeButton.SetActive(false);
+            loansButton.SetActive(false);
+            chatButton.SetActive(true);
+        }
+
+        UpdateWallet();
+    }
+
+    public void UpdateWallet()
+    {
+        var playerGuilders = Player.GetCount("MIS003-RAR-NN", "BankManager");
+        var playerCrowns = Player.GetCount("MIS002-UNC-NN", "BankManager");
+        var playerShillings = Player.GetCount("MIS001-COM-NN", "BankManager");
+        var playerHellers = Player.GetCount("MIS000-JUN-NN", "BankManager");
+        var playerObols = Player.GetCount("MIS010-COM-NN", "BankManager");
+
+        StartCoroutine(DelayedAdjust(guilders, playerGuilders));
+        StartCoroutine(DelayedAdjust(crowns, playerCrowns));
+        StartCoroutine(DelayedAdjust(shillings, playerShillings));
+        StartCoroutine(DelayedAdjust(hellers, playerHellers));
+        StartCoroutine(DelayedAdjust(oldWorldObols, playerObols));
+    }
+
+    IEnumerator DelayedAdjust(TextMeshProUGUI textMesh, int newNumber)
+    {
+        var textMeshContent = textMesh.text.Split(' ');
+        var currentNumber = int.Parse(textMeshContent[1]);
+
+        while (currentNumber != newNumber)
+        {
+            int difference = Mathf.Abs(newNumber - currentNumber);
+
+            int incrementAmount;
+
+            if (difference > 1000)
+            {
+                incrementAmount = 100;
+            }
+            else
+            {
+                incrementAmount = difference > 15 ? 10 : 1;
+            }
+
+            if (currentNumber < newNumber)
+            {
+                currentNumber += incrementAmount;
+            }
+            else
+            {
+                currentNumber -= incrementAmount;
+            }
+
+            textMesh.text = textMeshContent[0] + " " + currentNumber;
+            StartCoroutine(JumpNumber(textMesh));
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    IEnumerator JumpNumber(TextMeshProUGUI textMesh)
+    {
+        float nudgeAmount = 1f;
+        textMesh.gameObject.transform.position = new Vector3(textMesh.transform.position.x + nudgeAmount, textMesh.transform.position.y + nudgeAmount, textMesh.transform.position.z);
+
+        yield return new WaitForSeconds(0.03f);
+        textMesh.gameObject.transform.position = new Vector3(textMesh.transform.position.x - nudgeAmount, textMesh.transform.position.y - nudgeAmount, textMesh.transform.position.z);
+    }
+
+    public void CloseBankMenu()
+    {
+        TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
+    }
+}
