b2c8f48826695b27184477d6d08598d3edf3c9b9
Renatuscape <rrjonsberg@gmail.com>
2024-02-07 23:43:47 +0100

    Added music management and background loop.

---
+++ AudioManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource backgroundSource;
+
+    void Update()
+    {
+        
+    }
+}
