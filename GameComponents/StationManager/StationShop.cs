e47d3b5bd83d10552c5722a49b0d5160edd639ed
Renatuscape <rrjonsberg@gmail.com>
2024-02-20 01:00:41 +0100

    Fixed issue where references would be empty object that falsely passed the null check.

--- GameComponents/StationManager/StationShop.cs
+++ GameComponents/StationManager/StationShop.cs
@@ -1,48 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StationShop : MonoBehaviour
 {
-    public Shop shopType;
+    public Character shopkeeper;
+    public Shop shop;
+
+    public ShopMenu shopMenu;
+    public string shopkeeperID;
+    public string shopID;
     public TransientDataScript transientData;
     public BoxCollider2D col;
 
-    void Awake()
+    void Start()
     {
-        transientData = GameObject.Find("TransientData").GetComponent<TransientDataScript>();
+        shopMenu = TransientDataCalls.GetShopMenu();
+        transientData = TransientDataCalls.GetTransientData();
         col = GetComponent<BoxCollider2D>();
+        Debug.Log($"Station spawned with shopkeeperID {shopkeeperID} and shopID {shopID}.");
     }
 
     private void Update()
     {
         if (TransientDataScript.CameraView == CameraView.Normal)
-            col.enabled = true;
-        else
-            col.enabled = false;
+        {
+            if (transientData != null)
+            {
+                if (TransientDataScript.CameraView == CameraView.Normal)
+                {
+                    col.enabled = true;
+                }
+
+                else
+                {
+                    col.enabled = false;
+                }
+            }
+            else
+            {
+                transientData = TransientDataCalls.GetTransientData();
+            }
+        }
     }
     public void OnMouseDown()
     {
-        if (TransientDataScript.GameState == GameState.Overworld && TransientDataScript.CameraView == CameraView.Normal)
+        if (NullCheck())
         {
-            transientData.currentShop = shopType;
-            TransientDataScript.SetGameState(GameState.ShopMenu, name, gameObject);
-            //transientData.gameState = GameState.ShopMenu;
-            //Debug.Log(name + " changed GameState to " + GameState.ShopMenu);
+            if (TransientDataScript.GameState == GameState.Overworld && TransientDataScript.CameraView == CameraView.Normal)
+            {
+                shopMenu.SetUpShop(shopkeeper, shop);
+            }
         }
+
     }
 
     public void OnMouseOver()
     {
-        if (TransientDataScript.GameState == GameState.Overworld && TransientDataScript.CameraView == CameraView.Normal)
+        if (NullCheck())
         {
-            transientData.PrintFloatText($"{shopType.shopName}");
+            if (TransientDataScript.GameState == GameState.Overworld && TransientDataScript.CameraView == CameraView.Normal)
+            {
+                transientData.PrintFloatText($"{shop.name}");
+            }
         }
     }
 
     public void OnMouseExit()
     {
         transientData.DisableFloatText();
+    }
+
+    private bool NullCheck()
+    {
+        if (shopkeeper == null || string.IsNullOrEmpty(shopkeeper.objectID))
+        {
+            shopkeeper = Characters.all.FirstOrDefault(c => c.objectID == shopkeeperID);
+            Debug.Log($"Station found shopkeeper: {shopkeeper.name}");
+        }
+        if (shop == null || string.IsNullOrEmpty(shop.objectID))
+        {
+            shop = shopkeeper.GetShop(shopID);
+            Debug.Log($"Shop found: {shop.name}");
+
+        }
+        if (shopMenu == null)
+        {
+            shopMenu = TransientDataCalls.GetShopMenu();
+        }
+
+        return shopkeeper != null && shop != null;
     }
 }
