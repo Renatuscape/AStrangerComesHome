92014c5422611dbc0d549d49ee6e47c575547788
Renatuscape <rrjonsberg@gmail.com>
2024-02-21 18:44:46 +0100

    Removed reference to minimum character level. Created method to check all requirements and restrictions.

--- JsonManagers/QuestManager/AdvancementCheck.cs
+++ JsonManagers/QuestManager/AdvancementCheck.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using static UnityEditor.FilePathAttribute;
+using UnityEngine.Rendering.Universal;
+using UnityEngine;
 
 [System.Serializable]
 public class AdvancementCheck
 {
-    public int minimumCharacterLevel;
     public int minimumDaysPassed;
 
     //The player must have at least this much
@@ -11,4 +13,46 @@ public class AdvancementCheck
 
     //The player must have less than this amount
     public List<IdIntPair> restrictions;
+
+    public bool CheckRequirements(out int minDays)
+    {
+        minDays = minimumDaysPassed;
+
+        if (requirements != null && requirements.Count > 0)
+        {
+            Debug.Log($"Checking requirements.");
+
+            foreach (IdIntPair requirement in requirements)
+            {
+                int amount = Player.GetCount(requirement.objectID, "Choice Requirement Check");
+
+                Debug.Log($"{requirement.amount} {requirement.objectID} is required. Player has {amount}");
+
+                if (amount < requirement.amount)
+                {
+                    return false;
+                }
+
+                Debug.Log("Returned true.");
+            }
+        }
+        else
+        {
+            Debug.Log($"Requirement count was {requirements.Count}. Null or 0 check not passed. Check not run.");
+        }
+
+        if (restrictions != null && restrictions.Count > 0)
+        {
+            foreach (IdIntPair restriction in restrictions)
+            {
+                int amount = Player.GetCount(restriction.objectID, "Choice Restriction Check");
+                if (amount >= restriction.amount)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
