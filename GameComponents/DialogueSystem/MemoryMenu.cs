5cbaeb2525b79312e7ccb747319130bde16db3d9
Renatuscape <rrjonsberg@gmail.com>
2024-02-29 15:58:06 +0100

    Added dialogue printing and image search.

--- GameComponents/DialogueSystem/MemoryMenu.cs
+++ GameComponents/DialogueSystem/MemoryMenu.cs
@@ -1,26 +1,32 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MemoryMenu : MonoBehaviour
 {
-    public DialogueSystem dialogueSystem;
-    public GameObject dialogueContainer;
+    public StorySystem dialogueSystem;
+    public GameObject container;
     public GameObject endButton;
     public Dialogue activeMemory;
 
     public TextMeshProUGUI memoryTitle;
-    public Color shadowColour;
     public Image shadowBox;
     public Image illustration;
     public List<Sprite> illustrations;
 
     public List<GameObject> dialogueList;
+    public GameObject continueButton;
 
-    void Start()
+    public int index;
+    public float columnWidth;
+
+    Color shadowColour;
+
+    void Awake()
     {
         shadowColour = shadowBox.color;
     }
@@ -29,7 +35,7 @@ public class MemoryMenu : MonoBehaviour
     {
         shadowBox.color = new Color(shadowColour.r, shadowColour.g, shadowColour.b, 0);
 
-        foreach (var prefab in  dialogueList)
+        foreach (var prefab in dialogueList)
         {
             Destroy(prefab);
         }
@@ -37,6 +43,9 @@ public class MemoryMenu : MonoBehaviour
 
     public bool InitialiseReader(string questID, int questStage, bool isReadingFromArchive = false)
     {
+        illustration.gameObject.SetActive(false);
+        endButton.gameObject.SetActive(false);
+
         shadowBox.color = new Color(shadowColour.r, shadowColour.g, shadowColour.b, 0);
         var quest = Quests.FindByID(questID);
         activeMemory = quest.GetStage(questStage);
@@ -60,18 +69,72 @@ public class MemoryMenu : MonoBehaviour
         else
         {
             Debug.Log($"Initialising memory reader successfully.");
+
+            illustration.sprite = illustrations.Where(illustrations => activeMemory.objectID.Contains(illustrations.name)).FirstOrDefault();
+            if (illustration.sprite == null)
+            {
+                Debug.Log($"No illustration found in illustrations list with the name {activeMemory.objectID}.");
+            }
+
             memoryTitle.text = activeMemory.topicName ?? "A Stray Memory";
             TransientDataCalls.SetGameState(GameState.Dialogue, name, gameObject);
             gameObject.SetActive(true);
             StartCoroutine(FadeInShadowBox());
+            StartDialogue();
             return true;
         }
     }
 
-    public void ClickEndButton()
+    void StartDialogue()
     {
-        gameObject.SetActive(false);
-        TransientDataCalls.SetGameState(GameState.Overworld, name, gameObject);
+        index = 0;
+        columnWidth = container.GetComponent<RectTransform>().sizeDelta.x - 20;
+        Step();
+    }
+
+    void Step()
+    {
+        if (continueButton != null)
+        {
+            Destroy(continueButton);
+            continueButton = null;
+        }
+
+        container.GetComponent<VerticalLayoutGroup>().enabled = false;
+        var textBox = BoxFactory.CreateTextBox(activeMemory.content[index], columnWidth);
+        textBox.transform.SetParent(container.transform, false);
+        dialogueList.Add(textBox);
+        container.GetComponent<VerticalLayoutGroup>().enabled = true;
+        Canvas.ForceUpdateCanvases();
+
+        index++;
+
+        if (index == activeMemory.content.Count)
+        {
+            End();
+        }
+        else
+        {
+            container.GetComponent<VerticalLayoutGroup>().enabled = false;
+            continueButton = BoxFactory.CreateButton("Continue");
+            continueButton.GetComponent<Button>().onClick.AddListener(() => ClickContinueButton(continueButton));
+            continueButton.transform.SetParent(container.transform, false);
+            container.GetComponent<VerticalLayoutGroup>().enabled = true;
+            Canvas.ForceUpdateCanvases();
+        }
+    }
+
+    void End()
+    {
+        foreach (var prefab in dialogueList)
+        {
+            Destroy(prefab);
+        }
+
+        dialogueList.Clear();
+
+        illustration.gameObject.SetActive(true);
+        endButton.gameObject.SetActive(true);
     }
 
     IEnumerator FadeInShadowBox()
@@ -83,5 +146,19 @@ public class MemoryMenu : MonoBehaviour
             shadowBox.color = new Color(shadowColour.r, shadowColour.g, shadowColour.b, shadowTranslucence);
             yield return new WaitForSeconds(0.1f);
         }
+    }
+
+    public void ClickContinueButton(GameObject button)
+    {
+        Destroy(button);
+        continueButton = null;
+        Step();
+    }
+
+    public void ClickEndButton()
+    {
+        gameObject.SetActive(false);
+        Player.Set(activeMemory.questID, activeMemory.choices[0].advanceTo);
+        TransientDataCalls.SetGameState(GameState.Overworld, name, gameObject);
     }
 }
