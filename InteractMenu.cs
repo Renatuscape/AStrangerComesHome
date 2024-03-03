8f1d0f239a42b28f7eb41e7420417df0208a6d59
Renatuscape <rrjonsberg@gmail.com>
2024-03-03 14:19:27 +0100

    Created interact menu to provide alternative to start chatting with character, and putting one more step between interaction and shopping.

---
+++ InteractMenu.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
+
+public class InteractMenu : MonoBehaviour
+{
+    public static InteractMenu instance;
+    public StorySystem storySystem;
+    public MenuSystem menuSystem;
+    public List<GameObject> buttons;
+    public GameObject prefabContainer;
+
+    void Awake()
+    {
+        instance = GetComponent<InteractMenu>();
+    }
+    public void Initialise(Character character, Shop shop = null)
+    {
+        TransientDataCalls.SetGameState(GameState.Dialogue, name, gameObject);
+        Debug.Log($"Initialising interact menu with {character.name}");
+
+        buttons = new();
+
+        if (character != null && !string.IsNullOrEmpty(character.objectID))
+        {
+            PrintChatButton(character);
+
+            if (shop == null || string.IsNullOrEmpty(shop.objectID))
+            {
+                FindViableShops(character);
+            }
+            else
+            {
+                PrintShopButton(character, shop);
+            }
+        }
+
+        gameObject.SetActive(true);
+        PrintLeaveButton();
+    }
+
+    void PrintChatButton(Character character)
+    {
+        var chatButton = GetButton($"Chat with {character.name}");
+        chatButton.GetComponent<Button>().onClick.AddListener(() => storySystem.OpenTopicMenu(character.objectID));
+    }
+
+    void FindViableShops(Character character)
+    {
+        foreach (var shop in character.shops)
+        {
+            if (shop.CheckRequirements())
+            {
+                var isReadyToShop = PrintShopButton(character, shop);
+
+                if (!isReadyToShop)
+                {
+                    break; //Do not print multiple warnings about speaking to your friend or introducing yourself
+                }
+            }
+        }
+    }
+
+    bool PrintShopButton(Character character, Shop shop)
+    {
+        if (character.type == CharacterType.Arcana && Player.GetCount(character.objectID, name) < 1)
+        {
+            var shopButton = GetButton($"Speak with your old friend before shopping.");
+            shopButton.GetComponent<Button>().onClick.RemoveAllListeners();
+            return false;
+        }
+        else if (character.type == CharacterType.Unique && Player.GetCount(character.objectID, name) < 1)
+        {
+            var shopButton = GetButton($"Introduce yourself before shopping.");
+            shopButton.GetComponent<Button>().onClick.RemoveAllListeners();
+            return false;
+        }
+        else
+        {
+            var shopButton = GetButton($"Shop at {shop.name}");
+            shopButton.GetComponent<Button>().onClick.AddListener(() => menuSystem.shopMenu.SetUpShop(character, shop));
+            return true;
+        }
+
+    }
+
+    void PrintLeaveButton()
+    {
+        var leaveButton = GetButton($"Leave");
+        leaveButton.transform.SetParent(prefabContainer.transform);
+        buttons.Add(leaveButton);
+
+        leaveButton.GetComponent<Button>().onClick.AddListener(() => TransientDataCalls.SetGameState(GameState.Overworld, name, gameObject));
+    }
+
+    GameObject GetButton(string buttonText)
+    {
+        var button = BoxFactory.CreateButton(buttonText);
+        button.transform.SetParent(prefabContainer.transform);
+        buttons.Add(button);
+        button.GetComponent<Button>().onClick.AddListener(() => CloseInteractMenu());
+
+        return button;
+    }
+
+    public void CloseInteractMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        foreach (var button in buttons)
+        {
+            Destroy(button.gameObject);
+        }
+
+        buttons.Clear();
+    }
+
+    public static void Open(Character character, Shop shop = null)
+    {
+        if (instance == null)
+        {
+            Debug.LogWarning("Instance of InteractMenu was null.");
+        }
+        else
+        {
+            if (shop == null)
+            {
+                instance.Initialise(character);
+            }
+            else
+            {
+                instance.Initialise(character, shop);
+            }
+        }
+    }
+}
