6c01b04ebbd92b0459caa15b86f3ad0fcee5d2ad
Renatuscape <rrjonsberg@gmail.com>
2024-03-06 23:25:51 +0100

    Added Secret Alchemy method for checking recipes while experimenting.

--- JsonManagers/RecipeManager/Recipe.cs
+++ JsonManagers/RecipeManager/Recipe.cs
@@ -105,4 +105,184 @@ public static class Recipes
             return new IdIntPair() { objectID = "MAT030-JUN-NN", amount = 1 };
         }
     }
+
+    public static List<string> AttemptSecretAlchemy(Recipe attemptedRecipe, List<ItemIntPair> ingredientsIn)
+    {
+        List<string> alchemyLog = new();
+        List<ItemIntPair> recipeIngredients = new List<ItemIntPair>();
+        bool isAcolyte = false; 
+        bool isJourneyman = false; 
+        bool isExpert = false; 
+        bool isMaster = false; 
+        int secretAlchemy = Player.GetCount("ALC001", "Recipes, AttemptSecretAlchemy()");
+
+        if (secretAlchemy >= 8)
+        {
+            isMaster = true;
+        }
+        if (secretAlchemy >= 6)
+        {
+            isExpert = true;
+        }
+        if (secretAlchemy >= 4)
+        {
+            isJourneyman = true;
+        }
+        if (secretAlchemy >= 2)
+        {
+            isAcolyte = true;
+        }
+
+        foreach (var entry in attemptedRecipe.ingredients)
+        {
+            Item item = Items.FindByID(entry.objectID);
+
+            recipeIngredients.Add(new() { item = item, count = entry.amount });
+        }
+
+        ItemIntPair catalystIn = ingredientsIn.Where(entry => entry.item.type == ItemType.Catalyst).FirstOrDefault();
+        ItemIntPair catalystRecipe = recipeIngredients.Where(entry => entry.item.type == ItemType.Catalyst).FirstOrDefault();
+
+        ItemIntPair plantIn = ingredientsIn.Where(entry => entry.item.type == ItemType.Plant).FirstOrDefault();
+        ItemIntPair plantRecipe = recipeIngredients.Where(entry => entry.item.type == ItemType.Plant).FirstOrDefault();
+
+        var materialIngredientsIn = ingredientsIn.Where(i => i.item.type != ItemType.Catalyst && i.item.type != ItemType.Plant).ToList();
+        var materialIngredientsRecipe = recipeIngredients.Where(i => i.item.type != ItemType.Catalyst && i.item.type != ItemType.Plant).ToList();
+
+        if (catalystIn != null && catalystIn.item.objectID == catalystRecipe.item.objectID)
+        {
+
+            if (catalystIn.count > catalystRecipe.count)
+            {
+                alchemyLog.Add("The amount of catalyst is too high.");
+            }
+            else if (catalystIn.count < catalystRecipe.count)
+            {
+                alchemyLog.Add("The amount of catalyst is too low.");
+            }
+        }
+        else
+        {
+            alchemyLog.Add("The catalyst appears to be incorrect.");
+        }
+
+        if (plantIn != null && plantIn.item.objectID == plantRecipe.item.objectID)
+        {
+            if (isAcolyte)
+            {
+                if (plantIn.count > plantRecipe.count)
+                {
+                    alchemyLog.Add("I need less plant matter for the infusion.");
+                }
+                else if (plantIn.count < plantRecipe.count)
+                {
+                    alchemyLog.Add("I need more plant matter for the infusion.");
+                }
+            }
+        }
+        else
+        {
+            alchemyLog.Add("The choice of plant for the infusion appears to be incorrect.");
+        }
+
+        if (isAcolyte && !isMaster)
+        {
+            foreach (var material in materialIngredientsIn)
+            {
+                if (!materialIngredientsRecipe.Contains(material))
+                {
+                    alchemyLog.Add("At least one ingredient does not match the recipe.");
+                    break;
+                }
+            }
+        }
+
+        if (isJourneyman)
+        {
+            if (ingredientsIn.Count > recipeIngredients.Count)
+            {
+                alchemyLog.Add("There are too many types of ingredients.");
+            }
+            else if (ingredientsIn.Count < recipeIngredients.Count)
+            {
+                alchemyLog.Add("There are too few types of ingredients.");
+            }
+
+            foreach (var material in materialIngredientsIn)
+            {
+                if (materialIngredientsRecipe.Contains(material))
+                {
+                    alchemyLog.Add("At least one ingredient matches the recipe.");
+                    break;
+                }
+            }
+        }
+
+        if (isExpert)
+        {
+
+            List<ItemType> wrongTypes = new();
+
+            foreach (var material in materialIngredientsIn)
+            {
+                var type = material.item.type;
+
+                if (materialIngredientsRecipe.Where(m => m.item.type == type).ToList().Count == 0)
+                {
+                    wrongTypes.Add(type);
+                }
+            }
+
+            foreach (var type in wrongTypes)
+            {
+                Debug.Log($"This recipe does not require any materials of type {type}.");
+            }
+        }
+
+        if (isMaster)
+        {
+            int count = 0;
+            List<ItemIntPair> matchedMaterial = new();
+
+            foreach (var material in materialIngredientsIn)
+            {
+                if (!materialIngredientsRecipe.Contains(material))
+                {
+                    count++;
+                }
+                else
+                {
+                    matchedMaterial.Add(material);
+                }
+            }
+            if (count == 1)
+            {
+                alchemyLog.Add($"{count} material did not match.");
+            }
+            if (count > 1)
+            {
+                alchemyLog.Add($"{count} materials were not a match.");
+            }
+
+            foreach (var material in matchedMaterial)
+            {
+                var recipeMatch = materialIngredientsRecipe.Where(m => m.item.objectID == material.item.objectID).FirstOrDefault();
+
+                if (material.count > recipeMatch.count)
+                {
+                    alchemyLog.Add($"There was too much of one material.");
+                }
+                else if (material.count < recipeMatch.count)
+                {
+                    alchemyLog.Add($"There was too little of one material.");
+                }
+            }
+        }
+
+        if (alchemyLog.Count == 0)
+        {
+            alchemyLog.Add($"I found no mistakes in the recipe. Either the formula is correct, or I am not skilled enough at Secret Alchemy.");
+        }
+        return alchemyLog;
+    }
 }
