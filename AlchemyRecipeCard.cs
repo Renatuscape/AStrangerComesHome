3dd02b802910efbd197737b7b394ae54ef4eb99b
Renatuscape <rrjonsberg@gmail.com>
2024-03-09 15:32:31 +0100

    Created class for displaying a pinned recipe that is draggable and closable.

---
+++ AlchemyRecipeCard.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AlchemyRecipeCard : MonoBehaviour, IDragHandler
+{
+    [SerializeField]
+    private RectTransform dragRectTransform;
+    public TextMeshProUGUI recipeTitle;
+    public GameObject prefabContainer;
+    public List<GameObject> prefabs;
+
+    public void Initialise(Recipe recipe)
+    {
+        ClearPrefabs();
+        gameObject.SetActive(true);
+        recipeTitle.text = recipe.name;
+
+        foreach (IdIntPair ingredient in recipe.ingredients)
+        {
+            var prefab = BoxFactory.CreateItemRow(Items.FindByID(ingredient.objectID), ingredient.amount);
+            prefab.transform.SetParent(prefabContainer.transform, false);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void CloseCard()
+    {
+        ClearPrefabs();
+        gameObject.SetActive(false);
+    }
+
+    void ClearPrefabs()
+    {
+        foreach (var prefab in prefabs)
+        {
+            Destroy(prefab);
+        }
+
+        prefabs.Clear();
+    }
+
+    private void OnDisable()
+    {
+        ClearPrefabs();
+    }
+}
