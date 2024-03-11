f78575c4117dc13f3ceb5714fa7e911480c44f0c
Renatuscape <rrjonsberg@gmail.com>
2024-03-11 23:39:20 +0100

    Changed Start method to bring card to back on spawn.

--- AlchemyRecipePrefab.cs
+++ AlchemyRecipePrefab.cs
@@ -1,32 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AlchemyRecipePrefab : MonoBehaviour
 {
     public TextMeshProUGUI recipeTitle;
     public Anim_BobLoop bobAnimation;
     public GameObject recipeContent;
+    public Func<Recipe> pinRecipe;
     public float xOffset;
 
-    private void Start()
+    void Start()
     {
         xOffset = Random.Range(-7, 7);
-        var lastChildIndex = transform.parent.transform.childCount - 1;
-        var lastChildObject = transform.parent.transform.GetChild(lastChildIndex).gameObject;
-
-        if (lastChildObject == this)
-        {
-            OffsetContent(0);
-        }
-        else
-        {
-            OffsetContent(xOffset);
-        }
+        BringToBack();
     }
 
-    public void OffsetContent(float offset)
+    void OffsetContent(float offset)
     {
         recipeContent.transform.position = new Vector3(offset, recipeContent.transform.position.y, 0);
     }
@@ -35,11 +28,13 @@ public class AlchemyRecipePrefab : MonoBehaviour
     {
         transform.SetAsLastSibling();
         OffsetContent(0);
+        bobAnimation.enabled = true;
     }
 
     public void BringToBack()
     {
         transform.SetAsFirstSibling();
         OffsetContent(xOffset);
+        bobAnimation.enabled = false;
     }
 }
