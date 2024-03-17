93f7bd0ceac25bd7056144bfd124ba39069bc54b
Renatuscape <rrjonsberg@gmail.com>
2024-03-17 16:58:57 +0100

    Added class for secret alchemy prefab to print recipe information if unlocked.

---
+++ GameComponents/Alchemy/SecretAlchemyPrefab.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SecretAlchemyPrefab : MonoBehaviour
+{
+    public Recipe recipe;
+    public int level;
+
+    public void Initialise()
+    {
+        if (!string.IsNullOrEmpty(recipe.objectID))
+        {
+            level = Player.GetCount(recipe.objectID, name + recipe.objectID );
+        }
+    }
+}
