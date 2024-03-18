c3d23793826fad371008fa980ddaa9384be289be
Renatuscape <rrjonsberg@gmail.com>
2024-03-18 21:52:24 +0100

    Created class to track alchemy progress when a synthesiser is active.

---
+++ GameComponents/Alchemy/AlchemyTracker.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlchemyTracker : MonoBehaviour
+{
+    DataManagerScript dataManager;
+    public float timer;
+    public float tick;
+    public float progressAmount;
+
+    private void OnEnable()
+    {
+        Debug.Log("Enabling Alchemy Tracker.");
+        dataManager = TransientDataCalls.gameManager.dataManager;
+        UpdateValues();
+    }
+
+    private void Update()
+    {
+        if (dataManager.alchemySynthesisers != null && dataManager.alchemySynthesisers.Count > 0)
+        {
+            if (TransientDataScript.IsTimeFlowing())
+            {
+                timer += Time.deltaTime;
+
+                if (timer > tick)
+                {
+                    Progress();
+                }
+            }
+        }
+        else
+        {
+            gameObject.SetActive(false); // Don't use this resource unless it's actually necessary
+        }
+    }
+
+    void Progress()
+    {
+        UpdateValues();
+
+        bool anyActiveSynthesiser = false;
+
+        foreach (SynthesiserData synth in dataManager.alchemySynthesisers)
+        {
+            if (synth.isSynthActive && !synth.isSynthPaused && synth.progressSynth < synth.synthRecipe.requiredProgress)
+            {
+                anyActiveSynthesiser = true;
+                synth.progressSynth += progressAmount;
+            }
+        }
+
+        if (!anyActiveSynthesiser)
+        {
+            Debug.Log("No active synthesiser was found. Disabling Alchemy Tracker.");
+            gameObject.SetActive(false);
+        }
+    }
+
+    void UpdateValues()
+    {
+        timer = 0;
+        tick = 10 - (Player.GetCount("ALC004", name) * 0.5f);
+        progressAmount = 1 + (Player.GetCount("ALC003", name) * 0.2f);
+    }
+}
