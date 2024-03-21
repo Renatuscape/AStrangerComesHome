557f56634d30f8a1ea3680e24d3fd795a206cd89
Renatuscape <rrjonsberg@gmail.com>
2024-03-21 21:04:24 +0100

    Renamed garage manager to garage menu for clarity.

--- GarageMenu.cs
+++ GarageMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GarageManager : MonoBehaviour
+public class GarageMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
