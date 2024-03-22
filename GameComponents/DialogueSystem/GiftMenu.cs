1ec0d020057b17a9ca898a2841ee89f47e961e8e
Renatuscape <rrjonsberg@gmail.com>
2024-03-22 20:11:39 +0100

    added class for managing the gift menu.

---
+++ GameComponents/DialogueSystem/GiftMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftMenu : MonoBehaviour
+{
+    public Character character;
+    public PortraitRenderer portraitRenderer;
+    public void Setup(Character character)
+    {
+        this.character = character;
+        portraitRenderer.EnableForGifting(character);
+        TransientDataCalls.SetGameState(GameState.AlchemyMenu, name, gameObject);
+    }
+}
