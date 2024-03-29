a43ac2e1a3bd852e62638e1919f6d77cc336cab0
Renatuscape <rrjonsberg@gmail.com>
2024-03-29 22:11:45 +0100

    Checks for coach synthesisers and creates new ones when they are unlocked.

---
+++ CoachCabinetMenu.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CoachCabinetMenu : MonoBehaviour
+{
+    public GameObject menuContainer;
+    public GameObject miniSynthPrefab;
+
+    public void OpenCabinet()
+    {
+        int coachSynthesisersUnlocked = Player.GetCount("SCR004-SCR-NN", name);
+
+        if (coachSynthesisersUnlocked > 0)
+        {
+            var synthesisers = TransientDataCalls.gameManager.dataManager.alchemySynthesisers;
+            var coachSynthesisers = synthesisers.Where(s => s.synthesiserID.ToLower().Contains("coach")).ToList();
+
+            if (coachSynthesisers != null && coachSynthesisers.Count == coachSynthesisersUnlocked)
+            {
+                Debug.Log("Synthesisers found matched synthesisers unlocked.");
+            }
+            else if (coachSynthesisers != null && coachSynthesisers.Count < coachSynthesisersUnlocked)
+            {
+                while (coachSynthesisers.Count < coachSynthesisersUnlocked)
+                {
+                    SynthesiserData newSynth = new SynthesiserData() { synthesiserID = $"Coach{coachSynthesisers.Count + 1}" };
+                    synthesisers.Add(newSynth);
+                    coachSynthesisers.Add(newSynth);
+                }
+            }
+            else if (coachSynthesisers == null || coachSynthesisers.Count == 0)
+            {
+                Debug.Log($"No coach synthesisers found. Creating {coachSynthesisersUnlocked} new coach synthesisers.");
+
+                for (int i = 0; i < coachSynthesisersUnlocked; i++)
+                {
+                    SynthesiserData newSynth = new SynthesiserData() { synthesiserID = $"Coach{i + 1}" };
+                    synthesisers.Add(newSynth);
+                    coachSynthesisers.Add(newSynth);
+
+                    Debug.Log($"Synthesiser with ID Coach{i + 1} created.");
+                }
+            }
+
+            PrintSynthesisers(coachSynthesisers);
+        }
+        else
+        {
+            Debug.Log("No coach synthesisers unlocked.");
+        }
+    }
+
+    void PrintSynthesisers(List<SynthesiserData> synthesisers)
+    {
+        if (synthesisers != null && synthesisers.Count > 0)
+        {
+            foreach (SynthesiserData synthData in synthesisers)
+            {
+                GameObject miniSynth = Instantiate(miniSynthPrefab);
+
+                miniSynth.transform.SetParent(menuContainer.transform, false);
+
+                var script = miniSynth.GetComponent<AlchemyMiniSynth>();
+
+                script.Initialise(synthData);
+            }
+        }
+    }
+}
