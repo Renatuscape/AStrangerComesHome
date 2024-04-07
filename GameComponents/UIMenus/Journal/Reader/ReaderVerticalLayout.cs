a540bf46cb620eead39511b8665e771197a5eabe
Renatuscape <rrjonsberg@gmail.com>
2024-04-07 16:09:36 +0200

    Vertical layout builder added. Prints pages as designed in JSON regardless of overflow.

---
+++ GameComponents/UIMenus/Journal/Reader/ReaderVerticalLayout.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ReaderVerticalLayout : MonoBehaviour
+{
+    public BookReader reader;
+    public GameObject pageA;
+    public GameObject pageB;
+
+    public TextMeshProUGUI pageNumberL;
+    public TextMeshProUGUI pageNumberR;
+
+    List<Page> pages;
+    int pageIndex;
+
+    Book prevBook;
+
+    internal void Initialise(Book book)
+    {
+        Debug.Log("Vertical reader has been initialised. Attempting to set active.");
+
+        gameObject.SetActive(true);
+
+        if (prevBook != book)
+        {
+            pageIndex = 0;
+            prevBook = book;
+        }
+
+        pages = book.pages;
+        PrintPage();
+    }
+
+    void PrintPage()
+    {
+        reader.CleanReader();
+        pageNumberL.text = $"{pageIndex + 1}";
+        pageNumberR.text = $"{pageIndex + 2}";
+
+        if (pages != null && pages.Count > 0)
+        {
+            foreach (var text in pages[pageIndex].text)
+            {
+                var prefab = reader.PrintLine(text, pageA);
+            }
+
+            if (pages.Count > pageIndex + 1)
+            {
+                if (pages[pageIndex + 1].text != null && pages[pageIndex + 1].text.Count > 0)
+                {
+                    foreach (var text in pages[pageIndex + 1].text)
+                    {
+                        var prefab = reader.PrintLine(text, pageB);
+                    }
+                }
+            }
+        }
+    }
+
+    public void BtnForward()
+    {
+        pageIndex += 2;
+
+        if (pageIndex >= pages.Count)
+        {
+            pageIndex = 0;
+        }
+
+        PrintPage();
+    }
+
+    public void BtnBack()
+    {
+        pageIndex -= 2;
+
+        if (pageIndex < 0)
+        {
+            pageIndex = 0;
+        }
+
+        PrintPage();
+    }
+}
