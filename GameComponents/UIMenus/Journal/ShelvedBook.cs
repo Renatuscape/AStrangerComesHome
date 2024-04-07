ec86c4839b80d187223c78639643a51c293882f5
Renatuscape <rrjonsberg@gmail.com>
2024-04-07 16:06:50 +0200

    Moved to separate file from CollectionsBooks

---
+++ GameComponents/UIMenus/Journal/ShelvedBook.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShelvedBook : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public CollectionsBooks collectionsPage;
+    public Item bookItem;
+    public Book bookContent;
+    public bool isSelected;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (isSelected)
+        {
+            if (bookContent != null)
+            {
+                collectionsPage.reader.Initialise(bookContent);
+            }
+            else
+            {
+                TransientDataCalls.PushAlert("Nothing interesting in here.");
+            }
+        }
+        else
+        {
+            collectionsPage.SelectBook(this);
+            isSelected = true;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TransientDataCalls.PrintFloatText($"{bookItem.name}");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TransientDataCalls.DisableFloatText();
+        isSelected = false;
+    }
+
+    internal void Initialise(CollectionsBooks collectionsBooks, Item item)
+    {
+        bookItem = item;
+        collectionsPage = collectionsBooks;
+        bookContent = Books.FindByItemID(item.objectID);
+
+        if (bookContent == null)
+        {
+            Image[] images = transform.Find("ImageContainer").GetComponentsInChildren<Image>();
+
+            foreach (Image image in images)
+            {
+                if (image.gameObject.name != "Shadow")
+                {
+                    image.color = new Color(0.7f, 0.7f, 0.7f);
+                }
+            }
+        }
+    }
+}
