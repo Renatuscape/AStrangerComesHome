1a106d4b48dabbac3e427645097f2bedfb52ff71
Renatuscape <rrjonsberg@gmail.com>
2024-04-07 16:10:20 +0200

    Started on horizontal layout. Class should have scrolling and non-scrolling option.

---
+++ GameComponents/UIMenus/Journal/Reader/ReaderHorizontalLayout.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaderHorizontalLayout : MonoBehaviour
+{
+    public BookReader reader;
+
+    List<Page> pages;
+    int pageIndex;
+    Book prevBook;
+
+    internal void Initialise(Book book)
+    {
+        if (prevBook != book)
+        {
+            pageIndex = 0;
+            prevBook = book;
+        }
+        pages = book.pages;
+
+        gameObject.SetActive(true);
+    }
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
