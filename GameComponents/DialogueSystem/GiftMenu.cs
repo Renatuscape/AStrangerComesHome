50de90c6faddea2452196ed6229153d2858e10c6
Renatuscape <rrjonsberg@gmail.com>
2024-04-09 12:53:03 +0200

    Added close button and ensured that the game object is properly toggled on and off.

--- GameComponents/DialogueSystem/GiftMenu.cs
+++ GameComponents/DialogueSystem/GiftMenu.cs
@@ -6,10 +6,18 @@ public class GiftMenu : MonoBehaviour
 {
     public Character character;
     public PortraitRenderer portraitRenderer;
+    public GameObject giftMenu;
     public void Setup(Character character)
     {
         this.character = character;
         portraitRenderer.EnableForGifting(character);
-        TransientDataCalls.SetGameState(GameState.AlchemyMenu, name, gameObject);
+        gameObject.SetActive(true);
+        TransientDataCalls.SetGameState(GameState.Dialogue, name, gameObject);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        TransientDataCalls.SetGameState(GameState.Overworld, name, gameObject);
     }
 }
