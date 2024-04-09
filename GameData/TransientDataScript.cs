59f1aadf4c858da899f94db18fa476749fa4c57d
Renatuscape <rrjonsberg@gmail.com>
2024-04-09 16:36:31 +0200

    Added method for printing items with rarity colour and additional data.

--- GameData/TransientDataScript.cs
+++ GameData/TransientDataScript.cs
@@ -166,6 +166,48 @@ public static class TransientDataCalls
         }
     }
 
+    public static void PrintFloatEmbellishedItem(Item item, bool printPrice, bool printRarity)
+    {
+        string hexColour = "635550";
+        if (item.rarity == ItemRarity.Common)
+        {
+            hexColour = "63411b";
+        }
+        else if (item.rarity == ItemRarity.Uncommon)
+        {
+            hexColour = "00766a";
+        }
+        else if (item.rarity == ItemRarity.Rare)
+        {
+            hexColour = "2551a9";
+        }
+        else if (item.rarity == ItemRarity.Extraordinary)
+        {
+            hexColour = "772084";
+        }
+        else if (item.rarity == ItemRarity.Mythical)
+        {
+            hexColour = "b72b66";
+        }
+        else if (item.rarity == ItemRarity.Unique)
+        {
+            hexColour = "7d9c00";
+        }
+
+        string value = printPrice ? $"\nValue: {MoneyExchange.CalculateSellPrice(item)}" : "";
+        string rarity = printRarity ? $"\nRarity: {item.rarity}" : "";
+        string content = $"<color=#{hexColour}>{item.name}{value}{rarity}</color>";
+
+        if (NullCheck())
+        {
+            transientData.PrintFloatText(content);
+        }
+        else
+        {
+            Debug.Log("transientData not found.");
+        }
+    }
+
     public static void DisableFloatText()
     {
         if (NullCheck())
