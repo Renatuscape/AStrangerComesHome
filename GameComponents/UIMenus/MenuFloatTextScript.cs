752deb427d2fbda19402207ce727142e011e46fe
Renatuscape <rrjonsberg@gmail.com>
2024-04-09 16:36:16 +0200

    Menu is now disabled after a slight delay that is cancelled if a new call is made, to avoid flickering in menus.

--- GameComponents/UIMenus/MenuFloatTextScript.cs
+++ GameComponents/UIMenus/MenuFloatTextScript.cs
@@ -10,10 +10,13 @@ public class MenuFloatTextScript : MonoBehaviour
     public TransientDataScript transientData;
     public TextMeshProUGUI floatText;
     public Image backPanel;
+    bool disable;
+    float disableTimer;
+    float disableTick;
 
     void Awake()
     {
-        DisableFloatText();
+        Disable();
         transientData = GameObject.Find("TransientData").GetComponent<TransientDataScript>();
     }
 
@@ -29,20 +32,45 @@ public class MenuFloatTextScript : MonoBehaviour
         {
             DisableFloatText();
         }
+
+        if (disable)
+        {
+            disableTimer += Time.deltaTime;
+
+            if (disableTimer > disableTick)
+            {
+                Disable();
+            }
+        }
     }
 
     public void PrintFloatText(string content)
     {
-        backPanel.enabled = true;
-        floatText.text = content;
+        disableTick = 5;
+        disableTimer = 0;
+        disable = true;
+
         Canvas.ForceUpdateCanvases();
         floatText.transform.parent.GetComponent<VerticalLayoutGroup>().enabled = false;
+
+        backPanel.enabled = true;
+        floatText.text = content;
+
         floatText.transform.parent.GetComponent<VerticalLayoutGroup>().enabled = true;
+        Canvas.ForceUpdateCanvases();
     }
 
     public void DisableFloatText()
     {
+        disableTimer = 0;
+        disableTick = 0.2f;
+        disable = true;
+    }
+
+    void Disable()
+    {
         backPanel.enabled = false;
         floatText.text = " ";
+        disable = false;
     }
 }
