da98733bcb79fa427e593ab34798deb77ce45d63
Renatuscape <rrjonsberg@gmail.com>
2024-04-11 16:15:06 +0200

    Moved instantiation logic to BoxFactory. UpgradePrefab renamed to UpgradeIcon and moved to separate file in the BoxFactory directory.

--- GarageMenu.cs
+++ GarageMenu.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GarageMenu : MonoBehaviour
 {
+    public Character character;
     public GameObject prefabContainer;
-    public GameObject upgradePrefab;
     public List<GameObject> prefabList;
     void Start()
     {
@@ -16,80 +14,13 @@ public class GarageMenu : MonoBehaviour
 
     public void Initialise(Character character)
     {
+        this.character = character;
+
         foreach (Upgrade upgrade in Upgrades.all)
         {
-            var prefab = Instantiate(upgradePrefab);
+            var prefab = BoxFactory.CreateUpgradeIcon(upgrade, true, true);
             prefab.transform.SetParent(prefabContainer.transform, false);
             prefabList.Add(prefab);
-
-            prefab.AddComponent<UpgradePrefab>();
-            var script = prefab.GetComponent<UpgradePrefab>();
-
-            script.Setup(upgrade, true, true);
         }
-    }
-}
-
-public class UpgradePrefab : MonoBehaviour
-{
-    public Upgrade upgrade;
-    public TextMeshProUGUI priceText;
-    public TextMeshProUGUI levelText;
-
-    public int level;
-    public int price;
-
-    bool showPrice = false;
-    bool showLevel = false;
-
-    public void Setup(Upgrade upgrade, bool showLevel, bool showPrice)
-    {
-        this.upgrade = upgrade;
-        this.showLevel = showLevel;
-        this.showPrice = showPrice;
-
-        var imageComponent = gameObject.GetComponent<Image>();
-        imageComponent.sprite = upgrade.sprite;
-
-        if (!showLevel)
-        {
-            levelText.gameObject.SetActive(false);
-        }
-
-        if (!showPrice)
-        {
-            priceText.gameObject.SetActive(false);
-        }
-
-        if (showPrice || showLevel)
-        {
-            UpdateText();
-        }
-    }
-
-    public void UpdateText()
-    {
-        level = Player.GetCount(upgrade.objectID, name);
-
-        if (showLevel)
-        {
-            SetLevelText();
-        }
-
-        if (showPrice)
-        {
-            SetPriceTag();
-        }
-    }
-
-    public void SetLevelText()
-    {
-        levelText.text = level.ToString();
-    }
-    public void SetPriceTag()
-    {
-        price = upgrade.basePrice + (level * upgrade.basePrice * 5);
-
-        priceText.text = price.ToString();
     }
 }
