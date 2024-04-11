99893c530280748450decc8c1343ac00d8f8fdb1
Renatuscape <rrjonsberg@gmail.com>
2024-04-11 16:15:28 +0200

    UpgradePrefab renamed to UpgradeIcon and moved to BoxFactory directory.

---
+++ BoxFactory/UpgradeIcon.cs
@@ -0,0 +1,67 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeIcon : MonoBehaviour
+{
+    public Upgrade upgrade;
+    public TextMeshProUGUI priceText;
+    public TextMeshProUGUI levelText;
+
+    public int level;
+    public int price;
+
+    bool showPrice = false;
+    bool showLevel = false;
+
+    public void Setup(Upgrade upgrade, bool showLevel, bool showPrice)
+    {
+        this.upgrade = upgrade;
+        this.showLevel = showLevel;
+        this.showPrice = showPrice;
+
+        var imageComponent = gameObject.GetComponent<Image>();
+        imageComponent.sprite = upgrade.sprite;
+
+        if (!showLevel)
+        {
+            levelText.gameObject.SetActive(false);
+        }
+
+        if (!showPrice)
+        {
+            priceText.gameObject.SetActive(false);
+        }
+
+        if (showPrice || showLevel)
+        {
+            UpdateText();
+        }
+    }
+
+    public void UpdateText()
+    {
+        level = Player.GetCount(upgrade.objectID, name);
+
+        if (showLevel)
+        {
+            SetLevelText();
+        }
+
+        if (showPrice)
+        {
+            SetPriceTag();
+        }
+    }
+
+    public void SetLevelText()
+    {
+        levelText.text = level.ToString();
+    }
+    public void SetPriceTag()
+    {
+        price = upgrade.basePrice + (level * upgrade.basePrice * 5);
+
+        priceText.text = price.ToString();
+    }
+}
