988ba57d58919a5bbe5845e699cb8011363455c2
Renatuscape <rrjonsberg@gmail.com>
2024-04-12 12:40:50 +0200

    Moved to separate file. Succcessfully triggers SelectUpgrade in main menu.

---
+++ GameComponents/UIMenus/GarageMenu/GarageUpgradePrefab.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GarageUpgradePrefab : MonoBehaviour, IPointerClickHandler, IPointerExitHandler
+{
+    public GarageMenu garageMenu;
+    public Upgrade upgrade;
+    public bool isClicked;
+
+    public void Initialise(GarageMenu garageMenu, Upgrade upgrade)
+    {
+        this.garageMenu = garageMenu;
+        this.upgrade = upgrade;
+        isClicked = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (isClicked)
+        {
+            garageMenu.SelectUpgrade(upgrade);
+        }
+        else
+        {
+            isClicked = true;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isClicked = false;
+    }
+}
