aef5c0af86cbfa8343bf5f238a2992bd9c70a433
Renatuscape <rrjonsberg@gmail.com>
2024-04-13 12:44:03 +0200

    Now loads async. Static methods for setting up dialogue steps have been moved to separate class.

--- JsonManagers/DialogueManager/DialogueManager.cs
+++ JsonManagers/DialogueManager/DialogueManager.cs
@@ -6,32 +6,37 @@ using UnityEngine.UI;
 using System.Linq;
 using UnityEngineInternal;
 using System;
+using System.Threading.Tasks;
 
 public class DialogueManager : MonoBehaviour
 {
     public List<Dialogue> debugCharacterList = Dialogues.all;
     public bool allObjecctsLoaded = false;
-    public int filesLoaded = 0;
-    public int numberOfFilesToLoad = 1;
+    public int filesLoaded;
+    public int numberOfFilesToLoad;
 
-    void Start()
+    public Task StartLoading()
     {
-        //DialogueTagParser.UpdateTags(GameObject.Find("DataManager").GetComponent<DataManagerScript>());
+        List<Task> loadingTasks = new List<Task>();
+
+        gameObject.SetActive(true);
+        filesLoaded = 0;
+        numberOfFilesToLoad = 0;
 
         var info = new DirectoryInfo(Application.streamingAssetsPath + "/JsonData/Quests/Dialogues/");
         var fileInfo = info.GetFiles();
-        numberOfFilesToLoad = fileInfo.Count();
+
         foreach (var file in fileInfo)
         {
-            if (file.Extension != ".json")
+            if (file.Extension == ".json")
             {
-                numberOfFilesToLoad -= 1;
-            }
-            else
-            {
-                LoadFromJson(Path.GetFileName(file.FullName)); // Pass only the file name
+                numberOfFilesToLoad++;
+                Task loadingTask = LoadFromJsonAsync(Path.GetFileName(file.FullName)); // Pass only the file name
+                loadingTasks.Add(loadingTask);
             }
         }
+
+        return Task.WhenAll(loadingTasks);
     }
 
     [System.Serializable]
@@ -39,13 +44,13 @@ public class DialogueManager : MonoBehaviour
     {
         public Dialogue[] dialogues;
     }
-    public void LoadFromJson(string fileName)
+    public async Task LoadFromJsonAsync(string fileName)
     {
         string jsonPath = Application.streamingAssetsPath + "/JsonData/Quests/Dialogues/" + fileName;
 
         if (File.Exists(jsonPath))
         {
-            string jsonData = File.ReadAllText(jsonPath);
+            string jsonData = await Task.Run(() => File.ReadAllText(jsonPath));
             DialogueWrapper dataWrapper = JsonUtility.FromJson<DialogueWrapper>(jsonData);
 
             if (dataWrapper != null)
@@ -54,7 +59,7 @@ public class DialogueManager : MonoBehaviour
                 {
                     foreach (Dialogue dialogue in dataWrapper.dialogues)
                     {
-                        InitialiseDialogue(dialogue);
+                        DialogueSetup.InitialiseDialogue(dialogue);
                         filesLoaded++;
                     }
 
@@ -81,68 +86,4 @@ public class DialogueManager : MonoBehaviour
         }
     }
 
-    public static void InitialiseDialogue(Dialogue dialogue)
-    {
-        if (string.IsNullOrEmpty(dialogue.objectID))
-        {
-            Debug.Log($"Missing objectID for {dialogue}");
-        }
-        else
-        {
-            ParseDialogueID(dialogue);
-
-            if (dialogue.stageType == StageType.Dialogue)
-            {
-                ParseDialogueSteps(dialogue); //set up dialogue steps with proper speaker objects
-            }
-
-            Dialogues.all.Add(dialogue);
-        }
-    }
-    static void ParseDialogueID(Dialogue dialogue)
-    {
-        //SET STAGE TYPE
-        if (dialogue.objectID.Substring(14, 1) == "M")
-        {
-            dialogue.stageType = StageType.Memory;
-        }
-        else if (dialogue.objectID.Substring(14, 1) == "P")
-        {
-            dialogue.stageType = StageType.PopUp;
-        }
-        else if (dialogue.objectID.Substring(14, 1) == "N")
-        {
-            dialogue.stageType = StageType.Node;
-        }
-
-        //SET QUEST STAGE
-        dialogue.questStage = int.Parse(dialogue.objectID.Substring(11, 2));
-
-        //SET QUEST ID
-        dialogue.questID = dialogue.objectID.Substring(0, 10);
-    }
-    static void ParseDialogueSteps(Dialogue dialogue)
-    {
-        //Debug.Log($"parsing ${dialogue.objectID}" +
-        //    $"\n Content count: {dialogue.content.Count}");
-
-        for (int i = 0; i < dialogue.content.Count; i = i + 2)
-        {
-            string speakerTag = dialogue.content[i];
-            Character foundSpeaker = Characters.all.Find((s) => s.dialogueTag.ToLower() == speakerTag.ToLower());
-            if (foundSpeaker is not null)
-            {
-                DialogueStep step = new() { name = $"{foundSpeaker.name} - step{Mathf.FloorToInt(i / 2 + 1)}" };
-                step.speaker = foundSpeaker;
-                step.text = dialogue.content[i + 1];
-                //Debug.Log($"New step {i} - {i + 1}| {step.speaker}: {step.text}");
-                dialogue.dialogueSteps.Add(step);
-            }
-            else
-            {
-                Debug.LogWarning($"Could not parse dialogue content for {dialogue.questID} stage {dialogue.questStage} because speaker tag \"{speakerTag}\" return null.");
-            }
-
-        }
-    }
 }
