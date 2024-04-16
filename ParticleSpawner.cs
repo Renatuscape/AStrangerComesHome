a9592dcb9690e86569b7466b77436a5f7818cee8
Renatuscape <rrjonsberg@gmail.com>
2024-04-16 22:50:28 +0200

    Generic particle spawner for any type of particle prefab. Allows for multiple spawn targets with one class.

---
+++ ParticleSpawner.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour
+{
+    public List<GameObject> spawnTargets;
+    public List<SpriteRenderer> spawnedParticles;
+    public GameObject particlePrefab;
+    public GameObject particleContainer;
+    public string particleName;
+    public float spawnTime = 3;
+    public float minSpawnTime = 0.2f;
+    public bool pingPongSpawnTime;
+    public bool affectedByPlayerSpeed;
+
+    public int minParticles = 0;
+    public int maxParticles = 3;
+    public float spawnTimer = 0f;
+
+    float randomParticles;
+    public float calculatedSpawnTime;
+
+
+    void FixedUpdate()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (pingPongSpawnTime)
+        {
+            calculatedSpawnTime = Random.Range(minSpawnTime, spawnTimer);
+        }
+        else
+        {
+            calculatedSpawnTime = spawnTime;
+        }
+
+        if (affectedByPlayerSpeed)
+        {
+            calculatedSpawnTime = calculatedSpawnTime - TransientDataCalls.transientData.currentSpeed;
+        }
+
+        if (calculatedSpawnTime < 0.2f) //LIMIT MAX SPAWN
+            calculatedSpawnTime = 0.2f;
+
+        if (spawnTimer >= calculatedSpawnTime)
+        {
+            spawnTimer = 0;
+            SpawnTick();
+        }
+    }
+    void SpawnTick()
+    {
+        if (particleContainer == null)
+        {
+            particleContainer = gameObject;
+        }
+
+        if (spawnTargets == null || spawnTargets.Count == 0)
+        {
+            SpawnParticle(gameObject);
+        }
+        else
+        {
+            foreach (GameObject spawnTarget in spawnTargets)
+            {
+                SpawnParticle(spawnTarget);
+            }
+        }
+    }
+
+    void SpawnParticle(GameObject spawnTarget)
+    {
+        randomParticles = Random.Range(minParticles * Time.timeScale, maxParticles * Time.timeScale); // Random.Range(minParticles, maxParticles);
+
+        if (affectedByPlayerSpeed)
+        {
+            randomParticles = randomParticles * TransientDataCalls.transientData.currentSpeed;
+        }
+
+        for (int i = 0; i < (randomParticles + 1); i++)
+        {
+            var prefab = Instantiate(particlePrefab);
+            prefab.name = particleName;
+
+            prefab.transform.SetParent(particleContainer.transform);
+
+            prefab.transform.position = spawnTarget.transform.position;
+        }
+    }
+}
