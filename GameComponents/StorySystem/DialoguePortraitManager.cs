bf0f0758a00ad5e5789e149914db93841188a431
Renatuscape <rrjonsberg@gmail.com>
2024-04-16 12:11:41 +0200

    Moved all variables and functions that can be static to DialoguePortraitHelper class.

--- GameComponents/StorySystem/DialoguePortraitManager.cs
+++ GameComponents/StorySystem/DialoguePortraitManager.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,45 +10,25 @@ public class DialoguePortraitManager : MonoBehaviour
     public Image spriteRight;
     public PlayerSprite playerSprite;
 
-    float positionClose = 230;
-    float positionNormal = 630;
-    float positionMid = 0;
-    float positionFar = 860;
-    float positionOff = 1600;
-
     //float portraitWidth = 1228;
     //float portraitHeight = 1736;
 
-    float animationTick;
-    float animationTimer;
-
-    bool isAnimating = false;
-
+    private void Start()
+    {
+        DialoguePortraitHelper.portraitManager = this;
+    }
     private void OnEnable()
     {
+        var starterPosition = DialoguePortraitHelper.positionOff;
+
         spriteRight.gameObject.SetActive(false);
-        spriteRight.gameObject.transform.position = new Vector3(positionNormal, spriteRight.gameObject.transform.position.y, spriteRight.gameObject.transform.position.z);
+        spriteRight.gameObject.transform.localPosition = new Vector3(starterPosition, spriteRight.gameObject.transform.localPosition.y, spriteRight.gameObject.transform.localPosition.z);
         spriteLeft.gameObject.SetActive(false);
-        spriteLeft.gameObject.transform.position = new Vector3(positionNormal * -1, spriteLeft.gameObject.transform.position.y, spriteLeft.gameObject.transform.position.z);
+        spriteLeft.gameObject.transform.localPosition = new Vector3(starterPosition * -1, spriteLeft.gameObject.transform.localPosition.y, spriteLeft.gameObject.transform.localPosition.z);
         playerSprite.gameObject.SetActive(false);
-        playerSprite.gameObject.transform.position = new Vector3(positionNormal * -1, playerSprite.gameObject.transform.position.y, playerSprite.gameObject.transform.position.z);
-        
-        isAnimating = false;
-        animationTimer = 0;
+        playerSprite.gameObject.transform.localPosition = new Vector3(starterPosition * -1, playerSprite.gameObject.transform.localPosition.y, playerSprite.gameObject.transform.localPosition.z);
     }
 
-    private void Update()
-    {
-        if (isAnimating)
-        {
-            animationTimer += Time.deltaTime;
-
-            if (animationTimer > animationTick)
-            {
-                animationTimer = 0;
-            }
-        }
-    }
     public void StartDialogueEvent(DialogueEvent dEvent)
     {
         gameObject.SetActive(true);
@@ -68,62 +46,29 @@ public class DialoguePortraitManager : MonoBehaviour
         }
         else if (dEvent.isLeft)
         {
-            spriteLeft.sprite = dEvent.speaker.sprite; // Replace with call to sprite manager later
+            spriteLeft.sprite = dEvent.speaker.spriteCollection.GetFirstFrameFromEvent(dEvent.spriteID);
             playerSprite.gameObject.SetActive(false);
             spriteObject = spriteLeft.gameObject;
         }
         else
         {
-            spriteRight.sprite = dEvent.speaker.sprite; // Replace with call to sprite manager later
+            spriteRight.sprite = dEvent.speaker.spriteCollection.GetFirstFrameFromEvent(dEvent.spriteID); // dEvent.speaker.sprite; // Replace with call to sprite manager later
             spriteObject = spriteRight.gameObject;
         }
 
         if (spriteObject != null)
         {
-            SetStartPosition(dEvent, spriteObject);
-        }
-    }
+            spriteObject.SetActive(true);
 
-    void SetStartPosition(DialogueEvent dEvent, GameObject spriteContainer)
-    {
-        //OFF-FAR-NOR-CLO-MID
-        float startPosition;
+            if (!string.IsNullOrEmpty(dEvent.startingPlacement))
+            {
+                DialoguePortraitHelper.SetStartPosition(dEvent, spriteObject);
+            }
 
-        if (dEvent.startingPlacement != null && !string.IsNullOrEmpty(dEvent.startingPlacement))
-        {
-            if (dEvent.startingPlacement == "OFF")
+            if (!string.IsNullOrEmpty(dEvent.targetPlacement))
             {
-                startPosition = positionOff;
-            }
-            else if (dEvent.startingPlacement == "FAR")
-            {
-                startPosition = positionFar;
-            }
-            else if (dEvent.startingPlacement == "CLO")
-            {
-                startPosition = positionClose;
-            }
-            else if (dEvent.startingPlacement == "MID")
-            {
-                startPosition = positionMid;
-            }
-            else
-            {
-                startPosition = positionNormal;
+                DialoguePortraitHelper.SetTargetPosition(dEvent, spriteObject);
             }
         }
-        else
-        {
-            startPosition = positionNormal;
-        }
-
-        if (dEvent.isLeft || dEvent.speaker.objectID == "ARC000")
-        {
-            startPosition = startPosition * -1;
-        }
-
-        Debug.Log("Start position for event sprite is " + startPosition);
-        spriteContainer.transform.localPosition = new Vector3(startPosition, spriteContainer.transform.localPosition.y, spriteContainer.transform.localPosition.z);
-        spriteContainer.SetActive(true);
     }
 }
