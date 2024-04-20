bba1f93bb4865976ef200c23a49ebeab5cf65341
Renatuscape <rrjonsberg@gmail.com>
2024-04-20 12:32:25 +0200

    Added max value for book items.

--- GameData/StaticGameValues.cs
+++ GameData/StaticGameValues.cs
@@ -2,6 +2,7 @@
 public static class StaticGameValues
 {
     public static int maxItemValue = 99;
+    public static int maxBookValue = 9;
     public static int maxCharacterValue = 200;
     public static int maxQuestValue = 110; // anything at 100 or above is a completed state
     public static int maxRecipeValue = 1;
