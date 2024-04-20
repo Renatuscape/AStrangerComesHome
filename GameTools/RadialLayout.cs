d60b58efa9ee4a52264bb72095c6be51f4b6d19b
Renatuscape <rrjonsberg@gmail.com>
2024-04-21 00:41:47 +0200

    Removed OnValidate to stop error messages. Enabling delay is no longer necessary and has been removed.

--- GameTools/RadialLayout.cs
+++ GameTools/RadialLayout.cs
@@ -26,17 +26,9 @@ public class RadialLayout : LayoutGroup
     [Range(0f, 360f)]
     public float MinAngle, MaxAngle, StartAngle;
 
-    bool isEnabled = false;
     protected override void OnEnable()
     {
         base.OnEnable();
-        StartCoroutine(UpdateLayoutCoroutine());
-    }
-
-    IEnumerator UpdateLayoutCoroutine()
-    {
-        yield return new WaitForSeconds(1);
-        isEnabled = true;
     }
 
     public override void SetLayoutHorizontal()
@@ -54,46 +46,39 @@ public class RadialLayout : LayoutGroup
         CalculateRadial();
     }
 #if UNITY_EDITOR
-    protected override void OnValidate()
-    {
-        if (enabled)
-        {
-            base.OnValidate();
-            CalculateRadial();
-        }
-    }
+    //protected override void OnValidate()
+    //{
+    //    if (enabled)
+    //    {
+    //        base.OnValidate();
+    //        CalculateRadial();
+    //    }
+    //}
 #endif
     public void CalculateRadial()
     {
-        if (isEnabled)
-        {
-            m_Tracker.Clear();
-            if (transform.childCount == 0)
-                return;
-            float fOffsetAngle = ((MaxAngle - MinAngle)) / (transform.childCount - 1);
+        m_Tracker.Clear();
+        if (transform.childCount == 0)
+            return;
+        float fOffsetAngle = ((MaxAngle - MinAngle)) / (transform.childCount - 1);
 
-            float fAngle = StartAngle;
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                RectTransform child = (RectTransform)transform.GetChild(i);
-                if (child != null)
-                {
-                    //Adding the elements to the tracker stops the user from modifiying their positions via the editor.
-                    m_Tracker.Add(this, child,
-                    DrivenTransformProperties.Anchors |
-                    DrivenTransformProperties.AnchoredPosition |
-                    DrivenTransformProperties.Pivot);
-                    Vector3 vPos = new Vector3(Mathf.Cos(fAngle * Mathf.Deg2Rad), Mathf.Sin(fAngle * Mathf.Deg2Rad), 0);
-                    child.localPosition = vPos * fDistance;
-                    //Force objects to be center aligned, this can be changed however I'd suggest you keep all of the objects with the same anchor points.
-                    child.anchorMin = child.anchorMax = child.pivot = new Vector2(0.5f, 0.5f);
-                    fAngle += fOffsetAngle;
-                }
-            }
-        }
-        else
+        float fAngle = StartAngle;
+        for (int i = 0; i < transform.childCount; i++)
         {
-            Debug.Log("Something tried to call Radial Menu too soon.");
+            RectTransform child = (RectTransform)transform.GetChild(i);
+            if (child != null)
+            {
+                //Adding the elements to the tracker stops the user from modifiying their positions via the editor.
+                m_Tracker.Add(this, child,
+                DrivenTransformProperties.Anchors |
+                DrivenTransformProperties.AnchoredPosition |
+                DrivenTransformProperties.Pivot);
+                Vector3 vPos = new Vector3(Mathf.Cos(fAngle * Mathf.Deg2Rad), Mathf.Sin(fAngle * Mathf.Deg2Rad), 0);
+                child.localPosition = vPos * fDistance;
+                //Force objects to be center aligned, this can be changed however I'd suggest you keep all of the objects with the same anchor points.
+                child.anchorMin = child.anchorMax = child.pivot = new Vector2(0.5f, 0.5f);
+                fAngle += fOffsetAngle;
+            }
         }
     }
 }
