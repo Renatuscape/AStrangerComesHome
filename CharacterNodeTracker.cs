21af5808a516c3d9f5dafa9b7354283e741ea741
Renatuscape <rrjonsberg@gmail.com>
2024-04-27 18:16:40 +0200

    Created node tracker class to better organise methods.

---
+++ CharacterNodeTracker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class CharacterNodeTracker
+{
+    public static List<CharacterNode> allExistingNodes = new();
+    static List<CharacterNode> spawnedCharacterNodes = new();
+    public static void DisableNodeWithFade(string speakerID)
+    {
+        foreach (var cNode in spawnedCharacterNodes)
+        {
+            if (cNode.characterID == speakerID)
+            {
+                cNode.DisableWithFade();
+
+                break;
+            }
+        }
+    }
+
+    public static void UpdateNodesOnDayTick()
+    {
+        foreach (var cNode in allExistingNodes)
+        {
+            if (cNode.updateAtMidnight)
+            {
+                cNode.UpdateAtMidnight();
+            }
+        }
+    }
+
+    public static void ClearCharacterNodes()
+    {
+        spawnedCharacterNodes.Clear();
+        allExistingNodes.Clear();
+    }
+
+    public static bool AddCharacterNode(CharacterNode cNode)
+    {
+        var foundCharacter = spawnedCharacterNodes.FirstOrDefault(c => c.characterID == cNode.characterID);
+
+        if (foundCharacter == null)
+        {
+            spawnedCharacterNodes.Add(cNode);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool CheckIfCharacterExistsInWorld(string charID)
+    {
+        return spawnedCharacterNodes.FirstOrDefault(c => c.characterID == charID) != null;
+    }
+
+    public static void RemoveWorldCharacterFromList(string charID)
+    {
+        var foundCharacter = spawnedCharacterNodes.FirstOrDefault(c => c.characterID == charID);
+        spawnedCharacterNodes.Remove(foundCharacter);
+    }
+}
