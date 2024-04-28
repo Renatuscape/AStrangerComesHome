509cc6c47f7e44cb827529406689c2021952929b
Renatuscape <rrjonsberg@gmail.com>
2024-04-29 00:23:37 +0200

    Now spawns and removes stars depending on the screen alpha. Max stars value is rerolled each day.

--- GameComponents/ParallaxManager/NightSkyController.cs
+++ GameComponents/ParallaxManager/NightSkyController.cs
@@ -9,10 +9,17 @@ public class NightSkyController : MonoBehaviour
     public SpriteRenderer screen;
     public float timeOfDay;
     public float alphaValue;
+    public int starsToSpawn;
+    public GameObject starPrefab;
+    public GameObject starContainer;
+    public List<StarPrefab> starList = new();
+    public int maxStars;
+    public bool rolledForStars;
     void Start()
     {
         screen = GetComponent<SpriteRenderer>();
         dataManager = TransientDataScript.gameManager.dataManager;
+        maxStars = Random.Range(10, 101);
     }
 
     void Update()
@@ -32,15 +39,54 @@ public class NightSkyController : MonoBehaviour
             else if (timeOfDay >= 0.3f && timeOfDay <= 0.4f)
             {
                 alphaValue = Mathf.SmoothStep(1, 0, (timeOfDay - 0.3f) / 0.1f);
+
+                if (!rolledForStars)
+                {
+                    maxStars = Random.Range(10, 101);
+                    rolledForStars = true;
+                }
             }
 
             // Dusk
             else if (timeOfDay >= 0.7f && timeOfDay <= 0.8f)
             {
                 alphaValue = (timeOfDay - 0.7f) / 0.1f;
+                
+                if (rolledForStars)
+                {
+                    rolledForStars = false;
+                }
             }
 
             screen.color = new Color(screen.color.r, screen.color.g, screen.color.b, alphaValue);
+
+            // Interpolate starsToSpawn based on alphaValue
+            float interpolatedStars = Mathf.Lerp(0, maxStars, alphaValue); // Adjust max stars as needed
+            starsToSpawn = (int)interpolatedStars;
         }
+
+        if (starList.Count < starsToSpawn)
+        {
+            var newStar = Instantiate(starPrefab);
+            newStar.transform.SetParent(starContainer.transform, false);
+            newStar.transform.localPosition = new Vector3(Random.Range(-14.8f, 14.8f), Random.Range(5.7f, 8.2f), 0);
+            starList.Add(newStar.GetComponent<StarPrefab>());
+        }
+        else if (starList.Count > starsToSpawn)
+        {
+            var randomStar = starList[Random.Range(0, starList.Count - 1)];
+            starList.Remove(randomStar);
+            randomStar.PutOut();
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var star in starList)
+        {
+            Destroy(star.gameObject);
+        }
+
+        starList.Clear();
     }
 }
