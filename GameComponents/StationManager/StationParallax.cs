efc8d4ce72d41c78e9fd82ec9e4809795b7362e8
Renatuscape <rrjonsberg@gmail.com>
2024-04-28 15:17:41 +0200

    Now manages movement left and right, and destruction. Pushes child nodes to fade in and out.

--- GameComponents/StationManager/StationParallax.cs
+++ GameComponents/StationManager/StationParallax.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class StationParallax : MonoBehaviour
 {
+    public List<ParallaxCluster> clusters = new();
     TransientDataScript transientData;
     Vector3 spawnArea;
 
     public float parallaxMultiplier;
     public float spawnAdjustment = 3;
+    public bool readyToDestroy = false;
+    public int totalClusters;
+    public int clustersReady;
+
+    public bool movingLeft;
+    public bool movingRight;
     private void Awake()
     {
         transientData = GameObject.Find("TransientData").GetComponent<TransientDataScript>();
         spawnArea = new Vector3(-10 - (transientData.currentSpeed * spawnAdjustment), 0, 0);
         transform.position = spawnArea;
+        GetAllClusters();
+        totalClusters = clusters.Count;
     }
 
     // Update is called once per frame
@@ -22,5 +31,75 @@ public class StationParallax : MonoBehaviour
         var parallaxEffect = transientData.currentSpeed * parallaxMultiplier;
 
         transform.position = new Vector3(transform.position.x + parallaxEffect, transform.position.y, transform.position.z);
+
+        if (movingLeft && clustersReady == totalClusters)
+        {
+            transform.position = new Vector3(-28, 0, 0);
+            movingLeft = false;
+            clustersReady = 0;
+
+            foreach (var cluster in clusters)
+            {
+                cluster.StartFadeIn(false);
+            }
+        }
+        else if (movingRight && clustersReady == totalClusters)
+        {
+            transform.position = new Vector3(25, 0, 0);
+            movingRight = false;
+            clustersReady = 0;
+
+            foreach (var cluster in clusters)
+            {
+                cluster.StartFadeIn(false);
+            }
+        }
+        else if (readyToDestroy && clustersReady == totalClusters)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void GetAllClusters()
+    {
+        foreach (var child in transform.GetComponentsInChildren<ParallaxCluster>())
+        {
+            child.GetComponent<ParallaxCluster>().parentStation = this;
+            clusters.Add(child);
+        }
+    }
+    public void MoveLeft()
+    {
+        clustersReady = 0;
+        movingLeft = true;
+        movingRight = false;
+
+        foreach (var cluster in clusters)
+        {
+            cluster.StartFadeOut(true);
+        }
+    }
+
+    public void MoveRight()
+    {
+        clustersReady = 0;
+        movingRight = true;
+        movingLeft = false;
+
+        foreach (var cluster in clusters)
+        {
+            cluster.StartFadeOut(true);
+        }
+    }
+
+    public void RemoveStation()
+    {
+        clustersReady = 0;
+        readyToDestroy = true;
+
+        foreach (var cluster in clusters)
+        {
+            cluster.StartFadeOut(true);
+        }
     }
 }
