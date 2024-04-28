570ad4b2da318e693a7bac9045cac83b70aad088
Renatuscape <rrjonsberg@gmail.com>
2024-04-28 15:17:03 +0200

    Now fades sprites in and out, and tells parent station object when ready to move or destroy.

--- ParallaxCluster.cs
+++ ParallaxCluster.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class ParallaxCluster : MonoBehaviour
 {
+    public List<SpriteRenderer> frontLayerSprites;
+    public List<SpriteRenderer> midLayerSprites;
+    public List<SpriteRenderer> backLayerSprites;
     public List<GameObject> parallaxLayer = new();
+    public StationParallax parentStation;
     public GameObject parallaxFacade;
     public float customOffsetMultiplier;
     public float customMaxOffset;
     public bool hasNoMaxOffset;
+
+    private void Awake()
+    {
+        GetAllSpriteRenderers();
+        StartCoroutine(FadeIn(false));
+    }
     void Start()
     {
         float layerAdjustment = 0;
@@ -40,6 +50,116 @@ public class ParallaxCluster : MonoBehaviour
             }
 
             layerAdjustment += 0.05f;
+        }
+    }
+
+    public void StartFadeOut(bool setComplete)
+    {
+        StartCoroutine(FadeOut(setComplete));
+    }
+
+    public void StartFadeIn(bool setComplete)
+    {
+        StartCoroutine(FadeIn(setComplete));
+    }
+
+    IEnumerator FadeOut(bool setReady)
+    {
+        float alpha = 1;
+
+        while (alpha > -0.5)
+        {
+            alpha -= 0.02f;
+
+            foreach (SpriteRenderer rend in frontLayerSprites)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, alpha + 0.3f);
+            }
+
+            foreach (SpriteRenderer rend in midLayerSprites)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, alpha + 0.18f);
+            }
+
+            foreach (SpriteRenderer rend in backLayerSprites)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, alpha);
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        if (setReady)
+        {
+            parentStation.clustersReady++;
+        }
+    }
+    IEnumerator FadeIn(bool setReady)
+    {
+
+        float alpha = 0;
+
+        while (alpha < 1)
+        {
+            alpha += 0.01f;
+
+            foreach (SpriteRenderer rend in frontLayerSprites)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, alpha + 0.3f);
+            }
+
+            foreach (SpriteRenderer rend in midLayerSprites)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, alpha + 0.18f);
+            }
+
+            foreach (SpriteRenderer rend in backLayerSprites)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, alpha);
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        if (setReady)
+        {
+            parentStation.clustersReady++;
+        }
+    }
+
+    void GetAllSpriteRenderers()
+    {
+        List<SpriteRenderer> allRenderersFound = new();
+
+        foreach (SpriteRenderer child in transform.GetComponentsInChildren<SpriteRenderer>())
+        {
+            allRenderersFound.Add(child);
+
+            foreach (SpriteRenderer grandChild in child.gameObject.transform.GetComponentsInChildren<SpriteRenderer>())
+            {
+                allRenderersFound.Add(grandChild);
+
+                foreach (SpriteRenderer greatGrandChild in grandChild.gameObject.transform.GetComponentsInChildren<SpriteRenderer>())
+                {
+                    allRenderersFound.Add(greatGrandChild);
+                }
+            }
+        }
+
+        foreach (SpriteRenderer rend in allRenderersFound)
+        {
+            if (rend.sortingLayerName.ToLower().Contains("front"))
+            {
+                frontLayerSprites.Add(rend);
+            }
+            else if (rend.sortingLayerName.ToLower().Contains("mid"))
+            {
+                midLayerSprites.Add(rend);
+            }
+            else
+            {
+                backLayerSprites.Add(rend);
+            }
         }
     }
 }
