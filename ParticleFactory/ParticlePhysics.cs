15f4845ad7db2dbd60fcce7972e585ee664202e8
Renatuscape <rrjonsberg@gmail.com>
2024-05-01 23:38:36 +0200

    Adjusted for prefab system. No longer needs to be initiated; set up as prefab for individual particle types.

--- ParticleFactory/ParticlePhysics.cs
+++ ParticleFactory/ParticlePhysics.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ParticlePhysics : MonoBehaviour
@@ -14,52 +13,25 @@ public class ParticlePhysics : MonoBehaviour
     float magnitudeAdjustment = 0.2f;
     float scaleMin = 0.5f;
     float scaleMax = 2f;
-    public void Initiate(ParticleData particleData, string customSortingLayer)
+    void Start()
     {
-        if (rend == null)
-        {
-            rend = GetComponent<SpriteRenderer>();
-        }
 
-        if (customSortingLayer != null )
+        if (settings.randomDrag != 0 )
         {
-            rend.sortingLayerName = customSortingLayer;
-        }
-
-        settings = new()
-        {
-            particleLife = particleData.particleLife,
-            verticalAcceleration = particleData.verticalAcceleration,
-            horizontalAcceleration = particleData.horizontalAcceleration * magnitudeAdjustment,
-            gravity = particleData.gravity * magnitudeAdjustment,
-            adjustForCoachSpeed = particleData.adjustForCoachSpeed,
-            isSpinning = particleData.isSpinning,
-            isSpinningLeft = particleData.isSpinningLeft,
-            isGrowing = particleData.isGrowing,
-            isShrinking = particleData.isShrinking,
-            isFadeDisabled = particleData.isFadeDisabled
-        };
-
-        if (particleData.randomDrag != 0 )
-        {
-            settings.velocity = Random.Range(particleData.velocity - particleData.randomDrag, particleData.velocity);
-        }
-        else
-        {
-            settings.velocity = particleData.velocity;
+            settings.velocity = Random.Range(settings.velocity - settings.randomDrag, settings.velocity);
         }
 
         currentParticleLife = settings.particleLife;
 
         if (settings.isGrowing)
         {
-            scaleMin = Random.Range(0.8f, 1f);
-            scaleMax = Random.Range(1.5f, 2f);
+            scaleMin = Random.Range(0.5f, 1f);
+            scaleMax = Random.Range(1.5f, 2.5f);
         }
 
         if (settings.scatterRange != 0)
         {
-            scatterForce = Random.Range(particleData.scatterRange * -1, particleData.scatterRange) * magnitudeAdjustment;
+            scatterForce = Random.Range(settings.scatterRange * -1, settings.scatterRange) * magnitudeAdjustment;
 
         }
 
@@ -68,63 +40,66 @@ public class ParticlePhysics : MonoBehaviour
 
     void Update()
     {
-        if (TransientDataScript.IsTimeFlowing() && isActivated)
+        if (TransientDataScript.IsTimeFlowing())
         {
-            // VERTICAL MOVEMENT
-            float verticalForce = settings.velocity;
-            settings.velocity -= settings.gravity; // reduce applied force over time
-
-            // HORIZONTAL MOVEMENT
-            float horizontalForce = scatterForce;
-
-            if (scatterForce != 0)
+            if (!settings.disableWithoutSpeed || settings.disableWithoutSpeed && TransientDataScript.transientData.currentSpeed != 0)
             {
-                if (scatterForce > 0)
+                // VERTICAL MOVEMENT
+                float verticalForce = settings.velocity;
+                settings.velocity -= settings.gravity; // reduce applied force over time
+
+                // HORIZONTAL MOVEMENT
+                float horizontalForce = scatterForce;
+
+                if (scatterForce != 0)
                 {
-                    scatterForce -= settings.horizontalAcceleration * 0.05f;
-                }
-                else
-                {
-                    scatterForce += settings.horizontalAcceleration * 0.05f;
-                }
-            }
-
-            if (settings.adjustForCoachSpeed)
-            {
-                horizontalForce += TransientDataScript.transientData.currentSpeed * 0.9f;
-            }
-
-            horizontalForce = horizontalForce * Time.deltaTime;
-            verticalForce = verticalForce * Time.deltaTime;
-
-            currentParticleLife -= Time.deltaTime;
-
-            // SET POSITION
-            transform.position = new Vector3(transform.position.x + horizontalForce, transform.position.y + verticalForce, 0);
-
-
-            // ADJUST APPEARANCE
-            if (!settings.isFadeDisabled || settings.isGrowing)
-            {
-                float lifeRatio = currentParticleLife / settings.particleLife;
-
-                if (!settings.isFadeDisabled)
-                {
-                    float alpha = Mathf.Lerp(0, 1, lifeRatio);
-                    rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, alpha);
+                    if (scatterForce > 0)
+                    {
+                        scatterForce -= settings.horizontalAcceleration * 0.05f;
+                    }
+                    else
+                    {
+                        scatterForce += settings.horizontalAcceleration * 0.05f;
+                    }
                 }
 
-                if (settings.isGrowing)
+                if (settings.adjustForCoachSpeed)
                 {
-                    float scale = Mathf.Lerp(scaleMax, scaleMin, lifeRatio);
-                    transform.localScale = new Vector3(scale, scale, 1);
+                    horizontalForce += TransientDataScript.transientData.currentSpeed * 0.9f;
                 }
-            }
 
-            // CHECK FOR DEATH
-            if (currentParticleLife <= 0)
-            {
-                Destroy(gameObject); // Destroy the particle object
+                horizontalForce = horizontalForce * Time.deltaTime;
+                verticalForce = verticalForce * Time.deltaTime;
+
+                currentParticleLife -= Time.deltaTime;
+
+                // SET POSITION
+                transform.position = new Vector3(transform.position.x + horizontalForce, transform.position.y + verticalForce, 0);
+
+
+                // ADJUST APPEARANCE
+                if (!settings.isFadeDisabled || settings.isGrowing)
+                {
+                    float lifeRatio = currentParticleLife / settings.particleLife;
+
+                    if (!settings.isFadeDisabled)
+                    {
+                        float alpha = Mathf.Lerp(0, 1, lifeRatio);
+                        rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, alpha);
+                    }
+
+                    if (settings.isGrowing)
+                    {
+                        float scale = Mathf.Lerp(scaleMax, scaleMin, lifeRatio);
+                        transform.localScale = new Vector3(scale, scale, 1);
+                    }
+                }
+
+                // CHECK FOR DEATH
+                if (currentParticleLife <= 0)
+                {
+                    Destroy(gameObject); // Destroy the particle object
+                }
             }
         }
     }
