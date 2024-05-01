ae25d0d2cb195891bf9f0797d464ded319bd708e
Renatuscape <rrjonsberg@gmail.com>
2024-05-01 22:24:33 +0200

    Created particle data class to hold particle settings.

---
+++ ParticleFactory/ParticleData.cs
@@ -0,0 +1,18 @@
+﻿[System.Serializable]
+public class ParticleData
+{
+    public string particleID;
+    public float particleLife;
+    public float velocity;
+    public float randomDrag;
+    public float verticalAcceleration;
+    public float horizontalAcceleration;
+    public float gravity;
+    public float scatterRange;
+    public bool adjustForCoachSpeed;
+    public bool isSpinning;
+    public bool isSpinningLeft;
+    public bool isGrowing;
+    public bool isShrinking;
+    public bool isFadeDisabled; // otherwise fade gradually over time
+}
