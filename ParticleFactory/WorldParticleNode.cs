0e0561cc9fdc69f00702ead3a58e43c61f5cd4fc
Renatuscape <rrjonsberg@gmail.com>
2024-05-01 22:23:59 +0200

    Created world particle node for spawning particles.

---
+++ ParticleFactory/WorldParticleNode.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldParticleNode : MonoBehaviour
+{
+    public List<string> particles = new();
+    public string customSortingLayer;
+
+    public bool adjustSpawnWithSpeed = false;
+    public float maxTick;
+    public float minTick;
+    public int minParticles;
+    public int maxParticles;
+    public float tick;
+    public float spawnTimer;
+    private void Start()
+    {
+        tick = Random.Range(minTick, maxTick);
+    }
+
+    private void Update()
+    {
+        if (TransientDataScript.IsTimeFlowing())
+        {
+            spawnTimer += Time.deltaTime;
+
+            if (adjustSpawnWithSpeed)
+            {
+                spawnTimer += TransientDataScript.transientData.currentSpeed * 0.008f;
+            }
+
+            if (spawnTimer > tick)
+            {
+                SpawnEachParticleType(gameObject);
+                spawnTimer = 0;
+                tick = Random.Range(minTick, maxTick);
+            }
+        }
+    }
+
+    void SpawnEachParticleType(GameObject container)
+    {
+        foreach (string particle in particles)
+        {
+            int toSpawn = Random.Range(minParticles, maxParticles + 1);
+            int spawned = 0;
+
+            while (spawned < toSpawn)
+            {
+                var newParticle = ParticleFactory.SpawnWorldParticle(particle);
+
+                if (newParticle != null)
+                {
+                    newParticle.transform.SetParent(container.transform, false);
+                }
+                spawned++;
+            }
+        }
+    }
+}
