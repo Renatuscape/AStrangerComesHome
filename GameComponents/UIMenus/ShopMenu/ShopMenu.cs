2de42be025ac5bd5dba3d09e094e8a31c958812e
Renatuscape <rrjonsberg@gmail.com>
2024-05-05 01:34:03 +0200

    Removed old methods. Now sets up containers with ShopItemPrefab class.

--- GameComponents/UIMenus/ShopMenu/ShopMenu.cs
+++ GameComponents/UIMenus/ShopMenu/ShopMenu.cs
@@ -17,25 +17,37 @@ public class ShopMenu : MonoBehaviour
     public GameObject clearanceNotice;
     public GameObject shopItemPrefab;
 
-    public List<Item> shopInventory;
-    public List<GameObject> spawnedPrefabs;
+    public List<GameObject> buyFromShopPrefabs;
+    public List<GameObject> sellFromPlayerPrefabs;
+
+    public PageinatedContainer buyFromShopMenu;
+    public PageinatedContainer sellFromPlayerMenu;
+
+    public Button btnBuy;
+    public Button btnSell;
+    public Button btnLeave;
+
+    public Toggle toggleMultiple;
+
+    public GameObject itemInfoCard;
+    public GameObject itemDescriptionCard;
+    public TextMeshProUGUI itemName;
+    public TextMeshProUGUI itemPrice;
+    public TextMeshProUGUI itemRarity;
+    public TextMeshProUGUI itemDescription;
 
     void Awake()
     {
         profitMargin = 2f;
         dataManager = GameObject.Find("DataManager").GetComponent<DataManagerScript>();
+
+        btnBuy.onClick.AddListener(() => BtnBuyFromShop());
+        btnSell.onClick.AddListener(() => BtnSellFromPlayer());
+        btnLeave.onClick.AddListener(() => BtnLeave());
+
+        itemInfoCard.SetActive(false);
+        itemDescriptionCard.SetActive(false);
     }
-
-    private void OnDisable()
-    {
-        foreach (var item in spawnedPrefabs)
-        {
-            Destroy(item);
-        }
-
-        TransientDataScript.DisableFloatText();
-    }
-
     public void SetUpShop(Character shopkeeper, Shop shop)
     {
         SyncSkills();
@@ -46,77 +58,151 @@ public class ShopMenu : MonoBehaviour
         this.shopkeeper = shopkeeper;
         activeShop = shop;
 
-        EnablePortraits();
+        portraitRenderer.EnableForShop(shopkeeper.objectID);
 
         if (activeShop != null)
         {
-            //CALCULATE SHOP RATE
-            if (activeShop.saleDay == TransientDataScript.GetWeekDay())
+            // Set clearance sign
+            if (activeShop.saleDay == (int)TransientDataScript.GetWeekDay())
             {
                 clearanceNotice.SetActive(true);
-                profitMargin = activeShop.clearanceMargin - (judgement * 0.5f);
             }
             else
             {
                 clearanceNotice.SetActive(false);
-                profitMargin = activeShop.profitMargin - judgement;
             }
 
-            if (profitMargin < 0)
+            // CONTAINERS
+            sellFromPlayerPrefabs = sellFromPlayerMenu.Initialise(shop.GetSellList(), true, true, true);
+            buyFromShopPrefabs = buyFromShopMenu.Initialise(shop.GetBuyList(), false, true, true);
+
+            if (shop.buysItems)
             {
-                profitMargin = 0;
+                foreach (var item in sellFromPlayerPrefabs)
+                {
+                    var uiData = item.GetComponent<ItemUiData>();
+                    uiData.printRarity = true;
+                    var shopItemScript = item.AddComponent<ShopItemPrefab>();
+                    shopItemScript.Initialise(uiData.item, this, true);
+                }
             }
 
-        }
-    }
-
-
-    void EnablePortraits()
-    {
-        portraitRenderer.gameObject.SetActive(true);
-        portraitRenderer.EnableForShop(shopkeeper.objectID);
-    }
-
-    public void AttemptPurchase(Item item, int itemCost)
-    {
-        var inventoryAmount = Player.GetCount(item.objectID, name);
-
-        if (inventoryAmount < item.maxValue)
-        {
-            var purchase = MoneyExchange.Purchase(itemCost);
-
-            if (purchase)
+            if (shop.doesNotSell)
             {
-                Player.Add(item.objectID);
-                AudioManager.PlayUISound("handleCoins");
-                Debug.Log($"{activeShop.sucessfulPurchaseText} You purchased {item.name} for {itemCost}.");
-                TransientDataScript.PushAlert($"Purchased {item.name}. I now have {Player.GetCount(item.objectID, name)} total.");
+                BtnSellFromPlayer();
+                buyFromShopMenu.OpenPage(0);
             }
             else
             {
-                Debug.Log("Exchange returned false.");
-                TransientDataScript.PushAlert("I don't have enough money.");
+                foreach (var item in buyFromShopPrefabs)
+                {
+                    var uiData = item.GetComponent<ItemUiData>();
+                    uiData.printRarity = true;
+
+                    var shopItemScript = item.AddComponent<ShopItemPrefab>();
+                    shopItemScript.Initialise(uiData.item, this, false);
+                }
+
+                BtnBuyFromShop();
+                sellFromPlayerMenu.OpenPage(0);
+            }
+        }
+    }
+
+    public void BtnSellFromPlayer()
+    {
+        if (activeShop.buysItems)
+        {
+            sellFromPlayerMenu.gameObject.SetActive(true);
+            buyFromShopMenu.gameObject.SetActive(false);
+
+            btnSell.interactable = false;
+
+            if (!activeShop.doesNotSell)
+            {
+                btnBuy.interactable = true;
             }
 
         }
-        else
-        {
-            Debug.Log("I already have the maximum amount of this item.");
-            TransientDataScript.PushAlert("I don't have enough space for more of this.");
-        }
 
+        itemInfoCard.SetActive(false);
+        itemDescriptionCard.SetActive(false);
     }
 
+    public void BtnBuyFromShop()
+    {
+        if (!activeShop.doesNotSell)
+        {
+            sellFromPlayerMenu.gameObject.SetActive(false);
+            buyFromShopMenu.gameObject.SetActive(true);
+
+            btnBuy.interactable = false;
+
+            if (activeShop.buysItems)
+            {
+                btnSell.interactable = true;
+            }
+        }
+
+        itemInfoCard.SetActive(false);
+        itemDescriptionCard.SetActive(false);
+    }
+
+    public void BtnLeave()
+    {
+        TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
+        gameObject.SetActive(false);
+    }
+
+    public void HighlightShopItem(Item item)
+    {
+        itemName.text = item.name + $" ({Player.GetCount(item.objectID, name)} in inventory)";
+        itemPrice.text = activeShop.CalculateBuyFromShopPrice(item).ToString();
+        itemDescription.text = item.description;
+        itemRarity.text = item.rarity.ToString();
+
+        itemInfoCard.SetActive(true);
+        // itemDescriptionCard.SetActive(true);
+
+        //var inventoryAmount = Player.GetCount(item.objectID, name);
+
+        //if (inventoryAmount < item.maxValue)
+        //{
+        //    var purchase = MoneyExchange.Purchase(itemCost);
+
+        //    if (purchase)
+        //    {
+        //        Player.Add(item.objectID);
+        //        AudioManager.PlayUISound("handleCoins");
+        //        //Debug.Log($"{activeShop} You purchased {item.name} for {itemCost}.");
+        //        TransientDataScript.PushAlert($"Purchased {item.name}. I now have {Player.GetCount(item.objectID, name)} total.");
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Exchange returned false.");
+        //        TransientDataScript.PushAlert("I don't have enough money.");
+        //    }
+
+        //}
+        //else
+        //{
+        //    Debug.Log("I already have the maximum amount of this item.");
+        //    TransientDataScript.PushAlert("I don't have enough space for more of this.");
+        //}
+
+    }
+    public void HighlightPlayerItem(Item item)
+    {
+        itemName.text = item.name;
+        itemPrice.text = activeShop.CalculateSellFromInventoryPrice(item).ToString();
+        itemDescription.text = item.description;
+        itemRarity.text = item.rarity.ToString();
+
+        itemInfoCard.SetActive(true);
+        // itemDescriptionCard.SetActive(true);
+    }
     void SyncSkills()
     {
         judgement = Player.GetCount("ATT002", "ShopMenu");
     }
-}
-
-
-[System.Serializable]
-public struct ContainerPage
-{
-    public string typeName;
-    public List<Item> pageContent;
 }
