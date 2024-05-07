6d0d8f8ed34c0b3feb596a5630daf41539c74e3b
Renatuscape <rrjonsberg@gmail.com>
2024-05-08 00:23:56 +0200

    Created treasure collection class, which displays which treasures the player has had in their inventory at any point, regardless of count.

---
+++ GameComponents/UIMenus/Journal/CollectionsTreasures.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionsTreasures : MonoBehaviour
+{
+    public GameObject treasureContainer;
+    public GameObject mysteryPrefab;
+    private void OnEnable()
+    {
+        foreach (Transform child in treasureContainer.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Item item in Items.all)
+        {
+            if (item.type == ItemType.Treasure)
+            {
+
+                if (!Player.GetEntry(item.objectID, name, out var x))
+                {
+                    var prefab = BoxFactory.CreateItemIcon(item, false, 64, 14, false);
+                    prefab.transform.SetParent(treasureContainer.transform, false);
+
+                    Image[] images = prefab.transform.Find("ImageContainer").GetComponentsInChildren<Image>();
+
+                    foreach (Image image in images)
+                    {
+                        if (!image.name.ToLower().Contains("shadow"))
+                        {
+                            image.color = Color.black;
+                            var mystery = Instantiate(mysteryPrefab);
+                            mystery.transform.SetParent(image.gameObject.transform, false);
+                        }
+                    }
+                }
+                else
+                {
+                    var prefab = BoxFactory.CreateItemIcon(item, false, 64, 14, true);
+                    prefab.transform.SetParent(treasureContainer.transform, false);
+
+                    var script = prefab.GetComponent<ItemUiData>();
+                    script.printRarity = true;
+                }
+            }
+        }
+    }
+}
