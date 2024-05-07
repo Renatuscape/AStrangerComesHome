4bd45813c2df87cb6a23ee593ee5983c562cc37a
Renatuscape <rrjonsberg@gmail.com>
2024-05-07 11:06:43 +0200

    Rebuilt inventory to use pageinated container.

--- GameComponents/UIMenus/Journal/JournalInventoryPage.cs
+++ GameComponents/UIMenus/Journal/JournalInventoryPage.cs
@@ -1,35 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class JournalInventoryPage : MonoBehaviour
 {
-    public GameObject dynamicInventory;
-    public DynamicInventory inventory;
+    public PageinatedContainer inventoryContainer;
+    public List<GameObject> itemPrefabs;
+    public Item selectedItem;
+    public GameObject itemInfo;
+    public TextMeshProUGUI itemName;
+    public TextMeshProUGUI itemRarity;
+    public TextMeshProUGUI itemDescription;
 
     private void OnEnable()
     {
-        inventory.displayCatalysts = true;
-        inventory.displaySeeds = true;
-        inventory.displayPlants = true;
-        inventory.displayMaterials = true;
-        inventory.displayTreasures = true;
-        inventory.displayTrade = true;
-        inventory.displayMisc = true;
+        itemInfo.gameObject.SetActive(false);
+        itemName.text = "";
+        itemRarity.text = "";
+        itemDescription.text = "";
 
-        dynamicInventory.SetActive(true);
-        inventory.gameObject.SetActive(true);
-        inventory.UpdateWindowSize(450, 200);
-        inventory.UpdateWindowPosition(0, -45);
-        inventory.UpdateButtonContainerWidth(480);
-        inventory.PopulateItemContainer(DynamicInventoryPage.Catalysts);
+        itemPrefabs = inventoryContainer.Initialise(Player.GetInventory(), true, true, true);
+        SetUpInventoryItems();
+        inventoryContainer.gameObject.SetActive(true);
     }
 
     private void OnDisable()
     {
-        if (dynamicInventory != null)
+        inventoryContainer.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (inventoryContainer.selectedItem == null && selectedItem != null)
         {
-            dynamicInventory.SetActive(false);
+            CheckSelectItem();
         }
+    }
+
+    void SetUpInventoryItems()
+    {
+        foreach (var prefab in itemPrefabs)
+        {
+            var inventoryHelper = prefab.AddComponent<PageinatedInventoryHelper>();
+            inventoryHelper.inventory = this;
+        }
+    }
+    public void CheckSelectItem()
+    {
+        selectedItem = inventoryContainer.selectedItem;
+
+        if (selectedItem != null)
+        {
+            itemInfo.gameObject.SetActive(true);
+            itemName.text = Items.GetEmbellishedItemText(selectedItem, false, false, false);
+            itemRarity.text = selectedItem.rarity.ToString();
+            itemDescription.text = selectedItem.description;
+        }
+        else
+        {
+            itemInfo.gameObject.SetActive(false);
+            itemName.text = "";
+            itemRarity.text = "";
+            itemDescription.text = "";
+        }
+
+    }
+}
+
+public class PageinatedInventoryHelper : MonoBehaviour, IPointerClickHandler
+{
+    public JournalInventoryPage inventory;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        inventory.CheckSelectItem();
     }
 }
