d74e1820a7248fa9559a2d2385b4c91afc38cefe
Renatuscape <rrjonsberg@gmail.com>
2024-05-07 18:55:18 +0200

    Class allows journal to be forcibly opened. So far supports forcibly opening books.

---
+++ GameComponents/UIMenus/Journal/JournalController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class JournalController : MonoBehaviour
+{
+    public static JournalController instance;
+    public Journal journal;
+    public JournalCollectionsPage collectionsPage;
+    public CollectionsBooks bookCollection;
+    public BookReader bookReader;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void EnableJournal()
+    {
+        TransientDataScript.SetGameState(GameState.JournalMenu, "Journal.ForceReader", gameObject);
+        journal.gameObject.SetActive(true);
+        journal.mainPage = JournalMainPage.Collections;
+    }
+
+    public void ForceReader(Book book)
+    {
+        EnableJournal();
+        if (book.isLetter)
+        {
+            collectionsPage.EnableLetters();
+        }
+        else
+        {
+            collectionsPage.EnableBooks();
+        }
+
+        bookReader.Initialise(book);
+    }
+    public static void ForceReadBook(Book book)
+    {
+        instance.ForceReader(book);
+    }
+}
