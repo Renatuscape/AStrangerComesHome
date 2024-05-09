36eb5857bc30fcf9868cbbbd3105a7650decdceb
Renatuscape <rrjonsberg@gmail.com>
2024-05-09 14:22:05 +0200

    Removed sprite management for sprouts, and commented out obsolete image finder code.

--- JsonManagers/ItemManager/ItemManager.cs
+++ JsonManagers/ItemManager/ItemManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using UnityEngine.UI;
 using System.Threading.Tasks;
 
 public class ItemManager : MonoBehaviour
@@ -157,7 +156,6 @@ public class ItemManager : MonoBehaviour
         }
         else if (objectID.Contains("SEE"))
         {
-            SproutFinder(ref item);
             return ItemType.Seed;
         }
         else if (objectID.Contains("CAT"))
@@ -226,83 +224,83 @@ public class ItemManager : MonoBehaviour
         }
     }
 
-    public static void SproutFinder(ref Item seed)
-    {
-        seed.stage1 = FindSprite(1, seed.objectID);
-        seed.stage2 = FindSprite(2, seed.objectID);
-        seed.stage3 = FindSprite(3, seed.objectID);
+    //public static void SproutFinder(ref Item seed)
+    //{
+    //    seed.stage1 = FindSprite(1, seed.objectID);
+    //    seed.stage2 = FindSprite(2, seed.objectID);
+    //    seed.stage3 = FindSprite(3, seed.objectID);
 
-        Sprite FindSprite(int frame, string objectID)
-        {
-            string fileDirectory = Application.streamingAssetsPath + "/Sprites/Items/Sprouts/";
-            string filePath = fileDirectory + objectID.Substring(0, 6) + $"-{frame}.png";
-            Texture2D texture;
+    //    Sprite FindSprite(int frame, string objectID)
+    //    {
+    //        string fileDirectory = Application.streamingAssetsPath + "/Sprites/Items/Sprouts/";
+    //        string filePath = fileDirectory + objectID.Substring(0, 6) + $"-{frame}.png";
+    //        Texture2D texture;
 
-            if (!File.Exists(filePath))
-            {
-                //Debug.LogWarning($"Sprout not found for {objectID} stage {frame}. Using default.");
-                filePath = fileDirectory + objectID.Substring(0, 3) + $"000-{frame}.png";
+    //        if (!File.Exists(filePath))
+    //        {
+    //            //Debug.LogWarning($"Sprout not found for {objectID} stage {frame}. Using default.");
+    //            filePath = fileDirectory + objectID.Substring(0, 3) + $"000-{frame}.png";
 
-                if (!File.Exists(filePath))
-                {
-                    Debug.LogError($"Default image not found for type {objectID.Substring(0, 3)}! No image set for {objectID}");
-                    return null;
-                }
-            }
+    //            if (!File.Exists(filePath))
+    //            {
+    //                Debug.LogError($"Default image not found for type {objectID.Substring(0, 3)}! No image set for {objectID}");
+    //                return null;
+    //            }
+    //        }
 
-            byte[] imageBytes = File.ReadAllBytes(filePath);
-            texture = new Texture2D(2, 2); // Create an empty texture
-            texture.filterMode = FilterMode.Point; // Set filter mode to Point for pixel-perfect clarity. PREVENTS BLURRINESS
-            texture.LoadImage(imageBytes); // Load the image bytes
+    //        byte[] imageBytes = File.ReadAllBytes(filePath);
+    //        texture = new Texture2D(2, 2); // Create an empty texture
+    //        texture.filterMode = FilterMode.Point; // Set filter mode to Point for pixel-perfect clarity. PREVENTS BLURRINESS
+    //        texture.LoadImage(imageBytes); // Load the image bytes
 
-            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-        }
-    }
+    //        return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+    //    }
+    //}
 
-    public static Texture2D ImageFinder (ref string objectID)
-    {
-        string fileDirectory = Application.streamingAssetsPath + "/Sprites/Items/";
-        string filePath = fileDirectory + objectID.Substring(0, 6) + ".png";
-        Texture2D imageTexture;
+    //public static Texture2D ImageFinder (ref string objectID)
+    //{
+    //    string fileDirectory = Application.streamingAssetsPath + "/Sprites/Items/";
+    //    string filePath = fileDirectory + objectID.Substring(0, 6) + ".png";
+    //    Texture2D imageTexture;
 
-        if (!File.Exists(filePath))
-        {
-            //Debug.LogWarning($"Image not found for {objectID}. Using default.");
-            filePath = fileDirectory + objectID.Substring(0, 3) + "000.png";
+    //    if (!File.Exists(filePath))
+    //    {
+    //        //Debug.LogWarning($"Image not found for {objectID}. Using default.");
+    //        filePath = fileDirectory + objectID.Substring(0, 3) + "000.png";
 
-            if (!File.Exists(filePath))
-            {
-                Debug.LogError($"Default image not found for type {objectID.Substring(0, 3)}! No image set for {objectID}");
-                return null;
-            }
-        }
+    //        if (!File.Exists(filePath))
+    //        {
+    //            Debug.LogError($"Default image not found for type {objectID.Substring(0, 3)}! No image set for {objectID}");
+    //            return null;
+    //        }
+    //    }
 
-        byte[] imageBytes = File.ReadAllBytes(filePath);
-        imageTexture = new Texture2D(2, 2); // Create an empty texture
-        imageTexture.filterMode = FilterMode.Point; // Set filter mode to Point for pixel-perfect clarity. PREVENTS BLURRINESS
-        imageTexture.LoadImage(imageBytes); // Load the image bytes
+    //    byte[] imageBytes = File.ReadAllBytes(filePath);
+    //    imageTexture = new Texture2D(2, 2); // Create an empty texture
+    //    imageTexture.filterMode = FilterMode.Point; // Set filter mode to Point for pixel-perfect clarity. PREVENTS BLURRINESS
+    //    imageTexture.LoadImage(imageBytes); // Load the image bytes
 
-        return imageTexture;
-    }
+    //    return imageTexture;
+    //}
 
-    public static Sprite SpriteCreator(ref Texture2D texture)
-    {
-        return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-    }
+    //public static Sprite SpriteCreator(ref Texture2D texture)
+    //{
+    //    return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+    //}
 
     public static int CalculatePrice(ref Item item)
     {
         float price = 5;
 
-        int seedPrice = 5;
-        int plantPrice = 15;
-        int materialPrice = 30;
+        int seedPrice = 1;
+        int plantPrice = 30;
+        int materialPrice = 35;
         int catalystPrice = 40;
         int tradePrice = 50;
         int bookPrice = 100;
         int treasurePrice = 250;
 
-        float rarityMultiplier = 0.5f;
+        float rarityMultiplier = 0.3f;
 
         if ((int)item.rarity >= 0)
         {
