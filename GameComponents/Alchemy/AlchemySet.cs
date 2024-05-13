15fbc97e18d3de4768a5cab27cfdbb512a46160c
Renatuscape <rrjonsberg@gmail.com>
2024-05-13 17:21:16 +0200

    Now checks whether player has unlocked the relevant synthesiser.

--- GameComponents/Alchemy/AlchemySet.cs
+++ GameComponents/Alchemy/AlchemySet.cs
@@ -6,35 +6,64 @@ public class AlchemySet : MonoBehaviour
 {
     public AlchemyMenu alchemyMenu;
     public SynthesiserType synthesiser;
+    public string synthID = "none";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (synthesiser == SynthesiserType.Stella)
+        {
+            synthID = "SCR006";
+        }
+        else if (synthesiser == SynthesiserType.Capital)
+        {
+            synthID = "SCR007";
+        }
+        else if (synthesiser == SynthesiserType.Magus)
+        {
+            synthID = "SCR008";
+        }
+        else if (synthesiser == SynthesiserType.Home)
+        {
+            synthID = "SCR009";
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    bool CheckLevel()
     {
-        
+        if (synthID != "none")
+        {
+            if (Player.GetCount(synthID, name) > 0)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void OnMouseDown()
     {
-        // Add check to ensure that the corresponding synthesiser is actually unlocked
-        if (TransientDataScript.GameState == GameState.Overworld)
+        if (CheckLevel())
         {
-            if (alchemyMenu == null)
+            if (TransientDataScript.GameState == GameState.Overworld)
             {
-                alchemyMenu = TransientDataScript.gameManager.menuSystem.alchemyMenu;
-            }
+                if (alchemyMenu == null)
+                {
+                    alchemyMenu = TransientDataScript.gameManager.menuSystem.alchemyMenu;
+                }
 
 
-            if (alchemyMenu != null)
-            {
-                alchemyMenu.isDebugging = false;
-                alchemyMenu.Initialise(synthesiser);
+                if (alchemyMenu != null)
+                {
+                    alchemyMenu.isDebugging = false;
+                    alchemyMenu.Initialise(synthesiser);
+                }
             }
+        }
+        else
+        {
+            LogAlert.QueueTextAlert("I don't know how to use an alchemy set.");
         }
     }
 }
