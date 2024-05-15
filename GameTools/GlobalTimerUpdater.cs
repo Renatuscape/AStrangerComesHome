9921f593b8b026dfc3eb6be957c8ea7de736cf38
Renatuscape <rrjonsberg@gmail.com>
2024-05-15 11:38:21 +0200

    Created class for consolidation of common timers.

---
+++ GameTools/GlobalTimerUpdater.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalTimerUpdater : MonoBehaviour
+{
+    public NightSkyController nightSkyController;
+
+    public float animationTimer;
+    float animationTick = 0.15f;
+    public float secondTimer;
+    float secondTick = 1;
+
+    void Update()
+    {
+        if (TransientDataScript.IsTimeFlowing())
+        {
+            animationTimer += Time.deltaTime;
+            secondTimer += Time.deltaTime;
+
+            if (animationTimer >= animationTick)
+            {
+                animationTimer = 0;
+                PushAnimation();
+            }
+
+            if (secondTimer >= secondTick)
+            {
+                secondTimer = 0;
+                PushSecond();
+            }
+
+            PushEveryFrame();
+        }
+    }
+
+    void PushEveryFrame()
+    {
+        nightSkyController.StarFlicker();
+    }
+    void PushAnimation()
+    {
+        nightSkyController.UpdateNightSky();
+    }
+
+    void PushSecond()
+    {
+
+    }
+}
