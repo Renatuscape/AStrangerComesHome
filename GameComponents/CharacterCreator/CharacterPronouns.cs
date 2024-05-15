c60eb510cb06bb569e023bf979fab93962e6bdaf
Renatuscape <rrjonsberg@gmail.com>
2024-05-15 11:46:59 +0200

    Removed obsolete namespaces.

--- GameComponents/CharacterCreator/CharacterPronouns.cs
+++ GameComponents/CharacterCreator/CharacterPronouns.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using static UnityEngine.Rendering.DebugUI;
 
 public class CharacterPronouns : MonoBehaviour
 {
