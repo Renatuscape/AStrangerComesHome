f5f598e6f80aad355959db0196e86261bcecca96
Renatuscape <rrjonsberg@gmail.com>
2024-05-18 21:13:54 +0200

    Now uses object pooling to instantiate particles on start, and hands these out when called.

--- ParticleFactory/ParticleFactory.cs
+++ ParticleFactory/ParticleFactory.cs
@@ -6,38 +6,100 @@ public class ParticleFactory : MonoBehaviour
 {
     public static ParticleFactory instance;
     public List<GameObject> particlePrefabs;
-
+    public List<ObjectPool> objectPools = new();
+    bool isReady = false;
     void Start()
     {
         instance = this;
 
-    //    defaultBehaviour = new()
-    //{
-    //    new()
-    //    {
-    //        particleID = "smoke",
-    //        particleLife = 4f,
-    //        velocity = 0.8f,
-    //        randomDrag = 0.6f,
-    //        verticalAcceleration = 0.7f,
-    //        horizontalAcceleration = 0f,
-    //        gravity = 0f,
-    //        scatterRange = 2f,
-    //        adjustForCoachSpeed = true,
-    //        isGrowing = true,
-    //    }
-//};
+        foreach (GameObject obj in particlePrefabs)
+        {
+            ObjectPool pool = new();
+            pool.accessID = obj.name.ToLower();
+            pool.objectInstances = new();
+
+            for (int i = 0; i < 100; i++)
+            {
+                GameObject particle = Instantiate(obj);
+                particle.transform.SetParent(gameObject.transform);
+                particle.name = pool.accessID + " " + i;
+                particle.SetActive(false);
+                pool.objectInstances.Add(particle);
+            }
+
+            objectPools.Add(pool);
+        }
+
+        isReady = true;
     }
 
-    public GameObject GetWorldParticle(string particleID)
+    public void ReturnParticleToPool(GameObject particle)
     {
-        var prefab = particlePrefabs.FirstOrDefault(p => p.name.ToLower().Contains(particleID.ToLower()));
-        return Instantiate(prefab);
+        if (isReady)
+        {
+            var pool = objectPools.FirstOrDefault(p => particle.name.Contains(p.accessID));
+            pool.objectInstances.Add(particle);
+        }
+    }
+    public GameObject GetWorldParticle(GameObject parentObject, string particleID, string caller)
+    {
+        if (isReady)
+        {
+            var pool = objectPools.FirstOrDefault(p => p.accessID.Contains(particleID.ToLower()));
+            GameObject parent;
+
+            if (parentObject != null)
+            {
+                parent = parentObject;
+            }
+            else
+            {
+                parent = gameObject;
+            }
+
+            if (pool != null)
+            {
+                if (pool.objectInstances == null || pool.objectInstances.Count == 0)
+                {
+                    Debug.LogWarning(pool.accessID + " had no prefabs of type " + particleID + ". Returning null.");
+                    return null;
+                }
+                else
+                {
+                    var particle = pool.objectInstances[0];
+                    pool.objectInstances.Remove(particle);
+                    particle.SetActive(true);
+                    particle.GetComponent<ParticlePhysics>().PlayBehaviour(parentObject);
+                    return particle;
+                }
+            }
+            else
+            {
+                Debug.LogWarning("No pool found with accessID " + particleID);
+                return null;
+            }
+        }
+        else
+        {
+            return null;
+        }
     }
 
-    public static GameObject SpawnWorldParticle(string particleID)
+    public static GameObject GetParticle(GameObject parentObject, string particleID, string caller)
     {
         // Debug.Log("Attempting to spawn " + particleID);
-        return instance.GetWorldParticle(particleID);
+        return instance.GetWorldParticle(parentObject, particleID, caller);
     }
+
+    public static void ReturnParticle(GameObject particle)
+    {
+        instance.ReturnParticleToPool(particle);
+    }
+}
+
+[System.Serializable]
+public class ObjectPool
+{
+    public string accessID;
+    public List<GameObject> objectInstances;
 }
