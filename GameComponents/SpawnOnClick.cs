68a6630e3bd61ee72d3e8d2b124c540933fd2e5a
Renatuscape <rrjonsberg@gmail.com>
2024-05-18 21:12:45 +0200

    Now calls ParticleFactory instead of instantiating object.

--- GameComponents/SpawnOnClick.cs
+++ GameComponents/SpawnOnClick.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnOnClick : MonoBehaviour
 {
     public TransientDataScript transientData;
-    public GameObject particlePrefab;
+    public List<string> particleIDs;
     public GameState ifGameStateA;
     public GameState ifGameStateB; //if the particles spawn in multiple game state. Otherwise use the same for both
     public int minParticles = 1;
@@ -25,8 +25,10 @@ public class SpawnOnClick : MonoBehaviour
 
             for (int i = 0; i < randomNum; i++)
             {
-                Instantiate(particlePrefab, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
-                particlePrefab.name = "Particle" + name;
+                foreach (string id in particleIDs)
+                {
+                    ParticleFactory.GetParticle(gameObject, id, gameObject.name);
+                }
             }
         }
     }
