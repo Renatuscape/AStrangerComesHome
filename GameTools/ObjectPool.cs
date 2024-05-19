e32b2afce7ad40dd1cf7399430c7f48edd803524
Renatuscape <rrjonsberg@gmail.com>
2024-05-19 15:22:06 +0200

    Moved to separate file.

---
+++ GameTools/ObjectPool.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPool
+{
+    public string accessID;
+    public List<GameObject> objectInstances;
+}
