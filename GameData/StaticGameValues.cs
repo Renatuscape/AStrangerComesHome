681937ebe17548e0967a4c21fe2d1a735906a4b5
Renatuscape <rrjonsberg@gmail.com>
2024-05-21 15:32:06 +0200

    added StaticTags class to hold various objectID references, reducing the need to hard-code these.

--- GameData/StaticGameValues.cs
+++ GameData/StaticGameValues.cs
@@ -11,3 +11,41 @@ public static class StaticGameValues
     public static int maxUpgradeValue = 10;
     public static int defaultGiftableLevel = 10;
 }
+
+public static class StaticTags
+{
+    // Skills
+    public static string Gardening = "GAR000";
+    public static string Cultivation = "GAR001";
+    public static string Epistemology = "GAR002";
+    public static string Goetia = "GAR003";
+
+    public static string Alchemy = "ALC000";
+    public static string SecretAlchemy = "ALC001";
+    public static string ForbiddenAlchemy = "ALC002";
+    public static string Chemistry = "ALC003";
+    public static string Hermeneutics = "ALC004";
+
+    public static string Magic = "MAG000";
+    public static string Metaphysics = "MAG001";
+    public static string Rhetorics = "MAG002";
+    public static string Thaumaturgy = "MAG003";
+
+    // Attunements
+    public static string Wandering = "ATT000";
+    public static string Fate = "ATT001";
+    public static string Judgement = "ATT002";
+    public static string Knowledge = "ATT003";
+    public static string Creation = "ATT004";
+    public static string Mysticism = "ATT005";
+    public static string Prosperity = "ATT006";
+    public static string Dreaming = "ATT007";
+    public static string Destruction = "ATT008";
+
+    // Scripts
+    public static string unlockedPlanters = "SCR004";
+
+    // Misc
+    public static string WeedItem = "PLA000";
+    public static string CausticWasteRecipe = "REC001-NN";
+}
