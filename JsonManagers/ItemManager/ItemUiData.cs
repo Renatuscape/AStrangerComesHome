d84e3db771a5847c25f27140c7c8cf1164214563
Renatuscape <rrjonsberg@gmail.com>
2024-05-25 12:07:07 +0200

    Now holds references to textmesh and images.

--- JsonManagers/ItemManager/ItemUiData.cs
+++ JsonManagers/ItemManager/ItemUiData.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ItemUiData : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Item item;
+    public TextMeshProUGUI numberMesh;
+    public Image itemSprite;
+    public Image itemShadow;
     public bool disableFloatText;
     public bool printPrice;
     public bool printRarity;
