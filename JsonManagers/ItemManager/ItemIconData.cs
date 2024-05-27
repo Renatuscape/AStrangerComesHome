d373492dbd342f963334e32a0c5c5cd9a5c361a1
Renatuscape <rrjonsberg@gmail.com>
2024-05-27 10:51:48 +0200

    Added methods for returning to item pool and enabling/disabling/updating price and count text.

--- JsonManagers/ItemManager/ItemIconData.cs
+++ JsonManagers/ItemManager/ItemIconData.cs
@@ -8,7 +8,9 @@ using UnityEngine.UI;
 public class ItemIconData : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Item item;
-    public TextMeshProUGUI textMesh;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI priceText;
+    public Image[] images;
     public Image itemSprite;
     public Image itemShadow;
     public bool disableFloatText;
@@ -30,5 +32,80 @@ public class ItemIconData : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         {
             TransientDataScript.DisableFloatText();
         }
+    }
+
+    public void EnableCount(bool enable, int value)
+    {
+        if (enable)
+        {
+            countText.text = value.ToString();
+            priceText.transform.parent.gameObject.SetActive(false);
+            countText.transform.parent.gameObject.SetActive(true);
+        }
+        else
+        {
+            countText.transform.parent.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void UpdateCount(int value)
+    {
+        countText.text = value.ToString();
+    }
+
+    public void EnablePrice(bool enable, int value)
+    {
+        if (enable)
+        {
+            priceText.text = value.ToString();
+            priceText.transform.parent.gameObject.SetActive(true);
+            countText.transform.parent.gameObject.SetActive(false);
+        }
+        else
+        {
+            priceText.transform.parent.gameObject.SetActive(false);
+        }
+    }
+
+    public void UpdatePrice(int value)
+    {
+        priceText.text = value.ToString();
+    }
+
+    public void Return(string caller)
+    {
+        //Debug.Log("Attempting to return ItemIconData with gameobject name " + gameObject.name);
+
+        var alchemyDraggable = gameObject.GetComponent<AlchemyDraggableItem>();
+        if (alchemyDraggable != null)
+        {
+            Destroy(alchemyDraggable);
+        }
+
+        var shopDraggable = gameObject.GetComponent<DraggableShopItem>();
+        if (shopDraggable != null)
+        {
+            Destroy(shopDraggable);
+        }
+
+        var shopItem = gameObject.GetComponent<ShopItemPrefab>();
+        if (shopItem != null)
+        {
+            Destroy(shopItem);
+        }
+
+        var giftItem = gameObject.GetComponent<GiftItem>();
+        if (giftItem != null)
+        {
+            Destroy(giftItem);
+        }
+
+        priceText.transform.parent.gameObject.SetActive(false);
+        BoxFactory.ReturnItemIconToPool(this, caller);
+    }
+    private void OnDestroy()
+    {
+        Debug.LogWarning("Item icon named " + gameObject.name + " was destroyed! Fix old code, please :(");
     }
 }
