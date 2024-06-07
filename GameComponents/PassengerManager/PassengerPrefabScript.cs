e682a7f34de01835283e2356cb6003aea385f63c
Renatuscape <rrjonsberg@gmail.com>
2024-06-07 20:29:34 +0200

    Extensively refactored for use with the PassengerData class.

--- GameComponents/PassengerManager/PassengerPrefabScript.cs
+++ GameComponents/PassengerManager/PassengerPrefabScript.cs
@@ -1,92 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class PassengerPrefabScript : MonoBehaviour
 {
-    public DataManagerScript dataManager;
-    public TransientDataScript transientData;
     public GameObject passengerTarget;
     public SpriteRenderer spriteRenderer;
     public Item spiritEssence;
-    public bool isPassengerA;
-    public int fare;
+    public PassengerData passengerData;
 
-    public string passengerName;
-    public Location origin;
-    public Location destination;
+    public bool isReady;
 
-    public List<string> dialogueList;
-
-    void Awake()
+    public void Initialise(PassengerData passengerData)
     {
-        dataManager = GameObject.Find("DataManager").GetComponent<DataManagerScript>();
-        transientData = GameObject.Find("TransientData").GetComponent<TransientDataScript>();
-        passengerTarget = GameObject.Find("PassengerTarget");
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        gameObject.SetActive(false);
+        this.passengerData = passengerData;
+
+        if (!isReady)
+        {
+            passengerTarget = GameObject.Find("PassengerTarget");
+            spriteRenderer = GetComponent<SpriteRenderer>();
+
+            if (passengerData.seatID == "A")
+            {
+                gameObject.name = "PassengerA";
+                transform.position = new Vector3(passengerTarget.transform.position.x - 0.031f, passengerTarget.transform.position.y, -2);
+            }
+            else
+            {
+                gameObject.name = "PassengerB";
+                transform.position = new Vector3(passengerTarget.transform.position.x, passengerTarget.transform.position.y, -2);
+                transform.Rotate(new Vector3(0, -180, 0));
+            }
+
+            spiritEssence = Items.FindByID("CAT000");
+        }
+
+        UpdatePassengerData();
+
+        isReady = true;
     }
 
-    private void Start()
+    public void UpdatePassengerData()
     {
-        if (isPassengerA)
+        if (isReady)
         {
-            name = "PassengerA";
-            transform.position = new Vector3(passengerTarget.transform.position.x - 0.031f, passengerTarget.transform.position.y, -2);
+            if (passengerData.isActive)
+            {
+                spriteRenderer.sprite = SpriteFactory.GetPassengerByID(passengerData.spriteID);
+
+                CalculateFare();
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
         }
         else
         {
-            name = "PassengerB";
-            transform.position = new Vector3(passengerTarget.transform.position.x, passengerTarget.transform.position.y, -2);
-            transform.Rotate(new Vector3(0, -180, 0));
-        }
-    }
-    private void OnEnable()
-    {
-        spiritEssence = Items.FindByID("CAT000");
-        if (isPassengerA)
-        {
-            passengerName = dataManager.passengerNameA;
-            origin = dataManager.passengerOriginA;
-            destination = dataManager.passengerDestinationA;
-            dialogueList = dataManager.passengerChatListA;
-        }
-        if (!isPassengerA)
-        {
-            passengerName = dataManager.passengerNameB;
-            origin = dataManager.passengerOriginB;
-            destination = dataManager.passengerDestinationB;
-            dialogueList = dataManager.passengerChatListB;
-        }
-
-        var passengerSpriteList = GameObject.Find("PassengerSpawner").GetComponent<PassengerManager>().passengerSpriteList;
-        spriteRenderer.sprite = passengerSpriteList[Random.Range(0, passengerSpriteList.Count - 1)];
-
-        CalculateFare();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (isPassengerA && dataManager.passengerIsActiveA == false)
-            gameObject.SetActive(false);
-        if (!isPassengerA && dataManager.passengerIsActiveB == false)
-            gameObject.SetActive(false);
-
-        if (origin is null)
-        {
-            origin = transientData.currentRegion.locations[0]; //Check for closest place
-            CalculateFare();
+            Debug.Log("Attempted to set up passenger before it was ready.");
         }
     }
 
     void CalculateFare()
     {
-        float distance = CalculateDistance(origin, destination);
+        float distance = CalculateDistance(passengerData.origin, passengerData.destination);
         Debug.Log("Distance between A and B: " + distance);
-        fare = (int)(distance * distance) * (int)Mathf.Ceil(Player.GetCount("SCR012", name) * 0.5f);
-        Debug.Log("Fare calculated to " + fare);
+        passengerData.fare = (int)(distance * distance) * (int)Mathf.Ceil(Player.GetCount("SCR012", name) * 0.5f);
+        Debug.Log("Fare calculated to " + passengerData.fare);
     }
 
     public static float CalculateDistance(Location pointA, Location pointB)
@@ -99,43 +79,40 @@ public class PassengerPrefabScript : MonoBehaviour
     {
         if (TransientDataScript.GameState == GameState.Overworld && TransientDataScript.CameraView == CameraView.Normal)
         {
-            if (destination == transientData.currentLocation)
+            if (passengerData.destination.objectID == TransientDataScript.GetCurrentLocation().objectID)
             {
                 int fortune = Player.GetCount("ATT000", "PassengerPrefabScript");
 
                 // Actual fare is added by highest denomination
-                int added = MoneyExchange.AddHighestDenomination(fare);
+                int added = MoneyExchange.AddHighestDenomination(passengerData.fare);
 
                 AudioManager.PlayAmbientSound("handleCoins", -0.2f);
 
                 // Ensure the player had inventory space for full fare
-                if (added < fare)
+                if (added < passengerData.fare)
                 {
                     TransientDataScript.PushAlert("I was unable to accept the total fare. Better go to the bank!");
                 }
                 else
                 {
-                    TransientDataScript.PushAlert($"{passengerName} paid {fare} shillings.");
+                    TransientDataScript.PushAlert($"{passengerData.passengerName} paid {passengerData.fare} shillings.");
                 }
 
                 // Tip is paid out in hellers and scripted items are updated to track
-                Player.Add("MIS000", Random.Range(0, fare), true);
+                Player.Add("MIS000", Random.Range(0, passengerData.fare), true);
                 Player.Add("SCR000");
-                Player.Add("SCR001", fare);
+                Player.Add("SCR001", passengerData.fare);
 
                 // Spirit Essence drop
                 if (Random.Range(0, 100) > 80 - (fortune * 4))
                 {
                     Player.Add(spiritEssence.objectID);
-                    TransientDataScript.PushAlert($"{passengerName} dropped some Spirit Essence.");
+                    TransientDataScript.PushAlert($"{passengerData.passengerName} dropped some Spirit Essence.");
                     AudioManager.PlayAmbientSound("cloth3");
                 }
 
-                if (isPassengerA)
-                    dataManager.passengerIsActiveA = false;
-                else
-                    dataManager.passengerIsActiveB = false;
-
+                passengerData.isActive = false;
+                passengerData.characterID = string.Empty;
                 gameObject.SetActive(false);
             }
         }
@@ -145,10 +122,7 @@ public class PassengerPrefabScript : MonoBehaviour
     {
         if (TransientDataScript.GameState == GameState.Overworld && TransientDataScript.CameraView == CameraView.Normal)
         {
-            //var locationToString = destination.ToString();
-            //var locationName = Regex.Replace(locationToString, "(\\B[A-Z])", " $1");
-
-            TransientDataScript.PrintFloatText(passengerName + "\n" + destination.name);
+            TransientDataScript.PrintFloatText(passengerData.passengerName + "\n" + passengerData.destination.name);
         }
     }
 
