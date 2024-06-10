10c8b52d7b3e6b415602faad006a09b524b4af47
Renatuscape <rrjonsberg@gmail.com>
2024-06-10 09:36:48 +0200

    Renamed AnimationAction node to AnimationType, and AnimationFrames class to AnimationData. AnimationLibrary now has a method to fetch an animation of specific type, first or random if more exist.

--- Animation/AnimationLibrary.cs
+++ Animation/AnimationLibrary.cs
@@ -3,27 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-public enum AnimationAction
+public enum AnimationType
 {
     idle,
     idleAction,
     appear,
     disappear,
+    open,
+    close,
     walk,
     run,
     alert,
-    chat
+    chat,
+    use
 }
 
 public class AnimationLibrary : MonoBehaviour
 {
     public List<AnimatedSprite> animatedObject;
-
     public static AnimationLibrary animationLibrary;
-
+    float defaultFrameRate = 0.1f;
     private void Start()
     {
         animationLibrary = GetComponent<AnimationLibrary>();
+
+        foreach (var animatedSprite in animatedObject)
+        {
+            foreach (var anim in animatedSprite.animationData)
+            {
+                if (anim.frameRate == 0)
+                {
+                    anim.frameRate = defaultFrameRate;
+                }
+            }
+        }
     }
 
     public static AnimatedSprite GetAnimatedObject(string searchTerm)
@@ -42,15 +55,35 @@ public class AnimatedSprite
 {
     public string name;
     public Sprite still;
-    public AnimationFrames idle;
-    public List<AnimationFrames> frames;
+    public List<AnimationData> animationData;
+
+    public AnimationData GetAnimationType(AnimationType type, bool checkForRandom = false)
+    {
+        if (checkForRandom)
+        {
+            List<AnimationData> animations = animationData.FindAll(f => f.type == type).ToList();
+
+            if (animations.Count > 0)
+            {
+                return animations[UnityEngine.Random.Range(0, animations.Count)];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return animationData.FirstOrDefault(f => f.type == type);
+        }
+    }
 }
 
 [Serializable]
-public class AnimationFrames
+public class AnimationData
 {
-    public string name;
-    public AnimationAction type;
+    public AnimationType type;
+    public int customLoopStart;
     public bool disallowLooping;
     public float frameRate = 0.1f;
     public List<Sprite> frames;
