b826acaadd2db0344d7252d53b10e94a54fedd65
Renatuscape <rrjonsberg@gmail.com>
2024-06-16 00:12:37 +0200

    Created parent class for other bundles to inherit from.

---
+++ Interactables/InteractableBundleData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class InteractableBundleData
+{
+    public string bundleID;
+    public int spawnChance;
+    public int cooldown;
+    public int maxYieldPerItem;
+    public int maxItems;
+    public int extraItemPenalty;
+    public bool alwaysMaxYield;
+    public bool alwaysMaxItems;
+    public bool exactCooldown;
+    public bool disableRespawn;
+    public bool isPrioritised;
+    public bool yieldAllCustomContent;
+    public RequirementPackage requirements;
+    public List<IdIntPair> customContent;
+    public List<IdIntPair> lootOutput = new();
+}
