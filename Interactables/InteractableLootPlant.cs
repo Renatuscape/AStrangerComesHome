299cafb063d739ada31f3e80fcaa857c54758628
Renatuscape <rrjonsberg@gmail.com>
2024-06-16 00:14:19 +0200

    Added logic to InteractableLootPlant. InteractablePlantBundle now inherits from InteractableBundleData, and has yield loot method.

--- Interactables/InteractableLootPlant.cs
+++ Interactables/InteractableLootPlant.cs
@@ -3,40 +3,159 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class InteractableLootPlant : MonoBehaviour
 {
     public string plantNodeID;
     public InteractablePlantBundle plantBundle;
-    public bool showSproutOnEmpty;
+    public bool disableSproutOnFail;
+    public bool disableVesselOnFail;
+    public SpriteRenderer plantRenderer;
+    public List<SpriteRenderer> vesselRenderers;
+    public BoxCollider2D col;
+    public bool isEnabled;
     void Start()
     {
-        
+        col = GetComponent<BoxCollider2D>();
+
+        plantNodeID = gameObject.name;
+
+        if (plantBundle != null && plantBundle.CheckBundle(plantNodeID))
+        {
+            plantBundle.Setup();
+            plantRenderer.sprite = plantBundle.plant.sprite;
+            isEnabled = true;
+        }
+        else
+        {
+            col.enabled = false;
+
+            if (disableSproutOnFail)
+            {
+                plantRenderer.enabled = false;
+            }
+            if (disableVesselOnFail)
+            {
+                plantRenderer.enabled = false;
+
+                foreach (var rend in  vesselRenderers)
+                {
+                    rend.enabled = false;
+                }
+            }
+
+            if (!disableVesselOnFail && !disableSproutOnFail)
+            {
+                int maxGrowth = plantBundle.cooldown;
+                int progress = plantBundle.playerEntry != null ? plantBundle.playerEntry.amount : 0;
+
+                Item seed = plantBundle.seed;
+
+                if (progress < maxGrowth * 0.3f)
+                {
+                    plantRenderer.sprite = SpriteFactory.GetSprout(seed.objectID, 1);
+                }
+                else if (progress > maxGrowth * 0.3f && progress < maxGrowth * 0.6f)
+                {
+                    plantRenderer.sprite = SpriteFactory.GetSprout(seed.objectID, 2);
+                }
+                else if (progress > maxGrowth * 0.6f)
+                {
+                    plantRenderer.sprite = SpriteFactory.GetSprout(seed.objectID, 3);
+                }
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (plantBundle != null && isEnabled)
+        {
+            plantBundle.YieldLoot();
+        }
     }
 }
 
 [Serializable]
-public class InteractablePlantBundle
+public class InteractablePlantBundle : InteractableBundleData
 {
-    public string bundleID;
     public string seedID;
     public Item seed;
     public Item plant;
+    public IdIntPair playerEntry;
 
     public int maxPlantYield;
     public int maxSeedYield;
-    public int maxBonusYield;
-    public int spawnChance;
     public int seedDropChance;
-    public int cooldown;
-    public bool exactCooldown;
-    public bool disableRespawn;
-    public bool isPrioritised;
     public bool disableSeedLoot;
-    public bool alwaysYieldMax;
-    public bool yieldAllBonusLoot;
-    public List<string> bonusLoot;
-    public RequirementPackage requirements;
+    public bool dropOnlySeed;
+
+    public void YieldLoot()
+    {
+        lootOutput.Clear();
+
+        IdIntPair plantEntry;
+        IdIntPair seedEntry;
+
+        if (!dropOnlySeed)
+        {
+            plantEntry = new() { objectID = plant.objectID, amount = maxPlantYield};
+            lootOutput.Add(plantEntry);
+
+            if (!disableSeedLoot )
+            {
+                if (Random.Range(0, 100) < seedDropChance)
+                {
+                    seedEntry = new() { objectID = seed.objectID, amount = maxSeedYield };
+                    lootOutput.Add(seedEntry);
+                }
+            }
+        }
+        else
+        {
+            seedEntry = new() { objectID = seed.objectID, amount = maxSeedYield};
+            lootOutput.Add(seedEntry);
+        }
+
+        if (customContent != null && customContent.Count > 0)
+        {
+            if (yieldAllCustomContent)
+            {
+                lootOutput.AddRange(customContent);
+            }
+            else
+            {
+                int itemCount = 1;
+                List<IdIntPair> potentialLoot = new(customContent);
+
+                while (true)
+                {
+                    if (potentialLoot.Count == 0) { break; }
+
+                    int yieldChance = 100 - (extraItemPenalty * itemCount);
+
+                    if (Random.Range(0, 100) < yieldChance)
+                    {
+                        itemCount++;
+
+                        var randomEntry = potentialLoot[Random.Range(0, potentialLoot.Count)];
+                        lootOutput.Add(randomEntry);
+                        potentialLoot.Remove(randomEntry);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        foreach (var entry in lootOutput)
+        {
+            Player.Add(entry.objectID, alwaysMaxYield ? entry.amount : Random.Range(1, entry.amount + 1));
+        }
+    }
 
     public bool CheckBundle(string plantNodeID)
     {
@@ -51,10 +170,17 @@ public class InteractablePlantBundle
         else
         {
             plant = Items.FindByID(seed.outputID);
+
+            if (plant == null)
+            {
+                Debug.Log("CheckBundle could not find plant with ID " + seed.outputID);
+                return false;
+            }
         }
 
+        playerEntry = Player.claimedLoot.FirstOrDefault(l => l.objectID == bundleID);
 
-        if (Player.claimedLoot.FirstOrDefault(l => l.objectID == bundleID) != null)
+        if (playerEntry != null)
         {
             Debug.Log("Loot bundle exists in the claimed loot list.");
             return false;
@@ -78,12 +204,38 @@ public class InteractablePlantBundle
     {
         if (maxPlantYield == 0)
         {
-            maxPlantYield = 1;
+            maxPlantYield = seed.yield;
         }
-
-        if (!disableSeedLoot && maxSeedYield == 0)
+        if (!disableSeedLoot)
         {
-            maxSeedYield = 1;
+            if (maxSeedYield == 0)
+            {
+                maxSeedYield = 1;
+            }
+            if (seedDropChance == 0)
+            {
+                seedDropChance = 30;
+            }
+        }
+        if (spawnChance == 0)
+        {
+            spawnChance = 100;
+        }
+        if (cooldown == 0)
+        {
+            cooldown = 1 + seed.yield + seed.health;
+        }
+        if (maxYieldPerItem == 0)
+        {
+            maxYieldPerItem = 3;
+        }
+        if (maxItems == 0)
+        {
+            maxItems = 3;
+        }
+        if (extraItemPenalty == 0)
+        {
+            extraItemPenalty = 100 / (maxItems + 1);
         }
     }
 }
