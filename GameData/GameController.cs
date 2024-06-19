e1f0c002dd2689197dd1ae9b7b9b3b02034e56d3
Renatuscape <rrjonsberg@gmail.com>
2024-06-19 15:07:00 +0200

    Refactored game controller to utilise a dictionary for key bindings, reducing the side of the update function.

--- GameData/GameController.cs
+++ GameData/GameController.cs
@@ -1,277 +1,194 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     public GameManagerScript gm;
+    private int screenSetting = 2;
+
+    private readonly Dictionary<KeyCode, Action> keyBindings = new Dictionary<KeyCode, Action>();
+
+    private void Start()
+    {
+        InitializeKeyBindings();
+    }
 
     private void Update()
     {
-        if (TransientDataScript.GameState != GameState.Loading &&
-            TransientDataScript.GameState != GameState.MainMenu &&
-            TransientDataScript.GameState != GameState.ShopMenu &&
-            TransientDataScript.GameState != GameState.CharacterCreation &&
-            TransientDataScript.GameState != GameState.Dialogue)
+        var gameState = TransientDataScript.GameState;
+
+        if (gameState == GameState.Loading ||
+            gameState == GameState.MainMenu ||
+            gameState == GameState.ShopMenu ||
+            gameState == GameState.CharacterCreation ||
+            gameState == GameState.Dialogue)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            return;
+        }
+
+        ProcessInput();
+    }
+
+    private void InitializeKeyBindings()
+    {
+        keyBindings[KeyCode.Space] = ToggleCoachCamera;
+        keyBindings[KeyCode.Escape] = HandleEscapeKey;
+        keyBindings[KeyCode.Return] = ToggleEngineState;
+        keyBindings[KeyCode.Alpha1] = () => ToggleEngineState(EngineState.FirstGear);
+        keyBindings[KeyCode.Alpha2] = () => ToggleEngineState(EngineState.SecondGear);
+        keyBindings[KeyCode.Alpha3] = () => ToggleEngineState(EngineState.ThirdGear);
+        keyBindings[KeyCode.Backspace] = () => ToggleEngineState(EngineState.Reverse);
+        keyBindings[KeyCode.E] = () => ToggleEngineState(EngineState.FirstGear);
+        keyBindings[KeyCode.R] = () => ToggleEngineState(EngineState.Reverse);
+        keyBindings[KeyCode.M] = () => ToggleMap(TransientDataScript.GameState != GameState.MapMenu);
+        keyBindings[KeyCode.Tab] = ToggleJournalMenu;
+        keyBindings[KeyCode.J] = ToggleJournalMenu;
+        keyBindings[KeyCode.I] = ToggleJournalMenu; //() => ToggleJournalMenu(JournalMainPage.Inventory);
+        keyBindings[KeyCode.Q] = ToggleJournalMenu; //() => ToggleJournalMenu(JournalMainPage.Quests);
+    }
+
+    private void ProcessInput()
+    {
+        foreach (var keyBinding in keyBindings)
+        {
+            if (Input.GetKeyDown(keyBinding.Key))
             {
-                if (TransientDataScript.GameState == GameState.Overworld)
-                {
-                    if (TransientDataScript.CameraView != CameraView.Normal)
-                    {
-                        ToggleCoachCamera(false);
-                    }
-                    else
-                    {
-                        ToggleCoachCamera(true);
-                    }
-                }
-                else if (TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    ToggleMap(false);
-                }
-            }
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld)
-                {
-                    ToggleCoachCamera(false);
-                    Debug.Log("escape key was pressed");
-                    TransientDataScript.gameManager.menuSystem.startMenu.SetActive(true);
-                }
-                else if (TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    ToggleMap(false);
-                }
-                else if (TransientDataScript.GameState == GameState.JournalMenu)
-                {
-                    TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Return))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld ||
-                    TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    if (TransientDataScript.transientData.engineState != EngineState.Off)
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Off;
-                    }
-                    else
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.FirstGear;
-                    }
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld ||
-                    TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    if (TransientDataScript.transientData.engineState != EngineState.FirstGear)
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.FirstGear;
-                    }
-                    else
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Off;
-                    }
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld ||
-                   TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    if (TransientDataScript.transientData.engineState != EngineState.SecondGear)
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.SecondGear;
-                    }
-                    else
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Off;
-                    }
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld ||
-                    TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    if (TransientDataScript.transientData.engineState != EngineState.ThirdGear)
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.ThirdGear;
-                    }
-                    else
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Off;
-                    }
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Backspace))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld ||
-                    TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    if (TransientDataScript.transientData.engineState != EngineState.Reverse)
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Reverse;
-                    }
-                    else
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Off;
-                    }
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.E))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld ||
-                    TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    if (TransientDataScript.transientData.engineState != EngineState.Off)
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Off;
-                    }
-                    else
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.FirstGear;
-                    }
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.R))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld ||
-                    TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    if (TransientDataScript.transientData.engineState != EngineState.Reverse)
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Reverse;
-                    }
-                    else
-                    {
-                        TransientDataScript.transientData.engineState = EngineState.Off;
-                    }
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.M))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld)
-                {
-                    ToggleMap(true);
-                }
-                else if (TransientDataScript.GameState == GameState.MapMenu)
-                {
-                    ToggleMap(false);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Tab) || Input.GetKeyDown(KeyCode.J))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld)
-                {
-                    TransientDataScript.SetGameState(GameState.JournalMenu, name, gameObject);
-                    TransientDataScript.gameManager.menuSystem.journalMenu.SetActive(true);
-                }
-                else if (TransientDataScript.GameState == GameState.JournalMenu)
-                {
-                    TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.I))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld)
-                {
-                    TransientDataScript.SetGameState(GameState.JournalMenu, name, gameObject);
-                    TransientDataScript.gameManager.menuSystem.journalMenu.SetActive(true);
-                    TransientDataScript.gameManager.menuSystem.journalMenu.GetComponent<Journal>().mainPage = JournalMainPage.Inventory;
-                }
-                else if (TransientDataScript.GameState == GameState.JournalMenu)
-                {
-                    TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Q))
-            {
-                if (TransientDataScript.GameState == GameState.Overworld)
-                {
-                    TransientDataScript.SetGameState(GameState.JournalMenu, name, gameObject);
-                    TransientDataScript.gameManager.menuSystem.journalMenu.SetActive(true);
-                    TransientDataScript.gameManager.menuSystem.journalMenu.GetComponent<Journal>().mainPage = JournalMainPage.Quests;
-                }
-                else if (TransientDataScript.GameState == GameState.JournalMenu)
-                {
-                    TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
-                }
+                keyBinding.Value.Invoke();
+                break;
             }
         }
 
         if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.UpArrow))
         {
-            screenSetting++;
-            Debug.Log("Change resolution up registered.");
-            SetScreenResolution();
+            ChangeResolution(1);
         }
         else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.DownArrow))
         {
-            screenSetting--;
-            Debug.Log("Change resolution down registered.");
-            SetScreenResolution();
+            ChangeResolution(-1);
         }
     }
 
-    void ToggleMap(bool enable)
+    private void ToggleCoachCamera()
     {
-        if (enable)
+        if (TransientDataScript.GameState == GameState.Overworld)
         {
-            TransientDataScript.SetGameState(GameState.MapMenu, nameof(GameController), gameObject);
-            gm.mapComponent.ToggleEnable(true);
+            if (TransientDataScript.CameraView != CameraView.Normal)
+            {
+                ToggleCoachCamera(false);
+            }
+            else
+            {
+                ToggleCoachCamera(true);
+            }
         }
-        else
+        else if (TransientDataScript.GameState == GameState.MapMenu)
         {
-            TransientDataScript.SetGameState(GameState.Overworld, nameof(GameController), gameObject);
-            gm.mapComponent.ToggleEnable(false);
+            ToggleMap(false);
         }
     }
 
-    void ToggleCoachCamera(bool setClose)
+    private void HandleEscapeKey()
     {
-        if (setClose)
+        switch (TransientDataScript.GameState)
         {
-            gm.cameraComponent.CameraClose();
-        }
-        else
-        {
-            gm.cameraComponent.CameraNormal();
+            case GameState.Overworld:
+                ToggleCoachCamera(false);
+                Debug.Log("Escape key was pressed");
+                TransientDataScript.gameManager.menuSystem.startMenu.SetActive(true);
+                break;
+            case GameState.MapMenu:
+                ToggleMap(false);
+                break;
+            case GameState.JournalMenu:
+                TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
+                break;
         }
     }
 
-    int screenSetting = 2;
-    void SetScreenResolution()
+    private void ToggleEngineState()
     {
-        int max = 3;
-        if (screenSetting < 0)
-        {
-            screenSetting = max;
-        }
-        else if (screenSetting > max)
-        {
-            screenSetting = 0;
-        }
+        ToggleEngineState(TransientDataScript.transientData.engineState != EngineState.Off ? EngineState.Off : EngineState.FirstGear);
+    }
 
-        if (screenSetting == max)
+    private void ToggleEngineState(EngineState targetState)
+    {
+        if (TransientDataScript.GameState == GameState.Overworld || TransientDataScript.GameState == GameState.MapMenu)
         {
-            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+            TransientDataScript.transientData.engineState = TransientDataScript.transientData.engineState != targetState ? targetState : EngineState.Off;
         }
-        else if (screenSetting == 2)
+    }
+
+    private void ToggleMap(bool enable)
+    {
+        if (TransientDataScript.GameState == GameState.Overworld || TransientDataScript.GameState == GameState.MapMenu)
         {
-            Screen.SetResolution(1920, 1080, false);
+            TransientDataScript.SetGameState(enable ? GameState.MapMenu : GameState.Overworld, nameof(GameController), gameObject);
+            gm.mapComponent.ToggleEnable(enable);
         }
-        else if (screenSetting == 1)
+    }
+
+    private void ToggleCoachCamera(bool setClose)
+    {
+        if (TransientDataScript.GameState == GameState.Overworld)
         {
-            Screen.SetResolution(1440, 810, false);
+            if (setClose)
+            {
+                gm.cameraComponent.CameraClose();
+            }
+            else
+            {
+                gm.cameraComponent.CameraNormal();
+            }
         }
-        else if (screenSetting == 0)
+    }
+
+    private void ToggleJournalMenu()
+    {
+        if (TransientDataScript.GameState == GameState.Overworld)
         {
-            Screen.SetResolution(960, 540, false);
+            TransientDataScript.SetGameState(GameState.JournalMenu, name, gameObject);
+            TransientDataScript.gameManager.menuSystem.journalMenu.SetActive(true);
+        }
+        else if (TransientDataScript.GameState == GameState.JournalMenu)
+        {
+            TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
+        }
+    }
+
+    //private void ToggleJournalMenu(JournalMainPage mainPage)
+    //{
+    //    if (TransientDataScript.GameState == GameState.Overworld)
+    //    {
+    //        TransientDataScript.SetGameState(GameState.JournalMenu, name, gameObject);
+    //        TransientDataScript.gameManager.menuSystem.journalMenu.SetActive(true);
+    //        TransientDataScript.gameManager.menuSystem.journalMenu.GetComponent<Journal>().mainPage = mainPage;
+    //    }
+    //    else if (TransientDataScript.GameState == GameState.JournalMenu)
+    //    {
+    //        TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
+    //    }
+    //}
+
+    private void ChangeResolution(int direction)
+    {
+        screenSetting = (screenSetting + direction) % 4;
+        if (screenSetting < 0) screenSetting += 4;
+
+        switch (screenSetting)
+        {
+            case 0:
+                Screen.SetResolution(960, 540, false);
+                break;
+            case 1:
+                Screen.SetResolution(1440, 810, false);
+                break;
+            case 2:
+                Screen.SetResolution(1920, 1080, false);
+                break;
+            case 3:
+                Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+                break;
         }
 
         Debug.Log(Screen.currentResolution);
