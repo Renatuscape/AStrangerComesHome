579562c009b36c32f61a6ce7a82e7991f020b1af
Renatuscape <rrjonsberg@gmail.com>
2024-06-19 13:57:33 +0200

    Added methods for setting up a demo showing.

--- DebugMenu.cs
+++ DebugMenu.cs
@@ -46,4 +46,37 @@ public class DebugMenu : MonoBehaviour
             Player.Add(recipe.objectID, 50, true);
         }
     }
+
+    public void PurgePassengers()
+    {
+        TransientDataScript.gameManager.dataManager.seatA.isActive = false;
+        TransientDataScript.gameManager.dataManager.seatB.isActive = false;
+    }
+
+    public void EnableDemoMode()
+    {
+        Player.Add("ALC000", 1, true);
+        Player.Add("GAR000", 1, true);
+        Player.Add("SCR004", 1, true);
+        Player.Add("SCR006", 1, true);
+        Player.Add("SCR007", 1, true);
+        Player.Add("SCR010", 1, true);
+        Player.Add("SCR012", 1, true);
+        Player.Add("SCR013", 1, true);
+        Player.Add("SCR016", 1, true);
+    }
+
+    public void SkipPrologue()
+    {
+        Player.Add("MIS021", 1, true);
+        Player.Add("MIS022", 1, true);
+        Player.Add("MIS023", 1, true);
+
+        Player.SetQuest("ARC001-Q01", 18);
+        Player.SetQuest("ARC000-Q00", 7);
+        Player.SetQuest("ARC002-Q00", 100);
+        Player.SetQuest("ARC003-Q00", 100);
+        Player.SetQuest("ARC004-Q00", 100);
+        Player.SetQuest("ARC006-Q00", 100);
+    }
 }
