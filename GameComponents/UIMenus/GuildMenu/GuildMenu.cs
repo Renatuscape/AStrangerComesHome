50f1ada257350d5e375882e442b658794d735672
Renatuscape <rrjonsberg@gmail.com>
2024-06-21 00:03:39 +0200

    Rewards menu is now initialised when the menu is opened, and is toggled in and out of view, to avoid constantly rebuilding the tier list.

--- GameComponents/UIMenus/GuildMenu/GuildMenu.cs
+++ GameComponents/UIMenus/GuildMenu/GuildMenu.cs
@@ -18,11 +18,17 @@ public class GuildMenu : MonoBehaviour
         if (TransientDataScript.GameState != GameState.ShopMenu)
         {
             gameObject.SetActive(false);
+            rewardsMenu.gameObject.SetActive(false);
         }
     }
 
     public void Initialise(Character character)
     {
+        if (rewardsMenu.gameObject.activeInHierarchy)
+        {
+            rewardsMenu.gameObject.SetActive(false);
+        }
+
         this.character = character;
         totalPassengers = Player.GetCount(StaticTags.TotalPassengers, name);
         totalFare = Player.GetCount(StaticTags.TotalFare, name);
@@ -32,8 +38,35 @@ public class GuildMenu : MonoBehaviour
 
         gameObject.SetActive(true);
 
-        portraitRenderer.EnableForGarage(character.objectID);
+        portraitRenderer.EnableForShop(character.objectID);
+        InitialiseRewardsMenu();
+
         btnOpenRewardsMenu.onClick.RemoveAllListeners();
-        btnOpenRewardsMenu.onClick.AddListener(() => rewardsMenu.OpenRewardsMenu(character.objectID == StaticTags.Guildmaster, totalPassengers, totalFare));
+        btnOpenRewardsMenu.onClick.AddListener(() => ToggleRewardsMenu());
+    }
+
+    void InitialiseRewardsMenu()
+    {
+        rewardsMenu.GetComponent<RectTransform>().transform.localPosition = new Vector3(0, 1000, 0);
+        rewardsMenu.Initialise(character.objectID == StaticTags.Guildmaster, totalPassengers, totalFare);
+    }
+
+    public void ToggleRewardsMenu()
+    {
+        if (rewardsMenu.transform.localPosition.y > -120)
+        {
+            rewardsMenu.GetComponent<RectTransform>().transform.localPosition = new Vector3(0, -120, 0);
+        }
+        else
+        {
+            rewardsMenu.GetComponent<RectTransform>().transform.localPosition = new Vector3(0, 1000, 0);
+        }
+    }
+
+    public void Close()
+    {
+        rewardsMenu.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+        TransientDataScript.SetGameState(GameState.Overworld, name, gameObject);
     }
 }
