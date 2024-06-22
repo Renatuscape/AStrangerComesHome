17a92b2b29fd210eeababa8affc5d33abf338858
Renatuscape <rrjonsberg@gmail.com>
2024-06-22 22:57:14 +0200

    Started work on TextManager class for various text snippets that can be loaded from JSON.

---
+++ JsonManagers/TextManager/TextManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public static class TextManager
+{
+    public static Dictionary<string, string> loadedText = new() // Load content from Json in future
+    {
+        {"STORY0000", ""},
+        {"INTER0000", "New Game"},
+        {"INTER0001", "Continue"},
+    };
+
+    public static string GetText(string tag)
+    {
+        return loadedText[tag];
+    }
+}
