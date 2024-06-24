d5f6844699b13e99acf4a119d920574dd9e1d487
Renatuscape <rrjonsberg@gmail.com>
2024-06-24 21:16:56 +0200

    Added bundle for text nodes.

---
+++ Interactables/InteractableBundleText.cs
@@ -0,0 +1,35 @@
+﻿using static InteractableNodeText;
+
+public class InteractableBundleText : InteractableBundleData
+{
+    public TextType type;
+    public string textTag;
+    public string loadedText;
+    public bool disableReuse;
+    public bool lootClaimed;
+    public bool disableAddBookToInventory;
+    public bool Initialise()
+    {
+        bundleID = "WorldNodeLoot_" + textTag + "_" + (disableRespawn ? "disableRespawn" : "allowRespawn") + (exactCooldown ? "_ECD#" : "_CD#") + cooldown; ;
+
+        if (CheckIfLootable())
+        {
+            lootClaimed = true;
+        }
+
+        if (lootClaimed && disableReuse)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void ClaimLoot()
+    {
+        lootClaimed = true;
+        SaveNodeToPlayer();
+    }
+}
