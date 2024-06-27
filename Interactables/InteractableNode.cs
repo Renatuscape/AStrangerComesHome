2ac55e543f932ab587eeb70c08154a7e6d62b881
Renatuscape <rrjonsberg@gmail.com>
2024-06-27 10:12:41 +0200

    Added InteractableNode for inheritance by other node classes.

---
+++ Interactables/InteractableNode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class InteractableNode : MonoBehaviour
+{
+    public string nodeID;
+    public string animationID;
+    public bool hideOnLoot;
+    public bool hideBobber;
+    public InteractableBobber interactBobber;
+    public SpriteRenderer rend;
+    public BoxCollider2D col;
+    public AnimatedSprite animatedSprite;
+}
