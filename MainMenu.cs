d8daf819f337b0faf6ff3eda1f75fb4a7ea4ea32
Renatuscape <rrjonsberg@gmail.com>
2024-07-04 12:23:14 +0200

    Removed parallaxing for now. Add new scrolling background later.

--- MainMenu.cs
+++ MainMenu.cs
@@ -4,27 +4,5 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public TransientDataScript transientData;
-    public GameObject parallaxManager;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (gameObject.activeInHierarchy)
-        {
-            transientData.currentSpeed = 0.5f;
-            parallaxManager.SetActive(true);
-        }
-    }
-
-    private void OnDisable()
-    {
-        parallaxManager.SetActive(false);
-        transientData.currentSpeed = 0;
-    }
 }
