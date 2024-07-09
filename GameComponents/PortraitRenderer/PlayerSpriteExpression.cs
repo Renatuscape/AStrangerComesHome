1c77775ebf51d07d14a31762cb5e9e1ee38a3061
Renatuscape <rrjonsberg@gmail.com>
2024-07-09 17:39:53 +0200

    Created class for managinig expressions.

---
+++ GameComponents/PortraitRenderer/PlayerSpriteExpression.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class PlayerSpriteExpression
+{
+    public Image expression;
+    public Image eyebrowColour;
+    public Image lipTint;
+
+    public void SetExpression(PlayerExpressionPackage package)
+    {
+        Color currentBrowColour = eyebrowColour.color;
+        Color currentLipColour = lipTint.color;
+
+        expression.sprite = package.expression;
+        eyebrowColour.sprite = package.eyebrowColour;
+        lipTint.sprite = package.lipTint;
+
+        eyebrowColour.color = currentBrowColour;
+        lipTint.color = currentLipColour;
+    }
+}
