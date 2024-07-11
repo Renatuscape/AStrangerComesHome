ed2c581c52025d75ece58a4b57b586fe332e43a6
Renatuscape <rrjonsberg@gmail.com>
2024-07-11 23:22:17 +0200

    Removed commented out code. Added method to refresh sprite images without rebuilding entire object.

--- GameComponents/StorySystem/DialoguePlayerSprite.cs
+++ GameComponents/StorySystem/DialoguePlayerSprite.cs
@@ -1,75 +1,128 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialoguePlayerSprite : MonoBehaviour
 {
     public PlayerSprite playerSprite;
-    //public PlayerSpriteHair playerHair;
+    public CharacterExpressionCatalogue expressionCatalogue;
+    public List<Image> images = new();
+    public List<Image> referenceImages = new();
 
-    //public Image body;
-    //public Image head;
-    //public Image mouth;
-
-    //public Image eyesLines;
-    //public Image eyesIrises;
-    //public Image eyesSclera;
-    //public Image lipTint;
-
-    public Image expression;
-    public Image eyebrowColour;
-    public Image lipTint;
-    private void Awake()
-    {
-        eyebrowColour = playerSprite.playerHair.eyebrowColour;
-        lipTint = playerSprite.lipTint;
-    }
     private void OnEnable()
     {
-        //playerHair.eyebrowColour.sprite = playerSprite.playerHair.eyebrowColour.sprite;
-        //playerHair.accessoryLines.sprite = playerSprite.playerHair.accessoryLines.sprite;
-        //playerHair.accessoryColour.sprite = playerSprite.playerHair.accessoryColour.sprite;
-        //playerHair.accessoryOutline.sprite = playerSprite.playerHair.accessoryOutline.sprite;
-        //playerHair.frontLines.sprite = playerSprite.playerHair.frontLines.sprite;
-        //playerHair.frontColour.sprite = playerSprite.playerHair.frontColour.sprite;
-        //playerHair.backLines.sprite = playerSprite.playerHair.backLines.sprite;
-        //playerHair.backColour.sprite = playerSprite.playerHair.backColour.sprite;
-        //playerHair.outline.sprite = playerSprite.playerHair.outline.sprite;
-
-        //body.sprite = playerSprite.body.sprite;
-        //head.sprite = playerSprite.head.sprite;
-        //mouth.sprite = playerSprite.mouth.sprite;
-
-        //eyesLines.sprite = playerSprite.eyesLines.sprite;
-        //eyesIrises.sprite = playerSprite.eyesIrises.sprite;
-        //eyesSclera.sprite = playerSprite.eyesSclera.sprite;
-
-        //lipTint.sprite = playerSprite.lipTint.sprite;
-
-        //lipTint.color = TransientDataScript.GetColourFromHex(playerSprite.dataManager.playerSprite.lipTintHexColour);
-        //playerHair.LoadPackageWithColoursFromData(playerSprite.hairCatalogue.GetPackageByID(playerSprite.dataManager.playerSprite.hairID));
-
+        Clear();
         CopyAllChildren();
+    }
+
+    public void CycleExpressions()
+    {
+        if (expressionCatalogue != null)
+        {
+            SetExpression(expressionCatalogue.GetNextPackageByIndex(false));
+        }
+        else
+        {
+            Debug.Log("Expression Catalogue was null. Set reference.");
+        }
+    }
+
+    public void RefreshAllImages()
+    {
+        foreach (Image image in images)
+        {
+            var referenceImage = referenceImages.FirstOrDefault(i => i.name == image.name);
+
+            if (referenceImage != null)
+            {
+                image.sprite = referenceImage.sprite;
+
+                if (image.gameObject.activeInHierarchy != referenceImage.gameObject.activeInHierarchy)
+                {
+                    image.gameObject.SetActive(referenceImage.gameObject.activeInHierarchy);
+                }
+            }
+        }
+    }
+
+    public void SetExpressionToDefault()
+    {
+        SetExpression(expressionCatalogue.GetPackageByID("DEFAULT"));
+    }
+
+    public void SetExpression(PlayerExpressionPackage package)
+    {
+        Image expression = images.FirstOrDefault(i => i.name.Contains("Expression"));
+        Image lipTint = images.FirstOrDefault(i => i.name.Contains("LipTint"));
+        Image browColour = images.FirstOrDefault(i => i.name.Contains("BrowColour"));
+
+        if (expression != null)
+        {
+            expression.sprite = package.expression;
+            playerSprite.expression.sprite = expression.sprite;
+        }
+        else
+        {
+            Debug.Log("Expression Image was not found in list.");
+        }
+        if (lipTint != null)
+        {
+            Color color = lipTint.color;
+            lipTint.sprite = package.lipTint;
+            lipTint.color = color;
+            playerSprite.lipTint.sprite = lipTint.sprite;
+        }
+        else
+        {
+            Debug.Log("LipTint Image was not found in list.");
+        }
+        if (browColour != null)
+        {
+            Color color = browColour.color;
+            browColour.sprite = package.browColour;
+            browColour.color = color;
+            playerSprite.playerHair.browColour.sprite = browColour.sprite;
+        }
+        else
+        {
+            Debug.Log("BrowColour Image was not found in list.");
+        }
     }
 
     public void CopyAllChildren()
     {
         foreach (Transform child in playerSprite.gameObject.transform)
         {
+            referenceImages.Add(child.GetComponent<Image>());
+
             GameObject copiedChild = Instantiate(child.gameObject);
             copiedChild.transform.SetParent(gameObject.transform);
 
             copiedChild.transform.localPosition = child.localPosition;
             copiedChild.transform.localScale = new Vector3(1, 1, 1);
+
+            copiedChild.name = child.name;
+
+            images.Add(copiedChild.GetComponent<Image>());
         }
     }
 
     private void OnDisable()
     {
+        Clear();
+    }
+
+    void Clear()
+    {
         foreach (Transform child in gameObject.transform)
         {
             Destroy(child.gameObject);
         }
+
+        images.Clear();
+        referenceImages.Clear();
     }
 }
