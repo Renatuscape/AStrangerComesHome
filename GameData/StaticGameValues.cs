7859c573f01fc3626c3959c19e2ce5a0192b173f
Renatuscape <rrjonsberg@gmail.com>
2024-07-11 23:20:39 +0200

    Added static variables for theme backgrounds and text.

--- GameData/StaticGameValues.cs
+++ GameData/StaticGameValues.cs
@@ -1,4 +1,6 @@
 
+using UnityEngine;
+
 public static class StaticGameValues
 {
     public static int maxItemValue = 99;
@@ -11,4 +13,10 @@ public static class StaticGameValues
     public static int maxUpgradeValue = 10;
     public static int defaultGiftableLevel = 10;
     public static float parallaxFrameRate = 0.001f;
+
+    // Theme colours
+    public static Color lightText = new Color(0.8509f, 0.8666f, 0.8980f);
+    public static Color darkText = new Color(0.3921f, 0.2509f, 0.1294f);
+    public static Color lightThemeBackground = new Color(0.9137f, 0.8705f, 0.8078f);
+    public static Color darkThemeBackground = new Color(0.1882f, 0.1686f, 0.1607f);
 }
