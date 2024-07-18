b4f7ee770883bf154facf45e1ec532fdc707ec09
Renatuscape <rrjonsberg@gmail.com>
2024-07-18 22:58:02 +0200

    Added controller for setting text speed.

---
+++ GameComponents/StorySystem/DialogueTextSpeedController.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTextSpeedController: MonoBehaviour
+{
+    public Button btnSpeed;
+    public TextMeshProUGUI speedText;
+
+    private void Start()
+    {
+        btnSpeed.onClick.AddListener(() =>
+        {
+            GlobalSettings.TextSpeed++;
+
+            if (GlobalSettings.TextSpeed > 4 || GlobalSettings.TextSpeed == 0)
+            {
+                GlobalSettings.TextSpeed = 1;
+            }
+
+            GlobalSettings.SaveSettings();
+
+            speedText.text = GetSpeedText();
+        });
+    }
+    private void OnEnable()
+    {
+        speedText.text = GetSpeedText();
+    }
+
+    string GetSpeedText()
+    {
+        if (GlobalSettings.TextSpeed == 1)
+        {
+            return ">";
+        }
+        else if (GlobalSettings.TextSpeed == 2)
+        {
+            return ">>";
+        }
+        else if (GlobalSettings.TextSpeed == 3)
+        {
+            return ">>>";
+        }
+        else
+        {
+            return ">>>>";
+        }
+    }
+}
