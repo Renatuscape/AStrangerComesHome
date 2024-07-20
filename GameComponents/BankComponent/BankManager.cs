e77f196510ddba00c7bb9daa3e20283a91ba7fb4
Renatuscape <rrjonsberg@gmail.com>
2024-07-20 21:31:14 +0200

    Removed obsolete methods and added Currency enum, and rewardsButton reference.

--- GameComponents/BankComponent/BankManager.cs
+++ GameComponents/BankComponent/BankManager.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
+public enum Currency
+{
+    Heller,
+    Shilling,
+    Crown,
+    Guilder,
+    Total
+}
 public class BankManager : MonoBehaviour
 {
     public GameObject bankMenu;
@@ -11,6 +18,7 @@ public class BankManager : MonoBehaviour
     public GameObject exchangeButton;
     public GameObject loansButton;
     public GameObject chatButton;
+    public GameObject rewardsButton;
 
     public GameObject exchangeMenu;
     public GameObject accountMenu;
@@ -37,6 +45,7 @@ public class BankManager : MonoBehaviour
             exchangeButton.SetActive(false);
             loansButton.SetActive(false);
             chatButton.SetActive(false);
+            rewardsButton.SetActive(false);
 
             if (TransientDataScript.GameState != GameState.Dialogue)
             {
@@ -59,6 +68,7 @@ public class BankManager : MonoBehaviour
         bankMenu.SetActive(true);
         exchangeMenu.SetActive(false);
         accountMenu.SetActive(false);
+        rewardsButton.SetActive(false);
 
         if (TransientDataScript.GetWeekDay() == DayOfWeek.Lunden)
         {
@@ -66,6 +76,7 @@ public class BankManager : MonoBehaviour
             exchangeButton.SetActive(false);
             loansButton.SetActive(false);
             chatButton.SetActive(false);
+            rewardsButton.SetActive(false);
             closedNotice.SetActive(true);
             background.sprite = SpriteFactory.GetBackgroundSprite("Teller_Bank_Closed");
         }
@@ -75,6 +86,7 @@ public class BankManager : MonoBehaviour
             exchangeButton.SetActive(true);
             loansButton.SetActive(true);
             chatButton.SetActive(true);
+            rewardsButton.SetActive(true);
             closedNotice.SetActive(false);
             background.sprite = SpriteFactory.GetBackgroundSprite("Teller_Bank_Open");
         }
@@ -85,95 +97,11 @@ public class BankManager : MonoBehaviour
             leaveButton.SetActive(true);
             exchangeButton.SetActive(false);
             loansButton.SetActive(false);
+            rewardsButton.SetActive(false);
             chatButton.SetActive(true);
             closedNotice.SetActive(false);
             background.sprite = SpriteFactory.GetBackgroundSprite("Teller_Bank_Open");
         }
-
-        UpdateWallet();
-    }
-
-    public void UpdateWallet()
-    {
-        var playerHellers = Player.GetCount("MIS000", "BankManager");
-        var playerShillings = Player.GetCount("MIS001", "BankManager");
-        var playerCrowns = Player.GetCount("MIS002", "BankManager");
-        var playerGuilders = Player.GetCount("MIS003", "BankManager");
-        var playerObols = Player.GetCount("MIS010", "BankManager");
-
-        StartCoroutine(DelayedAdjust(guilders, playerGuilders));
-        StartCoroutine(DelayedAdjust(crowns, playerCrowns));
-        StartCoroutine(DelayedAdjust(shillings, playerShillings));
-        StartCoroutine(DelayedAdjust(hellers, playerHellers));
-        StartCoroutine(DelayedAdjust(oldWorldObols, playerObols));
-        StartCoroutine(DelayedAdjust(playerTotal, MoneyExchange.GetPlayerMoney()));
-
-    }
-
-    IEnumerator DelayedAdjust(TextMeshProUGUI textMesh, int newNumber)
-    {
-        var textMeshContent = textMesh.text.Split(' ');
-        var currentNumber = int.Parse(textMeshContent[1]);
-
-        while (currentNumber != newNumber)
-        {
-            int difference = Mathf.Abs(newNumber - currentNumber);
-
-            int incrementAmount;
-
-
-            if (difference > 1000000)
-            {
-                incrementAmount = 1000000;
-            }
-            else if (difference > 100000)
-            {
-                incrementAmount = 100000;
-            }
-            else if (difference > 50000)
-            {
-                incrementAmount = 50000;
-            }
-            else if(difference > 10000)
-            {
-                incrementAmount = 10000;
-            }
-            else if (difference > 5000)
-            {
-                incrementAmount = 5000;
-            }
-            else if (difference > 1000)
-            {
-                incrementAmount = 500;
-            }
-            else
-            {
-                incrementAmount = difference > 20 ? 20 : 1;
-            }
-
-            if (currentNumber < newNumber)
-            {
-                currentNumber += incrementAmount;
-            }
-            else
-            {
-                currentNumber -= incrementAmount;
-            }
-
-            textMesh.text = textMeshContent[0] + " " + currentNumber;
-            StartCoroutine(JumpNumber(textMesh));
-            yield return new WaitForSeconds(0.05f);
-        }
-    }
-
-    IEnumerator JumpNumber(TextMeshProUGUI textMesh)
-    {
-        float nudgeAmount = 1f;
-        textMesh.gameObject.transform.position = new Vector3(textMesh.transform.position.x + nudgeAmount, textMesh.transform.position.y + nudgeAmount, textMesh.transform.position.z);
-
-        yield return new WaitForSeconds(0.03f);
-        textMesh.gameObject.transform.position = new Vector3(textMesh.transform.position.x - nudgeAmount, textMesh.transform.position.y - nudgeAmount, textMesh.transform.position.z);
-        AudioManager.PlaySoundEffect("handleCoins", -0.1f);
     }
 
     public void CloseBankMenu()
