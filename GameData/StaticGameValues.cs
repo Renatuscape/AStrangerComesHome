6cce0a08c78838ec9a75dd422e965ff0ed33d5be
Renatuscape <rrjonsberg@gmail.com>
2024-07-24 20:16:13 +0200

    Added value for narrationText and added alpha value to light and dark text colour.

--- GameData/StaticGameValues.cs
+++ GameData/StaticGameValues.cs
@@ -15,8 +15,9 @@ public static class StaticGameValues
     public static float parallaxFrameRate = 0.001f;
 
     // Theme colours
-    public static Color lightText = new Color(0.8509f, 0.8666f, 0.8980f);
-    public static Color darkText = new Color(0.3921f, 0.2509f, 0.1294f);
+    public static Color lightText = new Color(0.8509f, 0.8666f, 0.8980f, 1);
+    public static Color darkText = new Color(0.3921f, 0.2509f, 0.1294f, 1);
+    public static Color narrationText = new Color(0.094f, 0.49f, 0.455f, 0.7f);
     public static Color lightThemeBackground = new Color(0.9137f, 0.8705f, 0.8078f);
     public static Color darkThemeBackground = new Color(0.1882f, 0.1686f, 0.1607f);
 }
