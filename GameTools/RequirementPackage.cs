82cd33672c36b9ee6badc4de934c03e3aa81833a
Renatuscape <rrjonsberg@gmail.com>
2024-07-29 20:47:21 +0200

    Added IdIntCondition and IdIntOr class for variable checks.

--- GameTools/RequirementPackage.cs
+++ GameTools/RequirementPackage.cs
@@ -7,8 +7,54 @@ public class RequirementPackage
     public float maxTimeOfDay;
     public int requiredDaysPassed;
     public List<int> weekDay;
-    public string requiredLocation;
+    public string locationID; // requiredLocation;
     public List<IdIntPair> restrictions;
     public List<IdIntPair> requirements;
-    public List<IdMinMax> minMaxRequirements;
+    public List<IdIntOr> conditions;
+}
+
+[System.Serializable]
+public class IdIntCondition
+{
+    public string objectID;
+    public int minAmount = -99999;
+    public int maxAmount = 99999;
+
+    public bool Check()
+    {
+        var count = Player.GetCount(objectID, "IdIntCondition");
+
+        if (count < minAmount && count > maxAmount)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
+[System.Serializable]
+public class IdIntOr
+{
+    public List<IdIntCondition> tests;
+    public int minPasses = 1;
+    public int maxPasses = 99;
+
+    public bool Check()
+    {
+        int checksPassed = 0;
+
+        foreach (var test in tests)
+        {
+            if (test.Check())
+            {
+                checksPassed++;
+            }
+        }
+
+        if (checksPassed < minPasses || checksPassed > maxPasses)
+        {
+            return false;
+        }
+        return true;
+    }
 }
