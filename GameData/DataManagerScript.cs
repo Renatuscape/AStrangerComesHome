50defa623d428710c1a84f90fbf1ff1cc538d68c
Renatuscape <rrjonsberg@gmail.com>
2024-08-01 15:12:27 +0200

    Added variable for disabling character customisation on death. Removed obsolete index variables. Added character preset class.

--- GameData/DataManagerScript.cs
+++ GameData/DataManagerScript.cs
@@ -13,8 +13,9 @@ public class DataManagerScript : MonoBehaviour
     public string saveID;
 
     //SETTINGS
-    public bool randomiseOnDeath;
     public bool saveOnDeath;
+    public bool randomiseOnDeath;
+    public bool disableCustomisationOnDeath;
 
     //PLAYER DATA
     public string playerName;
@@ -33,9 +34,7 @@ public class DataManagerScript : MonoBehaviour
     //PLAYER SPRITE
     public PlayerSpriteData playerSprite; // transition from old index system to using this class
 
-    public int bodyIndex;
     public int headIndex;
-    public int mouthIndex;
 
     //JOURNEY DATA
     public string postLocationID;
@@ -120,7 +119,7 @@ public class PlayerSpriteData
     public void ResetValues()
     {
         hairID = "HairA0";
-        bodyID = "Default";
+        bodyID = "BodyA0";
         headID = "Default";
         eyesID = "EyesA0";
 
@@ -135,5 +134,66 @@ public class PlayerSpriteData
 
         lipTintTransparency = 0;
         enableAccessory = false;
+        enableAccent = false;
+    }
+}
+
+[Serializable]
+public class PlayerPreset
+{
+    public string playerName;
+    public string playerGender;
+    public string pronounSub;
+    public string pronounObj;
+    public string pronounGen;
+
+    public string playerNameColour;
+    public int headIndex;
+
+    public PlayerSpriteData appearance;
+
+    public void PopulateFromDataManager(DataManagerScript dataManager)
+    {
+        playerName = dataManager.playerName;
+        playerGender = dataManager.playerGender;
+        pronounGen = dataManager.pronounGen;
+        pronounObj = dataManager.pronounObj;
+        pronounSub = dataManager.pronounSub;
+        playerNameColour = dataManager.playerNameColour;
+        headIndex = dataManager.headIndex;
+        appearance = dataManager.playerSprite;
+    }
+
+    public void SaveToDataManager(DataManagerScript dataManager, bool savePersonalia, bool colourOnly)
+    {
+        if (savePersonalia)
+        {
+            dataManager.playerName = playerName;
+            dataManager.playerGender = playerGender;
+            dataManager.pronounGen = pronounGen;
+            dataManager.pronounObj = pronounObj;
+            dataManager.pronounSub = pronounSub;
+
+            dataManager.playerNameColour = playerNameColour;
+        }
+
+        if (!colourOnly)
+        {
+            dataManager.headIndex = headIndex;
+
+            dataManager.playerSprite = appearance;
+        }
+        else
+        {
+            dataManager.playerSprite.eyesHexColour = appearance.eyesHexColour;
+            dataManager.playerSprite.hairHexColour = appearance.hairHexColour;
+            dataManager.playerSprite.hairAccentHexColour = appearance.hairAccentHexColour;
+            dataManager.playerSprite.accessoryHexColour = appearance.accessoryHexColour;
+            dataManager.playerSprite.tightsHexColour = appearance.tightsHexColour;
+            dataManager.playerSprite.cloakHexColour = appearance.cloakHexColour;
+            dataManager.playerSprite.vestHexColour = appearance.vestHexColour;
+            dataManager.playerSprite.lipTintHexColour = appearance.lipTintHexColour;
+            dataManager.playerSprite.lipTintTransparency = appearance.lipTintTransparency;
+        }
     }
 }
