d01c02822d7567d0e8991f74e807bf00efc9e9d6
Renatuscape <rrjonsberg@gmail.com>
2024-08-06 13:15:26 +0200

    Created class for managing draggable graphic.

---
+++ GameComponents/UIMenus/ShopMenu/ShopDragIcon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopDragIcon: MonoBehaviour
+{
+    public Image draggableImage;
+
+    public void DragThis(Sprite sprite)
+    {
+        draggableImage.sprite = sprite;
+        transform.position = Input.mousePosition;
+        gameObject.SetActive(true);
+    }
+
+    public void DisableIcon()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
