426ce0c788b9e960e19c74673abf83da098e8f95
Renatuscape <rrjonsberg@gmail.com>
2024-08-06 13:21:25 +0200

    Transactions are now triggered by clicking the shop register while the selected item is not null.

---
+++ GameComponents/UIMenus/ShopMenu/ShopRegister.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShopRegister : MonoBehaviour, IPointerDownHandler
+{
+    public ShopMenu shopMenu;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (shopMenu.selectedItem != null)
+        {
+            shopMenu.HandleTransaction();
+        }
+    }
+}
