da43d04fb38d1b8c385333e7ccdf17114031079a
Renatuscape <rrjonsberg@gmail.com>
2024-08-08 01:14:29 +0200

    Removed unused namespace.

--- GameTools/ReusableDialogueBox/ReusableDialogueBox.cs
+++ GameTools/ReusableDialogueBox/ReusableDialogueBox.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
