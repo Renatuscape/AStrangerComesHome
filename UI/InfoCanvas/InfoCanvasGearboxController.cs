8d62aeefab254bd37dd2a22ee097ce7b4d12fede
Renatuscape <rrjonsberg@gmail.com>
2024-08-10 19:38:20 +0200

    Moved to subfolder.

--- UI/InfoCanvas/InfoCanvasGearboxController.cs
+++ UI/InfoCanvas/InfoCanvasGearboxController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class InfoCanvasGearboxController : MonoBehaviour
 {
     public TransientDataScript transientData;
