c426e0115435afc057b24faf737b8c6b01273242
Renatuscape <rrjonsberg@gmail.com>
2024-08-11 13:35:53 +0200

    No longer attempts to scale canvas manually.

--- GameComponents/UIMenus/StartMenuScript.cs
+++ GameComponents/UIMenus/StartMenuScript.cs
@@ -7,27 +7,11 @@ public class StartMenuScript : MonoBehaviour
 {
     public GameObject controllerMenu;
     public GameObject settingsMenu;
-    public CanvasScaler canvasScaler;
 
-    private void Awake()
-    {
-        if (Screen.width <= 1920)
-            canvasScaler.scaleFactor = 2;
-        else if (Screen.width > 1920 && Screen.width < 3840)
-            canvasScaler.scaleFactor = 3;
-        else if (Screen.width >= 3840)
-            canvasScaler.scaleFactor = 4;
-    }
     void OnEnable()
     {
         controllerMenu.SetActive(false);
         settingsMenu.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void ToggleControlMenu()
