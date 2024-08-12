57859ab45cb00a1f7a42771c9db21e4bd549babb
Renatuscape <rrjonsberg@gmail.com>
2024-08-12 03:07:28 +0200

    Adds toggle function for true name and dropdown functionality.

--- GameComponents/UIMenus/SettingsMenu.cs
+++ GameComponents/UIMenus/SettingsMenu.cs
@@ -6,12 +6,48 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public Toggle alwaysHideCoachExteriorToggle;
-    public Dropdown resolutionDropdown;
+    public DataManagerScript dataManager;
+    public Toggle toggleCoachExterior;
+    public Toggle toggleTrueName;
+    public Toggle toggleRandomiseOnDeath;
+    public Toggle toggleCustomisationOnDeath;
+    public Toggle toggleForceSaveOnDeath;
+
+    public TMP_Dropdown resolutionDropdown;
     public GameObject content;
+    public GameObject behaviourPanel;
+
+    void Start()
+    {
+        toggleCoachExterior.onValueChanged.AddListener(ToggleAlwaysHideCoachExterior);
+        toggleTrueName.onValueChanged.AddListener(ToggleTrueName);
+
+        resolutionDropdown.onValueChanged.AddListener(ResolutionDropDown);
+
+        content.SetActive(false);
+    }
 
     public void Open()
     {
+        if (TransientDataScript.GameState == GameState.MainMenu)
+        {
+            behaviourPanel.SetActive(false);
+        }
+        else
+        {
+            behaviourPanel.SetActive(true);
+        }
+
+        toggleCoachExterior.isOn = GlobalSettings.AlwaysHideCoachExterior;
+        toggleTrueName.isOn = GlobalSettings.AlwaysTrueNamePlate;
+
+        if (dataManager != null)
+        {
+            toggleRandomiseOnDeath.isOn = dataManager.randomiseOnDeath;
+            toggleCustomisationOnDeath.isOn = dataManager.disableCustomisationOnDeath;
+            toggleForceSaveOnDeath.isOn = dataManager.saveOnDeath;
+        }
+
         content.SetActive(true);
     }
 
@@ -20,19 +56,39 @@ public class SettingsMenu : MonoBehaviour
         content.SetActive(false);
     }
 
-    void Start()
-    {
-        alwaysHideCoachExteriorToggle.onValueChanged.AddListener(ToggleAlwaysHideCoachExterior);
-    }
 
     void OnEnable()
     {
-        alwaysHideCoachExteriorToggle.isOn = GlobalSettings.AlwaysHideCoachExterior;
+        toggleCoachExterior.isOn = GlobalSettings.AlwaysHideCoachExterior;
     }
 
     void ToggleAlwaysHideCoachExterior(bool toggle)
     {
         GlobalSettings.AlwaysHideCoachExterior = toggle;
         GlobalSettings.SaveSettings();
+    }
+
+    void ToggleTrueName(bool toggle)
+    {
+        GlobalSettings.AlwaysTrueNamePlate = toggle;
+        GlobalSettings.SaveSettings();
+    }
+
+    void ResolutionDropDown(int option)
+    {
+        switch (option)
+        {
+            case 0:
+                Screen.SetResolution(1440, 810, false);
+                break;
+            case 1:
+                Screen.SetResolution(1920, 1080, false);
+                break;
+            case 2:
+                Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+                break;
+        }
+
+        Debug.Log(Screen.currentResolution);
     }
 }
