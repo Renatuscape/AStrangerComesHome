5a57763e4458e068af866bbf508e887d2ff5a9b5
Renatuscape <rrjonsberg@gmail.com>
2024-08-15 20:06:34 +0200

    Shows confirm menu before character creation is finalised.

---
+++ GameComponents/CharacterCreator/CharacterFinaliseMenu.cs
@@ -0,0 +1,57 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterFinaliseMenu: MonoBehaviour
+{
+    public DataManagerScript dataManager;
+    public TextMeshProUGUI detailsText;
+    public Button btnConfirm;
+    public Button btnCancel;
+
+    private void Start()
+    {
+        btnCancel.onClick.AddListener(() => gameObject.SetActive(false));
+        btnConfirm.onClick.AddListener(() => CompleteCharacterCreation());
+    }
+    private void OnEnable()
+    {
+        Character player = Characters.FindByID("ARC000");
+
+        if (player != null)
+        {
+            string playerName = $"<b>{player.ForceTrueNamePlate()}</b>,";
+            string cleanName = playerName.Replace("\u200B", "").Replace("\u00A0", "").Trim();
+
+            detailsText.text = $"In this incarnation, my true name is {cleanName}" +
+                $" but those who respect the old ways will refer to me as <b>{DialogueTagParser.ParseText("|the Traveller|")}</b> - at least until our bonds grow stronger.\r\n\r\n" +
+                $"My pronouns are <b>{dataManager.pronounSub.ToLower()}</b>/<b>{dataManager.pronounObj.ToLower()}</b>/<b>{dataManager.pronounGen.ToLower()}</b>, and ";
+
+            if (dataManager.playerGender == "Male")
+            {
+                detailsText.text += "I identify as male.";
+            }
+            else if (dataManager.playerGender == "Female")
+            {
+                detailsText.text += "I identify as female.";
+            }
+            else if (dataManager.playerGender == "Other")
+            {
+                detailsText.text += "I do not identify with a binary gender.";
+            }
+            else
+            {
+                detailsText.text += "my gender is inconsequential.";
+            }
+        }
+        else
+        {
+            detailsText.text = "Player data has not yet loaded properly. Please try again.";
+        }
+    }
+
+    void CompleteCharacterCreation()
+    {
+        TransientDataScript.ReturnToOverWorld("Character Creator", gameObject);
+    }
+}
