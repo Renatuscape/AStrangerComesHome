52ec26d0c0b4952827ab5e438338b218fc6e688e
Renatuscape <rrjonsberg@gmail.com>
2024-08-30 00:12:47 +0200

    Handles displayed information for seeds, including growth time.

---
+++ GameComponents/GardeningManager/GardenSeedFloat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GardenSeedFloat : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public ItemIconData itemData;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TransientDataScript.PrintFloatText(Items.GetEmbellishedItemText(itemData.item, false, true, false) + $"\nGrow time: {100 * (itemData.item.health + itemData.item.yield)}");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TransientDataScript.DisableFloatText();
+    }
+}
