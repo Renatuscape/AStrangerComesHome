6f950915e8cca282f996d0361013bba7e89e3c78
Renatuscape <rrjonsberg@gmail.com>
2024-08-30 21:33:14 +0200

    Changes double-click to single-click for selecting upgrade.

--- UI/UIMenus/GarageMenu/GarageUpgradePrefab.cs
+++ UI/UIMenus/GarageMenu/GarageUpgradePrefab.cs
@@ -1,33 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class GarageUpgradePrefab : MonoBehaviour, IPointerClickHandler, IPointerExitHandler
+public class GarageUpgradePrefab : MonoBehaviour, IPointerClickHandler
 {
     public GarageMenu garageMenu;
     public UpgradeIcon upgradeIcon;
-    public bool isClicked;
 
     public void Initialise(GarageMenu garageMenu, UpgradeIcon upgrade)
     {
         this.garageMenu = garageMenu;
         upgradeIcon = upgrade;
-        isClicked = false;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (isClicked)
-        {
-            garageMenu.SelectUpgrade(upgradeIcon);
-        }
-        else
-        {
-            isClicked = true;
-        }
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        isClicked = false;
+        garageMenu.SelectUpgrade(upgradeIcon);
     }
 }
