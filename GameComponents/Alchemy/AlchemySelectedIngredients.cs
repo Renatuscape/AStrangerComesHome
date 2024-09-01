f4eb168f6606131f90d70853b9af6e54ff10944a
Renatuscape <rrjonsberg@gmail.com>
2024-09-01 11:34:33 +0200

    Forcibly refreshes the canvas if the number of materials or infusion ingredients changes.

--- GameComponents/Alchemy/AlchemySelectedIngredients.cs
+++ GameComponents/Alchemy/AlchemySelectedIngredients.cs
@@ -2,19 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 public class AlchemySelectedIngredients : MonoBehaviour
 {
     public TextMeshProUGUI tipText;
     public GameObject materialContainer;
     public GameObject infusionContainer;
-
+    int previousMats;
+    int previousInfusion;
     private void Update()
     {
         int materialCount = materialContainer.transform.childCount;
         int infusionCount = infusionContainer.transform.childCount;
 
+        if (materialCount !=  previousMats || infusionCount != previousInfusion)
+        {
+            StartCoroutine(ForceRefresh());
+            previousMats = materialCount;
+            previousInfusion = infusionCount;
+        }
+
         materialContainer.SetActive(materialCount > 1); //Account for title
         infusionContainer.SetActive(infusionCount > 1); //Account for title
         tipText.gameObject.SetActive(materialCount == 1 && infusionCount == 1);
+    }
+
+    IEnumerator ForceRefresh()
+    {
+        Debug.Log("Force-refreshing alchemy ingredients");
+        yield return null;
+        Canvas.ForceUpdateCanvases();
+        GetComponent<VerticalLayoutGroup>().enabled = false;
+        yield return null;
+        Canvas.ForceUpdateCanvases();
+        GetComponent<VerticalLayoutGroup>().enabled = true;
+        yield return null;
+        Canvas.ForceUpdateCanvases();
     }
 }
