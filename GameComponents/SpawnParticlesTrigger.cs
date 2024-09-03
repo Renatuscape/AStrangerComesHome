5dc48c027c357e74f6faf89bb1f4513ecc868c0b
Renatuscape <rrjonsberg@gmail.com>
2024-09-03 22:38:33 +0200

    New class for simple particle spawning allows outside triggers besides clicks.

---
+++ GameComponents/SpawnParticlesTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticlesTrigger : MonoBehaviour
+{
+    public TransientDataScript transientData;
+    public List<string> particleIDs;
+    public bool doNotSpawnOnClick;
+    public int minParticles = 1;
+    public int maxParticles = 4;
+    private int randomNum;
+
+
+    void OnMouseDown()
+    {
+        if (!doNotSpawnOnClick)
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        if (TransientDataScript.IsTimeFlowing())
+        {
+            randomNum = Random.Range(minParticles, maxParticles);
+
+            for (int i = 0; i < randomNum; i++)
+            {
+                foreach (string id in particleIDs)
+                {
+                    ParticleFactory.GetParticle(gameObject, id, gameObject.name);
+                }
+            }
+        }
+    }
+}
