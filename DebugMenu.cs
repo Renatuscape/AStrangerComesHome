4ea9642332bf4e0e7d03e38dc29153e67cca1f9e
Renatuscape <rrjonsberg@gmail.com>
2024-09-10 18:02:45 +0200

    Updates SkipPrologue and EnableDemoMode for v0.0.1.0+ compatibility. Changes hardcoded tags to StaticTags entries where available.

--- DebugMenu.cs
+++ DebugMenu.cs
@@ -62,30 +62,49 @@ public class DebugMenu : MonoBehaviour
 
     public void EnableDemoMode()
     {
-        Player.Add("ALC000", 1, true);
-        Player.Add("GAR000", 1, true);
-        Player.Add("SCR004", 1, true);
-        Player.Add("SCR006", 1, true);
-        Player.Add("SCR007", 1, true);
-        Player.Add("SCR010", 1, true);
-        Player.Add("SCR012", 1, true);
-        Player.Add("SCR013", 1, true);
-        Player.Add("SCR016", 1, true);
+        Player.Add(StaticTags.Alchemy, 1, true);
+        Player.Add("MIS022", 1, true); // Chemistry license object
+        Player.Add("SCR006", 1, true); // Alchemy set Stella Point
+        Player.Add("SCR007", 1, true); // Alchemy set Capital
+        Player.Add(StaticTags.CoachSynths, 1, true);
+
+        Player.Add(StaticTags.CoachPlanters, 1, true);
+
+        Player.Add(StaticTags.GuildLicense, 1, true); // Guild license script
+        Player.Add("MIS021", 1, true); // Guild license object
+
+        Player.Add("SCR013", 1, true); // Merchantile license level
+        Player.Add("SCR016", 1, true); // Traveller estate unlock
 
         TransientDataScript.isDemoEnabled = true;
     }
 
     public void SkipPrologue()
     {
-        Player.Add("MIS021", 1, true);
-        Player.Add("MIS022", 1, true);
-        Player.Add("MIS023", 1, true);
+        // Ensure all mandatory rewards and quests are set to the least necessary level to complete the prologue chapter
 
-        Player.SetQuest("ARC001-Q01", 18);
-        Player.SetQuest("ARC000-Q00", 7);
+        // Alchemist Quest
+        Player.SetQuest("ARC001-Q00", 100);
+        Player.Add("ARC001", 3, true);
+        Player.Add(StaticTags.Fate, 1, true);
+
+
+        // Machinist Quest
         Player.SetQuest("ARC002-Q00", 100);
+        Player.Add("ARC002", 3, true);
+
+        // Gardener Quest - also unlocks gardening
         Player.SetQuest("ARC003-Q00", 100);
+        Player.Add("ARC003", 3, true);
+        Player.Add(StaticTags.Gardening, 1, true);
+        Player.Add(StaticTags.CoachPlanters, 1, true);
+
+        // Teller Quest
         Player.SetQuest("ARC004-Q00", 100);
+        Player.Add("ARC004", 3, true);
+
+        // Occultist Quest
         Player.SetQuest("ARC006-Q00", 100);
+        Player.Add("ARC006", 3, true);
     }
 }
