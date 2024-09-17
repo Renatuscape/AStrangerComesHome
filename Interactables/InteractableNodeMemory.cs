929687c08b459d4d09258a015a9b5f33b7c888f8
Renatuscape <rrjonsberg@gmail.com>
2024-09-17 15:46:15 +0200

    Adds variable for whether or not to auto play a memory's dialogue. Expands setup to test whether memorry has been claimed before and if it passes requirements. Adds null checks for animation.

--- Interactables/InteractableNodeMemory.cs
+++ Interactables/InteractableNodeMemory.cs
@@ -5,22 +5,46 @@ public class InteractableNodeMemory : InteractableNode
     // Use NodeID to in hierarchy to set the memory ID
 
     public Memory memory;
+    public bool autoPlayMemory;
     bool isReady = false;
     private void OnEnable()
     {
-        if (Player.GetCount(nodeID, name)  > 0) // Additional memories cannot be gained from locations, only quests/events
-        {
-            gameObject.SetActive(false);
-        }
-        else
+        if (TestConditions())
         {
             Setup();
         }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    bool TestConditions()
+    {
+        if (Player.GetCount(nodeID, name) > 0)// Additional memories cannot be gained from locations, only quests/events
+        {
+            return false;
+        }
+
+        memory = Memories.FindByID(nodeID);
+
+        if (memory != null)
+        {
+            if (memory.dialogue.CheckRequirements())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return false;
     }
 
     void Setup()
     {
-        memory = Memories.FindByID(nodeID);
         animatedSprite = AnimationLibrary.GetAnimatedObject("MemoryShard" + (memory.isUnique ? "_Unique" : ""));
 
         if (animatedSprite != null && rend != null)
@@ -46,6 +70,11 @@ public class InteractableNodeMemory : InteractableNode
         if (isReady && TransientDataScript.IsTimeFlowing())
         {
             CollectMemory();
+
+            if (autoPlayMemory)
+            {
+                TransientDataScript.gameManager.storySystem.StartMemory(memory);
+            }
         }
     }
 
@@ -54,15 +83,23 @@ public class InteractableNodeMemory : InteractableNode
         col.enabled = false;
         Player.Add(nodeID);
 
-        var useAnim = animatedSprite.GetAnimationType(AnimationType.use);
-
-        if (useAnim != null)
+        if (animatedSprite != null)
         {
-            StartCoroutine(AnimateAndFadeOut(useAnim, true));
+            var useAnim = animatedSprite.GetAnimationType(AnimationType.use);
+
+            if (useAnim != null)
+            {
+                StartCoroutine(AnimateAndFadeOut(useAnim, true));
+            }
+            else
+            {
+                Debug.LogWarning(nodeID + " node found no animation of the USE type. Check animation library or MemoryShard animations.");
+                StartCoroutine(FadeOutObject());
+            }
         }
         else
         {
-            Debug.LogWarning(nodeID + " node found no animation of the USE type. Check animation library or MemoryShard animations.");
+            Debug.LogWarning(nodeID + " node had no animated sprite. Check animation library or MemoryShard animations.");
             StartCoroutine(FadeOutObject());
         }
     }
