c037e23c13759ccd6d8542f5bf052a4e23226d43
Renatuscape <rrjonsberg@gmail.com>
2024-10-05 01:41:03 +0200

    Adds variable for tier name.

--- UI/UIMenus/GuildMenu/GuildRewardTier.cs
+++ UI/UIMenus/GuildMenu/GuildRewardTier.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class GuildRewardTier
 {
     public string tierID;
+    public string tierName;
     public string description;
     public List<IdIntPair> requirements;
     public List<IdIntPair> rewards;
