2c42a0d6038c4ddb5a85cf5b0eb56327adc6f54a
Renatuscape <rrjonsberg@gmail.com>
2024-10-04 23:42:52 +0200

    Streamlines class to hold data from JSON only.

--- UI/UIMenus/GuildMenu/GuildRewardTier.cs
+++ UI/UIMenus/GuildMenu/GuildRewardTier.cs
@@ -1,60 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using TMPro;
-using UnityEngine.UI;
 
 [Serializable]
 public class GuildRewardTier
 {
     public string tierID;
     public string description;
-    public int requiredCount;
-    public int playerCount;
     public List<IdIntPair> requirements;
     public List<IdIntPair> rewards;
-    public bool claimed;
-    public TextMeshProUGUI textMesh;
-    public Button button;
-
-    public void Setup(int playerCount, Button button)
-    {
-        this.playerCount = playerCount;
-        this.button = button;
-        textMesh = button.gameObject.GetComponentInChildren<TextMeshProUGUI>();
-
-        if (Player.claimedLoot.FirstOrDefault(l => l.objectID == tierID) != null)
-        {
-            UnityEngine.Debug.Log("Reward tier found in claimedLoot: " + tierID);
-            SetComplete();
-        }
-        else
-        {
-            UnityEngine.Debug.Log("NOT found in claimedLoot: " + tierID);
-        }
-    }
-
-    public void Claim()
-    {
-        if (!claimed)
-        {
-            Player.claimedLoot.Add(new() { objectID = tierID, amount = 1 });
-            SetComplete();
-
-            foreach (var entry in rewards)
-            {
-                Player.Add(entry);
-            }
-        }
-    }
-
-    void SetComplete()
-    {
-        claimed = true;
-        button.interactable = false;
-        textMesh.text = $"<color=#718c81><s>{textMesh.text}</s></color>";
-        ColorBlock colorBlock = button.colors;
-        colorBlock.disabledColor = new UnityEngine.Color(0.8071f, 0.9150f, 0.8301f);
-        button.colors = colorBlock;
-    }
 }
