4772b66d9ab8ddbf80545f8a5769f52fea9cdbff
Renatuscape <rrjonsberg@gmail.com>
2024-10-06 14:18:06 +0200

    Adds tags and tagsArray, and method for setting up tags.

--- GameData/BaseObject.cs
+++ GameData/BaseObject.cs
@@ -1,10 +1,23 @@
-﻿public class BaseObject
+﻿using System;
+using System.Linq;
+
+public class BaseObject
 {
     public string objectID;
     public string name;
     public string description;
+    public string tags;
+    public string[] tagsArray;
     public int maxValue;
     public ObjectType objectType;
+
+    public void SetupTags()
+    {
+        if (tags != null && tags.Length > 0)
+        {
+            tagsArray = tags.Split(new[] { ", " }, StringSplitOptions.None);
+        }
+    }
 }
 
 public enum ObjectType
