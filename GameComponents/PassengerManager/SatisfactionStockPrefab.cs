c7989f8dc6ee119f22d89c576d654874f7d8d0ab
Renatuscape <rrjonsberg@gmail.com>
2024-10-13 22:19:48 +0200

    Moves class to separate file.

---
+++ GameComponents/PassengerManager/SatisfactionStockPrefab.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SatisfactionStockPrefab : MonoBehaviour
+{
+    public Item item;
+    public ListItemPrefab listPrefab;
+
+    public void UpdateValues()
+    {
+        if (item != null && listPrefab.entry != null && !string.IsNullOrEmpty(item.name))
+        {
+            if (listPrefab.entry.amount == 0)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+                listPrefab.textMesh.text = $"{item.name} ({listPrefab.entry.amount})";
+            }
+        }
+        else if (listPrefab.entry == null || listPrefab.entry.objectID != "ALERT")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
