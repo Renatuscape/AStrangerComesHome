01f8a35889c4e51b84dc21c9c2dbb794de81a855
Renatuscape <rrjonsberg@gmail.com>
2024-10-14 14:43:43 +0200

    Adds new options for a tag container and displayed image sprite, and methods to set these up.

--- UI/PageinatedList/ListItemPrefab.cs
+++ UI/PageinatedList/ListItemPrefab.cs
@@ -8,4 +8,49 @@ public class ListItemPrefab: MonoBehaviour
     public TextMeshProUGUI textMesh;
     public Image bgImage;
     public Button button;
+    public GameObject tagContainer;
+    public GameObject tagPrefab;
+    public Image spriteImage;
+
+    public void CreateItemTags(Item item)
+    {
+        if (tagContainer != null && tagPrefab != null)
+        {
+            if (item != null && item.tagsArray != null)
+            {
+                foreach (var tag in item.tagsArray)
+                {
+                    var newTag = Instantiate(tagPrefab);
+
+                    newTag.transform.SetParent(tagContainer.transform, false);
+
+                    var formattedTag = tag[0].ToString().ToUpper() + tag.Remove(0, 1).ToLower();
+
+                    newTag.GetComponentInChildren<TextMeshProUGUI>().text = formattedTag;
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Tag container was null or tag prefab was null.");
+        }
+    }
+
+    public void DisplayItemSprite(string spriteID)
+    {
+        if (spriteImage != null)
+        {
+            var sprite = SpriteFactory.GetItemSprite(spriteID);
+
+            if (sprite != null)
+            {
+                spriteImage.sprite = sprite;
+                spriteImage.gameObject.SetActive(true);
+            }
+            else
+            {
+                spriteImage.gameObject.SetActive(false);
+            }
+        }
+    }
 }
