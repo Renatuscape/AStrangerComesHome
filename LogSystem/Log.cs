398e87e32f7d9095d20f0896d7c251066a82135d
Renatuscape <rrjonsberg@gmail.com>
2025-04-18 13:04:14 +0200

    Add class to control debug logging in the future

---
+++ LogSystem/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public static class Log
+{
+    internal static string Report {  get; private set; }
+    internal static void Write(string input)
+    {
+        string formattedEntry = $"\n{DateTime.Now}| {input}";
+        Debug.Log(formattedEntry);
+        Report += formattedEntry;
+    }
+
+    internal static void ToConsole()
+    {
+        Debug.Log(Report);
+    }
+
+    internal static void WriteError(string input)
+    {
+        string formattedEntry = $"\n{DateTime.Now}| ERROR | {input}";
+        Debug.LogError(formattedEntry);
+        Report += formattedEntry;
+    }
+}
