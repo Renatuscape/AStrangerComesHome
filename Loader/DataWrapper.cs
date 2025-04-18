90ecb5422c724a85c8ba24995cc8c8ef89fed30b
Renatuscape <rrjonsberg@gmail.com>
2025-04-18 17:22:24 +0200

    Create separate class to reuse when loading from JSON
    Loading classes were previously bloated by wrapper declaration

---
+++ Loader/DataWrapper.cs
@@ -0,0 +1,15 @@
+﻿[System.Serializable]
+public class DataWrapper // For wrapping around JSON objects when loading
+{
+    public Item[] items;
+    public Book[] books;
+    public Upgrade[] upgrades;
+    public Skill[] skills;
+    public Recipe[] recipes;
+    public Location[] locations;
+    public Region[] regions;
+    public Memory[] memories;
+    public Quest[] quests;
+    public Dialogue[] dialogues;
+    public Character[] characters;
+}
