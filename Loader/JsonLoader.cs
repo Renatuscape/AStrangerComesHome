f7c176708ce4773bc5a0463b51219a7a98393e20
Renatuscape <rrjonsberg@gmail.com>
2025-04-18 13:04:57 +0200

    Add abstract parent class for other json loaders to inherit from

---
+++ Loader/JsonLoader.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public abstract class JsonLoader : MonoBehaviour
+{
+    public string path = Application.streamingAssetsPath;
+    public string displayName = "Data";
+    public abstract Task StartLoading();
+}
