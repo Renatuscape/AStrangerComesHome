cf35519d8ae907535f9d6b5ba18bca52d7d9e9a6
Renatuscape <rrjonsberg@gmail.com>
2025-04-19 18:42:03 +0200

    Add new upgrade loader

---
+++ Loader/JsonLoaderUpgrades.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class JsonLoaderUpgrades : JsonLoader
+{
+    public JsonLoaderUpgrades()
+    {
+        displayName = "upgrades"; // This accesses the inherited field
+        path += "/JsonData/Upgrades/"; // Add to the streaming path
+    }
+
+    public override Task StartLoading()
+    {
+        return LoadFromJsonAsync("Upgrades.json");
+    }
+
+    public async Task LoadFromJsonAsync(string fileName)
+    {
+        string jsonPath = path + fileName;
+
+        if (File.Exists(jsonPath))
+        {
+            string jsonData = await Task.Run(() => File.ReadAllText(jsonPath));
+            DataWrapper dataWrapper = JsonUtility.FromJson<DataWrapper>(jsonData);
+
+            if (dataWrapper != null)
+            {
+                if (dataWrapper.upgrades != null)
+                {
+                    foreach (Upgrade entry in dataWrapper.upgrades)
+                    {
+                        UpgradeManager.Initialise(entry);
+                        Repository.instance.upgrades.Add(entry);
+                    }
+
+                    Log.Write($"All {displayName.ToUpper()} successfully loaded from Json file {fileName}.");
+                }
+                else
+                {
+                    Log.WriteError($"Object array for {displayName.ToUpper()} is null in JSON data. Ensure that... \n\t - The JSON data is wrapped in one object\n\t - That the parent JSON object has a name corresponding to the data wrapper\n\t - That the C# class is serializable.");
+                }
+            }
+            else
+            {
+                Log.WriteError("JSON data is malformed. No wrapper found?");
+                Log.Write("RAW JSON OUTPUT:\n" + jsonData); // Log the JSON data for inspection
+            }
+        }
+        else
+        {
+            Log.WriteError("JSON file not found: " + jsonPath);
+        }
+    }
+}
