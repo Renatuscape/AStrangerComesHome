9c2e8f95f21e5a424a4e0bccc2a474c38a292bea
Renatuscape <rrjonsberg@gmail.com>
2025-04-19 18:43:34 +0200

    Remove loader functionality

--- JsonManagers/LocationManager/LocationManager.cs
+++ JsonManagers/LocationManager/LocationManager.cs
@@ -1,73 +1,10 @@
-using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
-using System;
 
 public class LocationManager : MonoBehaviour
 {
-    public List<Location> debugLocationList = Locations.all;
-    public List<Region> debugRegionList = Regions.all;
-    public bool allObjecctsLoaded = false;
-    public int filesLoaded = 0;
-    public int numberOfFilesToLoad = 1;
-
-    public void StartLoading()
-    {
-        gameObject.SetActive(true);
-        LoadFromJson("Locations.json");
-        //Remember to update numberOfFilesToLoad if more files are added
-    }
-
-    [System.Serializable]
-    public class LocationWrapper //Necessary for Unity to read the .json contents as an object
-    {
-        public Location[] locations;
-    }
-    public void LoadFromJson(string fileName)
-    {
-        string jsonPath = Application.streamingAssetsPath + "/JsonData/Locations/" + fileName;
-
-        if (File.Exists(jsonPath))
-        {
-            string jsonData = File.ReadAllText(jsonPath);
-            LocationWrapper dataWrapper = JsonUtility.FromJson<LocationWrapper>(jsonData);
-
-            if (dataWrapper != null)
-            {
-                if (dataWrapper.locations != null)
-                {
-                    foreach (Location location in dataWrapper.locations)
-                    {
-                        InitialiseLocation(location);
-                    }
-                    filesLoaded++;
-                    if (filesLoaded == numberOfFilesToLoad)
-                    {
-                        allObjecctsLoaded = true;
-                        Debug.Log("All LOCATIONS successfully loaded from Json.");
-                    }
-                }
-                else
-                {
-                    Debug.LogError("Location array is null in JSON data. Check that the list has a wrapper with the \'locations\' tag and that the object class is tagged serializable.");
-                }
-            }
-            else
-            {
-                Debug.LogError("JSON data is malformed. No wrapper found?");
-                Debug.Log(jsonData); // Log the JSON data for inspection
-            }
-        }
-        else
-        {
-            Debug.LogError("JSON file not found: " + jsonPath);
-        }
-    }
-
-    public static void InitialiseLocation(Location location)
+    public static void Initialise(Location location)
     {
         objectIDReader(ref location);
-        Locations.all.Add(location);
     }
 
     public static void objectIDReader(ref Location location)
