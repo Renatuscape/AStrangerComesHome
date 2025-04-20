f696e96d0814f54e7b8cec716fad5119e7f38f4d
Renatuscape <rrjonsberg@gmail.com>
2025-04-20 22:46:42 +0200

    Make static and search repository instead of Dialogues.all

--- JsonManagers/DialogueManager/DialogueManager.cs
+++ JsonManagers/DialogueManager/DialogueManager.cs
@@ -1,85 +1,208 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
-using System.Threading.Tasks;
 
-public class DialogueManager : MonoBehaviour
+public static class DialogueManager
 {
-    public List<Dialogue> debugCharacterList = Dialogues.all;
-    public bool allObjecctsLoaded = false;
-    public int filesLoaded;
-    public int numberOfFilesToLoad;
-
-    public Task StartLoading()
+    public static void Initialise(Dialogue dialogue)
     {
-        List<Task> loadingTasks = new List<Task>();
-
-        gameObject.SetActive(true);
-        filesLoaded = 0;
-        numberOfFilesToLoad = 0;
-
-        var info = new DirectoryInfo(Application.streamingAssetsPath + "/JsonData/Quests/Dialogues/");
-        var fileInfo = info.GetFiles();
-
-        foreach (var file in fileInfo)
+        if (string.IsNullOrEmpty(dialogue.objectID))
         {
-            if (file.Extension == ".json")
+            Debug.Log($"Missing objectID for {dialogue}");
+        }
+        else
+        {
+            ParseDialogueID(dialogue);
+
+            if (dialogue.stageType == StageType.Dialogue)
             {
-                numberOfFilesToLoad++;
-                Task loadingTask = LoadFromJsonAsync(Path.GetFileName(file.FullName)); // Pass only the file name
-                loadingTasks.Add(loadingTask);
+                ParseDialogueSteps(dialogue); //set up dialogue steps with proper speaker objects
+            }
+
+            if (string.IsNullOrEmpty(dialogue.speakerID))
+            {
+                dialogue.speakerID = dialogue.objectID.Split('-')[0];
             }
         }
-
-        return Task.WhenAll(loadingTasks);
     }
-
-    [System.Serializable]
-    public class DialogueWrapper //Necessary for Unity to read the .json contents as an object
+    static void ParseDialogueID(Dialogue dialogue)
     {
-        public Dialogue[] dialogues;
-    }
-    public async Task LoadFromJsonAsync(string fileName)
-    {
-        string jsonPath = Application.streamingAssetsPath + "/JsonData/Quests/Dialogues/" + fileName;
-
-        if (File.Exists(jsonPath))
+        //SET STAGE TYPE
+        if (dialogue.objectID.Substring(14, 1) == "M")
         {
-            string jsonData = await Task.Run(() => File.ReadAllText(jsonPath));
-            DialogueWrapper dataWrapper = JsonUtility.FromJson<DialogueWrapper>(jsonData);
+            dialogue.stageType = StageType.Memory;
+        }
+        else if (dialogue.objectID.Substring(14, 1) == "P")
+        {
+            dialogue.stageType = StageType.PopUp;
+        }
+        else if (dialogue.objectID.Substring(14, 1) == "N")
+        {
+            dialogue.stageType = StageType.Node;
+        }
+        else if (dialogue.objectID.Substring(14, 1) == "L")
+        {
+            dialogue.stageType = StageType.Letter;
+        }
 
-            if (dataWrapper != null)
+        //SET QUEST STAGE
+        dialogue.questStage = int.Parse(dialogue.objectID.Substring(11, 2));
+
+        //SET QUEST ID
+        dialogue.questID = dialogue.objectID.Substring(0, 10);
+    }
+    static void ParseDialogueSteps(Dialogue dialogue)
+    {
+        dialogue.dialogueEvents = CreateEventsFromStringList(dialogue.content);
+    }
+
+    public static List<DialogueEvent> CreateEventsFromStringList (List<string> content)
+    {
+        List<DialogueEvent> events = new();
+
+        for (int i = 0; i < content.Count; i = i + 2)
+        {
+            // PARSE SPEAKER TAG
+            var speakerLine = content[i];
+
+            if (!speakerLine.Contains("-")) // Treat as old step
             {
-                if (dataWrapper.dialogues != null)
+                string speakerTag = content[i];
+                Character foundSpeaker = Repository.instance.characters.Find((s) => s.dialogueTag.ToLower() == speakerTag.ToLower());
+                if (foundSpeaker != null)
                 {
-                    foreach (Dialogue dialogue in dataWrapper.dialogues)
-                    {
-                        DialogueSetup.InitialiseDialogue(dialogue);
-                        filesLoaded++;
-                    }
+                    // PARSE CONTENT AND ADD EVENT
+                    DialogueEvent dEvent = new() { speaker = foundSpeaker, eventName = $"{foundSpeaker.name} - step{Mathf.FloorToInt(i / 2 + 1)}" };
+                    dEvent.content = content[i + 1];
+                    events.Add(dEvent);
 
-                    if (filesLoaded == numberOfFilesToLoad)
+                    if (string.IsNullOrEmpty(dEvent.startingPlacement) && string.IsNullOrEmpty(dEvent.targetPlacement))
                     {
-                        allObjecctsLoaded = true;
-                        Debug.Log("All DIALOGUE successfully loaded from Json.");
+                        dEvent.targetPlacement = "NOR";
                     }
                 }
                 else
                 {
-                    Debug.LogError("Dialogue objet is null in JSON data. Check that the list has a wrapper with the \'dialogue\' tag and that the object class is tagged serializable.");
+                    Debug.LogError($"Could not parse dialogue content for content stage {i} ({content[i]}) because speaker tag \"{speakerTag}\" return null. Tags in dictionary: " + DialogueTagParser.tags.Count);
                 }
             }
             else
             {
-                Debug.LogError("JSON data is malformed. No wrapper found?");
-                Debug.Log(jsonData); // Log the JSON data for inspection
+                try
+                {
+                    DialogueEvent dEvent = ParseDialogueEventID(speakerLine);
+                    dEvent.content = content[i + 1];
+                    events.Add(dEvent);
+
+                    if (string.IsNullOrEmpty(dEvent.startingPlacement) && string.IsNullOrEmpty(dEvent.targetPlacement))
+                    {
+                        dEvent.targetPlacement = "NOR";
+                    }
+                }
+                catch
+                {
+                    Debug.LogError($"Something went wrong when attempting to parse content for step {i} ({content[i + 1]} ).");
+                }
             }
+        }
+
+        return events;
+    }
+
+    public static DialogueEvent ParseDialogueEventID(string eventID)
+    {
+        // Example ID: Arcanist-S#SAD-SP#MID-TP#FAR-E#SHAKE-MAS#SLO-L#TRUE-HO#TRUE
+        // = show sad arcanist sprite, start mid and move slowly to far position, shaking effect, left position portrait and hide other
+
+        // Example ID: Gardener-S#HAPPY-OPO#Arcanist
+        // = show happy gardener and override other portrait with alchemist. Other portrait override will also be parsed if it exists.
+
+        string[] eventTags = eventID.Split('-');
+        Character speaker = Characters.FindByTag(eventTags[0], "ParseDialogueEvent");
+
+        if (speaker != null)
+        {
+            DialogueEvent dEvent = new();
+            dEvent.speaker = speaker;
+
+            if (eventID.Contains("L#TRUE"))
+            {
+                dEvent.isLeft = true;
+            }
+
+            if (eventID.Contains("HO#TRUE"))
+            {
+                dEvent.hideOtherPortrait = true;
+            }
+
+            if (eventID.Contains("HB#TRUE"))
+            {
+                dEvent.hideBothPortraits = true;
+            }
+
+            if (eventID.Contains("HS#TRUE"))
+            {
+                dEvent.hideSpeakerPortrait = true;
+            }
+
+            foreach (string tag in eventTags)
+            {
+                if (tag.Contains("SP#"))
+                {
+                    dEvent.startingPlacement = tag.Replace("SP#", "");
+                }
+                else if (tag.Contains("TP#"))
+                {
+                    dEvent.targetPlacement = tag.Replace("TP#", "");
+                }
+                else if (tag.Contains("MAS#"))
+                {
+                    dEvent.moveAnimationSpeed = tag.Replace("MAS#", "");
+                }
+                else if (tag.Contains("OPO#"))
+                {
+                    dEvent.otherPortraitOverride.Add(tag.Replace("OPO#", ""));
+                    // Add override parse call here
+                }
+                else if (tag.Contains("E#"))
+                {
+                    dEvent.effect = tag.Replace("E#", "");
+                }
+                else if (tag.Contains("S#"))
+                {
+                    // Format in a way that a future SpriteManager can handle.
+
+                    var spriteTag = dEvent.speaker.objectID + "-" + tag.Replace("S#", "");
+                    // Debug.Log("Parsed sprite tag: " + spriteTag);
+
+                    dEvent.spriteID = spriteTag;
+                }
+                else if (tag.Contains("BG#"))
+                {
+                    // Format in a way that a future SpriteManager can handle.
+
+                    var spriteTag = dEvent.speaker.objectID + "-" + tag.Replace("BG#", "");
+                    // Debug.Log("Parsed sprite tag: " + spriteTag);
+
+                    dEvent.backgroundID = spriteTag;
+                }
+            }
+
+            if (string.IsNullOrEmpty(dEvent.spriteID))
+            {
+                dEvent.spriteID = "DEFAULT";
+            }
+
+            return dEvent;
         }
         else
         {
-            Debug.LogError("JSON file not found: " + jsonPath);
+            Debug.LogWarning("Could not find speaker object for event ID " + eventID);
+            return null;
         }
     }
 
+    static DialogueEvent ParseOverrideEventID(string eventID)
+    {
+        return new();
+    }
 }
