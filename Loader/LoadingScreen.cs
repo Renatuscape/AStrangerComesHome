fb1a20e2c2d190acf2a66e00b0533927b326b186
Renatuscape <rrjonsberg@gmail.com>
2025-04-20 22:40:33 +0200

    Loading screen persists from GameLoader scene, replacing old loader canvas

---
+++ Loader/LoadingScreen.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Slider bar;
+    public TextMeshProUGUI contextMesh;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        GameManagerScript.loadingCanvas = this;
+    }
+
+    public void Simplify()
+    {
+        contextMesh.text = "";
+        bar.gameObject.SetActive(false);
+    }
+}
